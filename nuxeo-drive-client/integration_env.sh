c2317a982cf11210f264f02a91dde0c978d9e635
Olivier Grisel <olivier.grisel@ensta.org>
2012-06-18 20:32:29 +0200

    test file rename + add env setup for nuxeo client tests

---
+++ nuxeo-drive-client/integration_env.sh
@@ -0,0 +1,4 @@
+# Setup default test env with server on localhost
+export NXDRIVE_TEST_NUXEO_URL=http://localhost:8080/nuxeo
+export NXDRIVE_TEST_USER=Administrator
+export NXDRIVE_TEST_PASSWORD=Administrator
