3385154d42a94f1c5ad164f63de0f2948fa272af
Olivier Grisel <olivier.grisel@ensta.org>
2012-06-18 20:14:49 +0200

    more tests

--- nuxeo-drive-client/nxdrive/tests/test_client.py
+++ nuxeo-drive-client/nxdrive/tests/test_client.py
@@ -57,22 +57,33 @@ def test_make_documents():
     assert_true(nxclient.exists(doc_1))
     assert_equal(nxclient.get_content(doc_1), "")
     doc_1_info = nxclient.get_info(doc_1)
+    assert_equal(doc_1_info.name, 'Document 1.txt')
     assert_equal(doc_1_info.uid, doc_1)
     assert_equal(doc_1_info.digest, None)
+    assert_equal(doc_1_info.folderish, False)
 
     doc_2 = nxclient.make_file(TEST_WORKSPACE, 'Document 2.txt',
                               content=SOME_TEXT_CONTENT)
     assert_true(nxclient.exists(doc_2))
     assert_equal(nxclient.get_content(doc_2), SOME_TEXT_CONTENT)
     doc_2_info = nxclient.get_info(doc_2)
+    assert_equal(doc_2_info.name, 'Document 2.txt')
+    assert_equal(doc_2_info.uid, doc_2)
     assert_equal(doc_2_info.digest, SOME_TEXT_DIGEST)
+    assert_equal(doc_2_info.folderish, False)
 
     nxclient.delete(doc_2)
     assert_true(nxclient.exists(doc_1))
     assert_false(nxclient.exists(doc_2))
 
-    folder_1 = nxclient.make_folder(TEST_WORKSPACE, SOME_TEXT_CONTENT)
+    folder_1 = nxclient.make_folder(TEST_WORKSPACE, 'A new folder')
     assert_true(nxclient.exists(folder_1))
+    folder_1_info = nxclient.get_info(folder_1)
+    assert_equal(folder_1_info.name, 'A new folder')
+    assert_equal(folder_1_info.uid, folder_1)
+    assert_equal(folder_1_info.digest, None)
+    assert_equal(folder_1_info.folderish, True)
+
     doc_3 = nxclient.make_file(folder_1, 'Document 3.txt',
                                content=SOME_TEXT_CONTENT)
     nxclient.delete(folder_1)
