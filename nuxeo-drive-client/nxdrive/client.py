033b0bc35d5e3ba4bad90acd53125e296b43a05f
Olivier Grisel <olivier.grisel@ensta.org>
2012-06-19 19:44:24 +0200

    more work and tests on the filesystem API: implement deduplication logic for filenames

--- nuxeo-drive-client/nxdrive/client.py
+++ nuxeo-drive-client/nxdrive/client.py
@@ -1,6 +1,5 @@
 """Uniform API to access both local and remote resource for synchronization."""
 
-from pprint import pprint
 from collections import namedtuple
 from datetime import datetime
 from email.mime.base import MIMEBase
@@ -16,6 +15,7 @@ import shutil
 import time
 import urllib2
 import urllib
+import re
 
 
 # Make the following an optional binding configuration
@@ -34,6 +34,10 @@ class Unauthorized(Exception):
                 "the provided credentials" % (self.user_id, self.server_url))
 
 
+class NotFound(Exception):
+    pass
+
+
 # Data transfer objects
 
 class FileInfo(object):
@@ -42,7 +46,7 @@ class FileInfo(object):
     def __init__(self, root, path, folderish, last_modification_time,
                  digest_func='md5'):
         self.root = root  # the sync root folder local path
-        self.path = path  # the relative path to the root
+        self.path = path  # the truncated path (under the root)
         self.folderish = folderish  # True if a Folder
 
         # Last OS modification date of the file
@@ -51,13 +55,19 @@ class FileInfo(object):
         # Function to use
         self._digest_func = digest_func.lower()
 
+        # Precompute base name once and for all are it's often useful in
+        # practice
+        self.name = os.path.basename(path)
+
     def get_digest(self):
         """Lazy computation of the digest"""
+        if self.folderish:
+            return None
         digester = getattr(hashlib, self._digest_func, None)
         if digester is None:
             raise ValueError('Unknow digest method: ' + self.digest_func)
 
-        filepath = os.path.join(self.root, self.path)
+        filepath = os.path.join(self.root, self.path[1:])
         h = digester()
         with open(filepath, 'rb') as f:
             while True:
@@ -93,22 +103,21 @@ class LocalClient(object):
     """Client API implementation for the local file system"""
 
     def __init__(self, base_folder, digest_func='md5'):
+        while len(base_folder) > 1 and base_folder.endswith(os.path.sep):
+            base_folder = base_folder[:-1]
         self.base_folder = base_folder
         self._digest_func = digest_func
 
-    def authenticate(self):
-        # TODO
-        return True
-
     # Getters
     def get_info(self, ref):
-        os_path = os.path.join(self.base_folder, ref)
+        os_path = self._abspath(ref)
         if not os.path.exists(os_path):
-            return None
+            raise NotFound("Could not found file '%s' under '%s'" % (
+                ref, self.base_folder))
         folderish = os.path.isdir(os_path)
         stat_info = os.stat(os_path)
         mtime = datetime.fromtimestamp(stat_info.st_mtime)
-        path = os_path[len(self.base_folder) + 1:]
+        path = '/' + os_path[len(self.base_folder) + 1:]
         # On unix we could use the inode for file move detection but that won't
         # work on Windows. To reduce complexity of the code and the possibility
         # to have Windows specific bugs, let's not use the unixe inode at all.
@@ -117,13 +126,13 @@ class LocalClient(object):
                         digest_func=self._digest_func)
 
     def get_content(self, ref):
-        return open(os.path.join(self.base_folder, ref), "rb").read()
+        return open(self._abspath(ref), "rb").read()
 
     def get_descendants(self, ref=None):
         if ref is None:
             os_path = self.base_folder
         else:
-            os_path = os.path.join(self.base_folder, ref)
+            os_path = self._abspath(ref)
         result = []
         for root, dirs, files in os.walk(os_path):
             for dir in dirs:
@@ -137,24 +146,61 @@ class LocalClient(object):
         return result
 
     def make_folder(self, parent, name):
-        os.mkdir(os.path.join(self.base_folder, parent, name))
+        os_path, name = self._abspath_deduped(parent, name)
+        os.mkdir(os_path)
+        return os.path.join(parent, name)
 
     def make_file(self, parent, name, content=None):
-        with open(os.path.join(self.base_folder, parent, name), "wcb") as f:
+        os_path, name = self._abspath_deduped(parent, name)
+        with open(os_path, "wcb") as f:
             if content:
                 f.write(content)
+        return os.path.join(parent, name)
 
     def update(self, ref, content):
-        with open(os.path.join(self.base_folder, ref), "wb") as f:
+        with open(self._abspath(ref), "wb") as f:
             f.write(content)
 
     def delete(self, ref):
-        # TODO: support the OS trash?
-        os_path = os.path.join(self.base_folder, ref)
+        # TODO: add support the OS trash?
+        os_path = self._abspath(ref)
         if os.path.isfile(os_path):
             os.unlink(os_path)
         elif os.path.isdir(os_path):
             shutil.rmtree(os_path)
+
+    def exists(self, ref):
+        os_path = self._abspath(ref)
+        return os.path.exists(os_path)
+
+    def _abspath(self, ref):
+        if not ref.startswith('/'):
+            raise ValueError("FileClient expects ref starting with '/'")
+        return os.path.abspath(os.path.join(self.base_folder, ref[1:]))
+
+    def _abspath_deduped(self, parent, orig_name):
+        # decompose the name into actionable components
+        if "." in orig_name:
+            name, extension = orig_name.rsplit('.', 1)
+            suffix = "." + extension
+        else:
+            name, suffix = orig_name, ""
+
+        for _ in range(1000):
+            os_path = self._abspath(os.path.join(parent, name + suffix))
+            if not os.path.exists(os_path):
+                return os_path, name + suffix
+
+            # the is a duplicated file, try to come with a new name
+            m = re.match(r'(.*)__(\d)', name)
+            if m:
+                short_name, increment = m.groups()
+                name = "%s__%d" % (short_name, int(increment) + 1)
+            else:
+                name = name + '__1'
+
+        raise ValueError("Failed to de-duplicate '%s' under '%s'" % (
+            orig_name, parent))
 
 
 class NuxeoClient(object):
@@ -201,15 +247,6 @@ class NuxeoClient(object):
         for operation in response["operations"]:
             self.operations[operation['id']] = operation
 
-    @staticmethod
-    def authenticate(self, server):
-        # Perform a cheap query to trigger authentication check
-        try:
-            self.fetch_api()
-            return True
-        except Unauthorized:
-            return False
-
     def exists(self, ref):
         if ref.startswith('/'):
             results = self.query(
@@ -227,7 +264,15 @@ class NuxeoClient(object):
         raise NotImplementedError()
 
     def get_info(self, ref):
-        doc = self.fetch(ref)
+        try:
+            doc = self.fetch(ref)
+        except urllib2.HTTPError as e:
+            if e.code == 404:
+                raise NotFound("Could not find '%s' on '%s'" % (
+                    ref, self.server_url))
+            else:
+                raise e
+
         props = doc['properties']
         folderish = 'Folderish' in doc['facets']
 
--- nuxeo-drive-client/nxdrive/tests/test_filesystem_client.py
+++ nuxeo-drive-client/nxdrive/tests/test_filesystem_client.py
@@ -1,67 +1,52 @@
 import os
 import hashlib
+import tempfile
+import shutil
 from nose import with_setup
-from nose import SkipTest
 from nose.tools import assert_true
 from nose.tools import assert_false
 from nose.tools import assert_equal
 from nose.tools import assert_not_equal
 from nose.tools import assert_raises
 
-from nxdrive.client import NuxeoClient
-from nxdrive.client import Unauthorized
+from nxdrive.client import LocalClient
 from nxdrive.client import NotFound
 
 
-TEST_WORKSPACE = '/default-domain/workspaces/test-nxdrive'
-
+LOCAL_TEST_FOLDER = None
+TEST_WORKSPACE = None
 nxclient = None
 
 
-def setup_integration_server():
-    global nxclient
-    NUXEO_URL = os.environ.get('NXDRIVE_TEST_NUXEO_URL')
-    USER = os.environ.get('NXDRIVE_TEST_USER')
-    PASSWORD = os.environ.get('NXDRIVE_TEST_PASSWORD')
-    if None in (NUXEO_URL, USER, PASSWORD):
-        raise SkipTest("No integration server configuration found in "
-                       "environment.")
-    nxclient = NuxeoClient(NUXEO_URL, USER, PASSWORD)
-
-    parent_path = os.path.dirname(TEST_WORKSPACE)
-    workspace_name = os.path.basename(TEST_WORKSPACE)
-    nxclient.create(parent_path, 'Workspace', name=workspace_name,
-                    properties={'dc:title': 'Nuxeo Drive Tests'})
+def setup_temp_folder():
+    global nxclient, LOCAL_TEST_FOLDER, TEST_WORKSPACE
+    LOCAL_TEST_FOLDER = tempfile.mkdtemp('-nuxeo-drive-tests')
+    nxclient = LocalClient(LOCAL_TEST_FOLDER)
+    TEST_WORKSPACE = nxclient.make_folder('/', 'Some Workspace')
+    assert_true(nxclient.exists(TEST_WORKSPACE))
 
 
-def teardown_integration_server():
-    if nxclient is not None and nxclient.exists(TEST_WORKSPACE):
-        nxclient.delete(TEST_WORKSPACE)
+def teardown_temp_folder():
+    if os.path.exists(LOCAL_TEST_FOLDER):
+        shutil.rmtree(LOCAL_TEST_FOLDER)
 
 
-with_integration_server = with_setup(
-    setup_integration_server, teardown_integration_server)
+with_temp_folder = with_setup(setup_temp_folder, teardown_temp_folder)
 
 
-@with_integration_server
-def test_authentication_failure():
-    assert_raises(
-        Unauthorized,
-        NuxeoClient, nxclient.server_url, 'someone else', 'bad password')
-
-
-@with_integration_server
+@with_temp_folder
 def test_make_documents():
     SOME_TEXT_CONTENT = "Some text content."
     SOME_TEXT_DIGEST = hashlib.md5(SOME_TEXT_CONTENT).hexdigest()
+    EMPTY_DIGEST = hashlib.md5().hexdigest()
 
     doc_1 = nxclient.make_file(TEST_WORKSPACE, 'Document 1.txt')
     assert_true(nxclient.exists(doc_1))
     assert_equal(nxclient.get_content(doc_1), "")
     doc_1_info = nxclient.get_info(doc_1)
     assert_equal(doc_1_info.name, 'Document 1.txt')
-    assert_equal(doc_1_info.uid, doc_1)
-    assert_equal(doc_1_info.get_digest(), None)
+    assert_equal(doc_1_info.path, doc_1)
+    assert_equal(doc_1_info.get_digest(), EMPTY_DIGEST)
     assert_equal(doc_1_info.folderish, False)
 
     doc_2 = nxclient.make_file(TEST_WORKSPACE, 'Document 2.txt',
@@ -70,7 +55,7 @@ def test_make_documents():
     assert_equal(nxclient.get_content(doc_2), SOME_TEXT_CONTENT)
     doc_2_info = nxclient.get_info(doc_2)
     assert_equal(doc_2_info.name, 'Document 2.txt')
-    assert_equal(doc_2_info.uid, doc_2)
+    assert_equal(doc_2_info.path, doc_2)
     assert_equal(doc_2_info.get_digest(), SOME_TEXT_DIGEST)
     assert_equal(doc_2_info.folderish, False)
 
@@ -82,7 +67,7 @@ def test_make_documents():
     assert_true(nxclient.exists(folder_1))
     folder_1_info = nxclient.get_info(folder_1)
     assert_equal(folder_1_info.name, 'A new folder')
-    assert_equal(folder_1_info.uid, folder_1)
+    assert_equal(folder_1_info.path, folder_1)
     assert_equal(folder_1_info.get_digest(), None)
     assert_equal(folder_1_info.folderish, True)
 
@@ -93,7 +78,7 @@ def test_make_documents():
     assert_false(nxclient.exists(doc_3))
 
 
-@with_integration_server
+@with_temp_folder
 def test_complex_filenames():
     # create another folder with the same title
     title_with_accents = u"\xc7a c'est l'\xe9t\xe9 !"
@@ -105,17 +90,24 @@ def test_complex_filenames():
     title_with_accents = u"\xc7a c'est l'\xe9t\xe9 !"
     folder_2 = nxclient.make_folder(TEST_WORKSPACE, title_with_accents)
     folder_2_info = nxclient.get_info(folder_2)
-    assert_equal(folder_2_info.name, title_with_accents)
+    assert_equal(folder_2_info.name, title_with_accents + "__1")
     assert_not_equal(folder_1, folder_2)
 
+    title_with_accents = u"\xc7a c'est l'\xe9t\xe9 !"
+    folder_3 = nxclient.make_folder(TEST_WORKSPACE, title_with_accents)
+    folder_3_info = nxclient.get_info(folder_3)
+    assert_equal(folder_3_info.name, title_with_accents + "__2")
+    assert_not_equal(folder_1, folder_3)
+
     # Create a file
-    # TODO: handle sanitization of the '/' character in local name
+    # TODO: handle sanitization of the '/' and '\' characters in local name
     long_filename = u"\xe9" * 50 + u"%$#!*()[]{}+_-=';:&^" + ".doc"
     file_1 = nxclient.make_file(folder_1, long_filename)
     file_1 = nxclient.get_info(file_1)
     assert_equal(file_1.name, long_filename)
+    assert_equal(file_1.path, os.path.join(folder_1_info.path, long_filename))
 
 
-@with_integration_server
-def test_missing_document():
+@with_temp_folder
+def test_missing_file():
     assert_raises(NotFound, nxclient.get_info, '/Something Missing')
