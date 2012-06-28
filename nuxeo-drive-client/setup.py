e7741d886bdc43e9a6ff8a9b1a5b5ed0e3630355
Olivier Grisel <olivier.grisel@ensta.org>
2012-06-28 20:36:30 +0200

    NXP-9586: Optional setup for esky / cx_Freeze for standalone distribution

--- nuxeo-drive-client/setup.py
+++ nuxeo-drive-client/setup.py
@@ -2,6 +2,11 @@
 #
 # Copyright (C) 2012 Nuxeo
 #
+try:
+    from esky import bdist_esky
+except ImportError:
+    print("WARN: Install esky and cx_Freeze if you want to build the "
+          " auto-updatable, standalone distribution of nxdrive.")
 from distutils.core import setup
 
 setup(
@@ -12,6 +17,14 @@ setup(
     author_email="ogrisel@nuxeo.com",
     url='http://github.com/nuxeo/nuxeo-drive',
     packages=['nxdrive'],
-    scripts=["bin/nxdrive"],
+    scripts=["bin/nxdrive.py"],
+    options={
+        "bdist_esky": {
+            # forcibly include some modules
+            #"includes": ["nxdrive"],
+            # forcibly exclude some other modules
+            "excludes": ["pydoc", "ipdb"],
+        }
+    },
     long_description=open('README.rst').read(),
 )
