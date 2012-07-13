c356f97d24ea2af7e33059fd63705d0035c250a8
Olivier Grisel <olivier.grisel@ensta.org>
2012-07-13 20:37:39 +0200

    more work in progress on windows packaging

--- nuxeo-drive-client/setup.py
+++ nuxeo-drive-client/setup.py
@@ -2,16 +2,31 @@
 #
 # Copyright (C) 2012 Nuxeo
 #
+
+import sys
+
 try:
-    from esky import bdist_esky
+    from cx_Freeze import setup, Executable
+
+    base = None
+    if sys.platform == "win32":
+        #base = "Win32GUI"
+        base = None
+    executables = [Executable("bin/ndrive", base=base)]
+    scripts = []
+    # TODO: investigate with esky to get an auto-updateable version but
+	# then make sure that we can still have .msi and .dmg packages
+	# instead of simple zip files.
 except ImportError:
-    print("WARN: Install esky and cx_Freeze if you want to build the "
-          " auto-updatable, standalone distribution of nxdrive.")
-from distutils.core import setup
+    print("WARN: Install cx_Freeze if you want to build the "
+          " standalone distribution of nxdrive.")
+    from distutils.core import setup
+    executables = []
+    scripts = ["bin/ndrive"]
 
 setup(
     name='nuxeo-drive',
-    version='0.1.0-git',
+    version='0.1.0',
     description="Desktop synchronization client for Nuxeo.",
     author="Olivier Grisel",
     author_email="ogrisel@nuxeo.com",
@@ -20,14 +35,16 @@ setup(
         'nxdrive',
         'nxdrive.tests',
     ],
-    scripts=["bin/ndrive"],
-    options={
-        "bdist_esky": {
-            # forcibly include some modules
-            #"includes": ["nxdrive"],
-            # forcibly exclude some other modules
-            "excludes": ["pydoc", "ipdb"],
-        }
-    },
+    scripts=scripts,
+	executables=executables,
+	options = {
+        "build_exe": {
+            "packages": [
+                "sqlalchemy.dialects.sqlite",
+                "nose",
+            ],
+            "excludes": ["ipdb", "clf", "IronPython"],
+        },
+	},
     long_description=open('README.rst').read(),
 )
--- nuxeo-drive-client/tools/windows/nxdrive-setup-dev.ps1
+++ nuxeo-drive-client/tools/windows/nxdrive-setup-dev.ps1
@@ -1,7 +1,7 @@
 # Powershell script: save on Desktop and Right Click "Run with PowerShell"
 
 # Get ready to download and install dev tools from the web
-$storagedir = "$pwd\nxdrive-tools"
+$storagedir = "$pwd\nxdrive-downloads"
 mkdir $storagedir -ErrorAction SilentlyContinue
 $webclient = New-Object System.Net.WebClient
 
@@ -33,6 +33,14 @@ echo "Installing Setuptools from $ez_setup"
 python $ez_setup
 echo "Installing pip"
 easy_install pip
+
+# Install cx_Freeze manually as pip does not work for this package
+$url = "http://downloads.sourceforge.net/project/cx-freeze/4.3/cx_Freeze-4.3.win32-py2.7.msi?r=http%3A%2F%2Fcx-freeze.sourceforge.net%2F&ts=1342189378&use_mirror=dfn"
+$cx_freeze = "$storagedir\cx_Freeze-4.3.win32-py2.7.msi"
+echo "Downloading cx_Freeze from $url"
+$webclient.DownloadFile($url, $cx_freeze)
+echo "Installing cx_Freeze from $cx_freeze"
+msiexec.exe /qn /I $cx_freeze
 
 echo "You can now clone the nuxeo-drive repo:"
 echo "git clone https://github.com/nuxeo/nuxeo-drive.git"
