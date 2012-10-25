6215199f3a6a1b43e479da11ede822cc674c850e
Olivier Grisel <olivier.grisel@ensta.org>
2012-10-25 02:20:45 +0200

    NXP-10354: make win32 protocol handler registration work without Administrator priviledges

--- nuxeo-drive-client/nxdrive/protocol_handler/win32.py
+++ nuxeo-drive-client/nxdrive/protocol_handler/win32.py
@@ -12,7 +12,19 @@ def find_exe_path():
         exe_path = nxdrive_path.replace(frozen_suffix, 'ndrivew.exe')
         if os.path.exists(exe_path):
             return exe_path
+    # TODO: handle the python.exe + python script as sys.argv[0] case as well
     return None
+
+
+def update_key(reg, path, attributes=()):
+    """Helper function to create / set a key with attribute values"""
+    import _winreg
+    key = _winreg.CreateKey(reg, path)
+    _winreg.CloseKey(key)
+    key = _winreg.OpenKey(reg, path, 0, _winreg.KEY_WRITE)
+    for attribute, type_, value in attributes:
+        _winreg.SetValueEx(key, attribute, 0, type_, value)
+    _winreg.CloseKey(key)
 
 
 def register_protocol_handlers(controller):
@@ -25,26 +37,22 @@ def register_protocol_handlers(controller):
                  'skipping protocol handler registration')
         return
 
-    command = '"' + exe_path + '" %1'
-    log.debug("Registering 'nxdrive' protocol handler command: %s", command)
+    log.debug("Registering 'nxdrive' protocol handler to: %s", exe_path)
     reg = _winreg.ConnectRegistry(None, _winreg.HKEY_CURRENT_USER)
-    try:
-        # Create the nxdrive key if missing:
-        nxdrive = _winreg.CreateKey(reg, 'nxdrive')
-        _winreg.CloseKey(nxdrive)
-    except _winreg.EnvironmentError:
-        # Already existing key
-        pass
 
-    nxdrive = _winreg.OpenKey(reg, 'nxdrive', 0, _winreg.KEY_WRITE)
-    _winreg.SetValueEx(nxdrive,'EditFlags', 0, _winreg.REG_DWORD, 2)
-    _winreg.SetValueEx(nxdrive,'', 0, _winreg.REG_SZ, 'URL:nxdrive Protocol')
-    _winreg.SetValueEx(nxdrive,'URL Protocol', 0, _winreg.REG_SZ, '')
-    _winreg.CloseKey(nxdrive)
+    # Create the nxdrive protocol key
+    nxdrive_class_path = 'Software\\Classes\\nxdrive'
+    nxdrive_class_attributes = (
+        ('EditFlags', _winreg.REG_DWORD, 2),
+        ('', _winreg.REG_SZ, 'URL:nxdrive Protocol'),
+        ('URL Protocol', _winreg.REG_SZ, ''),
+    )
+    update_key(reg, nxdrive_class_path, nxdrive_class_attributes)
 
-    comm = _winreg.CreateKey(reg, 'nxdrive\\shell\\open\\command')
-    _winreg.CloseKey(comm)
-
-    comm = _winreg.OpenKey(reg, 'nxdrive\\shell\\open\\command', 0, _winreg.KEY_WRITE)
-    _winreg.SetValueEx(comm,'', 0, _winreg.REG_SZ, command)
-    _winreg.CloseKey(comm)
+    # Create the nxdrive command key
+    command = '"' + exe_path + '" "%1"'
+    command_path = nxdrive_class_path + '\\shell\\open\\command'
+    command_attributes = (
+        ('', _winreg.REG_SZ, command)
+    )
+    update_key(reg, command_path, command_attributes)
