24a6f92a87a3271a06ac83d1bd88b10912cbe4c2
Olivier Grisel <olivier.grisel@ensta.org>
2013-02-01 19:39:37 +0100

    NXP-10414: fix bug in incr change detection on roots

--- nuxeo-drive-client/nxdrive/synchronizer.py
+++ nuxeo-drive-client/nxdrive/synchronizer.py
@@ -778,7 +778,7 @@ class Synchronizer(object):
                     cl = self.get_remote_client_from_docpair(doc_pair)
                     new_info = cl.get_info(remote_ref, raise_if_missing=False)
                     if (old_remote_parent_ref is None
-                        and new_info.parent_uid == old_remote_parent_ref):
+                        or new_info.parent_uid == old_remote_parent_ref):
                         # Perform a regular document update
                         log.debug('Refreshing doc_pair %s', doc_pair.remote_name)
                         doc_pair.update_remote(new_info)
