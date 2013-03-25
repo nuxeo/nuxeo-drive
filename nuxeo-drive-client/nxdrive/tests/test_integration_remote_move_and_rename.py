3c5d158b87e142553f40fbd2034ccd824ff0c5b9
Antoine Taillefer <ataillefer@nuxeo.com>
2013-03-25 15:53:02 +0100

    NXP-11184: Add test on pair states + check complete parent path of renamed files

--- nuxeo-drive-client/nxdrive/tests/test_integration_remote_move_and_rename.py
+++ nuxeo-drive-client/nxdrive/tests/test_integration_remote_move_and_rename.py
@@ -3,6 +3,7 @@ import time
 
 from nxdrive.tests.common import IntegrationTestCase
 from nxdrive.client import LocalClient
+from nxdrive.model import LastKnownState
 
 
 class TestIntegrationRemoteMoveAndRename(IntegrationTestCase):
@@ -29,13 +30,14 @@ class TestIntegrationRemoteMoveAndRename(IntegrationTestCase):
 
         self.controller_1.synchronizer.update_synchronize_server(self.sb_1)
 
-        sync_root_folder_1 = os.path.join(self.local_nxdrive_folder_1,
+        self.sync_root_folder_1 = os.path.join(self.local_nxdrive_folder_1,
                                        self.workspace_title)
-        self.local_client_1 = LocalClient(sync_root_folder_1)
+        self.local_client_1 = LocalClient(self.sync_root_folder_1)
         self.remote_client_1 = self.remote_file_system_client_1
 
         self.workspace_id = ('defaultSyncRootFolderItemFactory#default#'
                             + self.workspace)
+        self.workspace_pair_local_path = '/' + self.workspace_title
 
         self.file_1_id = self.remote_client_1.make_file(self.workspace_id,
             u'Original File 1.txt',
@@ -69,6 +71,7 @@ class TestIntegrationRemoteMoveAndRename(IntegrationTestCase):
         sb, ctl = self.sb_1, self.controller_1
         remote_client = self.remote_client_1
         local_client = self.local_client_1
+        session = ctl.get_session()
 
         # Rename /Original File 1.txt to /Renamed File 1.txt
         remote_client.rename(self.file_1_id, u'Renamed File 1.txt')
@@ -78,10 +81,18 @@ class TestIntegrationRemoteMoveAndRename(IntegrationTestCase):
         time.sleep(self.AUDIT_CHANGE_FINDER_TIME_RESOLUTION)
         self.assertEquals(ctl.synchronizer.update_synchronize_server(sb), 1)
 
+        # Check local file name
         self.assertEquals(remote_client.get_info(self.file_1_id).name,
             u'Renamed File 1.txt')
         self.assertFalse(local_client.exists(u'/Original File 1.txt'))
         self.assertTrue(local_client.exists(u'/Renamed File 1.txt'))
+        # Check file state
+        file_1_state = session.query(LastKnownState).filter_by(
+            remote_name=u'Renamed File 1.txt').one()
+        self.assertEquals(file_1_state.local_path,
+            os.path.join(self.workspace_pair_local_path,
+            u'Renamed File 1.txt'))
+        self.assertEquals(file_1_state.local_name, u'Renamed File 1.txt')
 
         # Rename 'Renamed File 1.txt' to 'Renamed Again File 1.txt'
         # and 'Original File 1.1.txt' to
@@ -101,26 +112,40 @@ class TestIntegrationRemoteMoveAndRename(IntegrationTestCase):
             u'Renamed Again File 1.txt')
         self.assertEquals(remote_client.get_info(self.file_1_1_id).name,
             u'Renamed File 1.1 \xe9.txt')
+        # Check local file names
         self.assertFalse(local_client.exists(u'/Renamed File 1.txt'))
         self.assertTrue(local_client.exists(u'/Renamed Again File 1.txt'))
         self.assertFalse(local_client.exists(
             u'/Original Folder 1/Original File 1.1.txt'))
         self.assertTrue(local_client.exists(
             u'/Original Folder 1/Renamed File 1.1 \xe9.txt'))
+        # Check file states
+        file_1_state = session.query(LastKnownState).filter_by(
+            remote_name=u'Renamed Again File 1.txt').one()
+        self.assertEquals(file_1_state.local_path,
+            os.path.join(self.workspace_pair_local_path,
+            u'Renamed Again File 1.txt'))
+        self.assertEquals(file_1_state.local_name, u'Renamed Again File 1.txt')
+        file_1_1_state = session.query(LastKnownState).filter_by(
+            remote_name=u'Renamed File 1.1 \xe9.txt').one()
+        self.assertEquals(file_1_1_state.local_path,
+            os.path.join(self.workspace_pair_local_path,
+            u'Original Folder 1/Renamed File 1.1 \xe9.txt'))
+        self.assertEquals(file_1_1_state.local_name,
+            u'Renamed File 1.1 \xe9.txt')
 
         # Check parents of renamed files to ensure it is an actual rename
         # that has been performed and not a move
         file_1_local_info = local_client.get_info(
             u'/Renamed Again File 1.txt')
         file_1_parent_path = file_1_local_info.filepath.rsplit('/', 1)[0]
-        self.assertEquals(os.path.basename(file_1_parent_path),
-            self.workspace_title)
+        self.assertEquals(file_1_parent_path, self.sync_root_folder_1)
 
         file_1_1_local_info = local_client.get_info(
             u'/Original Folder 1/Renamed File 1.1 \xe9.txt')
         file_1_1_parent_path = file_1_1_local_info.filepath.rsplit('/', 1)[0]
-        self.assertEquals(os.path.basename(file_1_1_parent_path),
-            u'Original Folder 1')
+        self.assertEquals(file_1_1_parent_path,
+            os.path.join(self.sync_root_folder_1, u'Original Folder 1'))
 
     def test_remote_rename_folder(self):
         sb, ctl = self.sb_1, self.controller_1
