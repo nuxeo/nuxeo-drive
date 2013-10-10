afacbbe5b610137013c7af7b89e85537df8c88cd
Antoine Taillefer <ataillefer@nuxeo.com>
2013-10-10 12:09:56 +0200

    NXP-9612: Windows overlay - WIP

--- nuxeo-drive-client/nxdrive/overlay/win32/pyoverlay.py
+++ nuxeo-drive-client/nxdrive/overlay/win32/pyoverlay.py
@@ -1,7 +1,9 @@
 from win32com.shell import shell, shellcon
+import pythoncom
 import winerror
 import urllib
 import subprocess
+import os
 
 driveRoots = []
 currentFolderUri = None
@@ -9,7 +11,7 @@ syncStatuses = None
 
 
 def nxlog(msg):
-    logFile = open("C:\\driveOverlay.log", "a")
+    logFile = open("C:\\Users\\nuxeo\\driveOverlay.log", "a")
     logFile.write(msg + "\n")
     logFile.close()
 
@@ -37,27 +39,34 @@ class IconOverlay:
     _reg_progid_ = 'NuxeoDrive.PythonOverlayHandler'
     _reg_desc_ = 'Icon Overlay Handler for Nuxeo Drive'
     _public_methods_ = ['GetOverlayInfo', 'GetPriority', 'IsMemberOf']
-    _com_interfaces_ = [shell.IID_IShellIconOverlayIdentifier]
+    _com_interfaces_ = [shell.IID_IShellIconOverlayIdentifier, pythoncom.IID_IDispatch]
 
     def __init__(self):
         nxlog("Starting")
 
-    def get_overlay_info(self):
+    def GetOverlayInfo(self):
         nxlog("return Normal icon")
-        return (r'C:\Drive\NormalIcon.ico', 0, shellcon.ISIOI_ICONFILE)
+        icon_path = os.path.abspath(r'C:\Users\nuxeo\Desktop\nuxeo-drive\nuxeo-drive-client\nxdrive\data\icons\overlay\win32\NormalIcon.ico')
+        nxlog("iconpath = " + icon_path)
+        f = open(icon_path, 'r')
+        nxlog("file opened")
+        f.close()
+        nxlog("file closed")
+        return (icon_path, 0, shellcon.ISIOI_ICONFILE)
 
-    def get_priority(self):
-        return 50
+    def GetPriority(self):
+        nxlog("calling IconOverlay.GetPriority")
+        return 0
 
     def IsMemberOf(self, fname, attributes):
         nxlog("calling isMember on " + fname)
-        if ("nuxeo" in fname):
+        if ("Nuxeo" in fname):
             if ("finan" in fname):
-                return winerror.S_FALSE
+                return winerror.E_FAIL
             else:
                 nxlog("return ok for synced")
                 return winerror.S_OK
-        return winerror.S_FALSE
+        return winerror.E_FAIL
 
 
 class PendingIconOverlay:
@@ -66,41 +75,101 @@ class PendingIconOverlay:
     _reg_progid_ = 'NuxeoDrive.PythonPendingOverlayHandler'
     _reg_desc_ = 'Icon Overlay Handler for Nuxeo Drive Pending files'
     _public_methods_ = ['GetOverlayInfo', 'GetPriority', 'IsMemberOf']
-    _com_interfaces_ = [shell.IID_IShellIconOverlayIdentifier]
+    _com_interfaces_ = [shell.IID_IShellIconOverlayIdentifier, pythoncom.IID_IDispatch]
 
     def __init__(self):
         nxlog("Starting")
 
     def GetOverlayInfo(self):
         nxlog("return Modified icon")
-        return (r'C:\Drive\ModifiedIcon.ico', 0, shellcon.ISIOI_ICONFILE)
+        icon_path = os.path.abspath(r'C:\Users\nuxeo\Desktop\nuxeo-drive\nuxeo-drive-client\nxdrive\data\icons\overlay\win32\ModifiedIcon.ico')
+        nxlog("iconpath = " + icon_path)
+        f = open(icon_path, 'r')
+        nxlog("file opened")
+        f.close()
+        nxlog("file closed")
+        return (icon_path, 0, shellcon.ISIOI_ICONFILE)		
 
     def GetPriority(self):
-        return 50
+        nxlog("calling PendingIconOverlay.GetPriority")
+        return 0
 
     def IsMemberOf(self, fname, attributes):
         nxlog("calling isMember on " + fname)
-        if ("nuxeo" in fname):
+        if ("Nuxeo" in fname):
             if ("finan" in fname):
                 nxlog("return ok for pending")
                 return winerror.S_OK
             else:
-                return winerror.S_FALSE
-        return winerror.S_FALSE
+                return winerror.E_FAIL
+        return winerror.E_FAIL
 
 
-if __name__ == '__main__':
-    import win32api
-    import win32con
-    import win32com.server.register
+# if __name__ == '__main__':
+    # import win32api
+    # import win32con
+    # import win32com.server.register
 
-    win32com.server.register.UseCommandLine(IconOverlay)
-    keyname = r'Software\Microsoft\Windows\CurrentVersion\Explorer\ShellIconOverlayIdentifiers\NuxeoDriveOverlay'
-    key = win32api.RegCreateKey(win32con.HKEY_LOCAL_MACHINE, keyname)
-    win32api.RegSetValue(key, None, win32con.REG_SZ, IconOverlay._reg_clsid_)
+    # win32com.server.register.UseCommandLine(IconOverlay)
+    # keyname = r'Software\Microsoft\Windows\CurrentVersion\Explorer\ShellIconOverlayIdentifiers\NuxeoDriveOverlay'
+    # key = win32api.RegCreateKey(win32con.HKEY_LOCAL_MACHINE, keyname)
+    # win32api.RegSetValue(key, None, win32con.REG_SZ, IconOverlay._reg_clsid_)
 
-    win32com.server.register.UseCommandLine(PendingIconOverlay)
-    keyname = r'Software\Microsoft\Windows\CurrentVersion\Explorer\ShellIconOverlayIdentifiers\NuxeoDrivePendingOverlay'
-    key = win32api.RegCreateKey(win32con.HKEY_LOCAL_MACHINE, keyname)
-    win32api.RegSetValue(key, None, win32con.REG_SZ,
-                         PendingIconOverlay._reg_clsid_)
+    # win32com.server.register.UseCommandLine(PendingIconOverlay)
+    # keyname = r'Software\Microsoft\Windows\CurrentVersion\Explorer\ShellIconOverlayIdentifiers\NuxeoDrivePendingOverlay'
+    # key = win32api.RegCreateKey(win32con.HKEY_LOCAL_MACHINE, keyname)
+    # win32api.RegSetValue(key, None, win32con.REG_SZ,
+                         # PendingIconOverlay._reg_clsid_)
+
+REG_PATH =r'Software\Microsoft\Windows\CurrentVersion\Explorer\ShellIconOverlayIdentifiers'
+REG_KEY = "NuxeoDriveOverlay"
+REG_KEY2 = "NuxeoDrivePendingOverlay"
+						 
+def DllRegisterServer():
+    print "Registering %s" % REG_KEY
+    import _winreg
+
+    key = _winreg.CreateKey(_winreg.HKEY_LOCAL_MACHINE, REG_PATH)
+    subkey = _winreg.CreateKey(key, IconOverlay._reg_progid_)
+    _winreg.SetValueEx(subkey, None, 0, _winreg.REG_SZ, IconOverlay._reg_clsid_)
+    print "Registration complete: %s" % IconOverlay._reg_desc_
+
+def DllUnregisterServer():
+    print "Unregistering %s" % REG_KEY
+    import _winreg
+    try:
+        key = _winreg.DeleteKey(_winreg.HKEY_LOCAL_MACHINE, r"%s\%s" % (REG_PATH, IconOverlay._reg_progid_))
+    except WindowsError, details:
+        import errno
+        if details.errno != errno.ENOENT:
+            raise
+    print "Unregistration complete: %s" % IconOverlay._reg_desc_
+
+def DllRegisterServer2():
+    print "Registering %s" % REG_KEY2
+    import _winreg
+
+    key = _winreg.CreateKey(_winreg.HKEY_LOCAL_MACHINE, REG_PATH)
+    subkey = _winreg.CreateKey(key, PendingIconOverlay._reg_progid_)
+    _winreg.SetValueEx(subkey, None, 0, _winreg.REG_SZ, PendingIconOverlay._reg_clsid_)
+    print "Registration complete: %s" % PendingIconOverlay._reg_desc_
+
+def DllUnregisterServer2():
+    print "Unregistering %s" % REG_KEY2
+    import _winreg
+    try:        
+		key = _winreg.DeleteKey(_winreg.HKEY_LOCAL_MACHINE, r"%s\%s" % (REG_PATH, PendingIconOverlay._reg_progid_))
+    except WindowsError, details:
+        import errno
+        if details.errno != errno.ENOENT:
+            raise
+    print "Unregistration complete: %s" % PendingIconOverlay._reg_desc_
+	
+if __name__=='__main__':
+    from win32com.server import register
+    register.UseCommandLine(IconOverlay,
+                            finalize_register = DllRegisterServer,
+                            finalize_unregister = DllUnregisterServer)
+    register.UseCommandLine(PendingIconOverlay,
+                            finalize_register = DllRegisterServer2,
+                            finalize_unregister = DllUnregisterServer2)
