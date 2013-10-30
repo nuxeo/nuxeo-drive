626cd328942d7718f0e648858dd5f98db5e27170
Antoine Taillefer <ataillefer@nuxeo.com>
2013-10-30 16:19:36 +0100

    NXP-11587: Cleanup permission hierarchy test: always delete user workspace and deactivate permission profile

--- nuxeo-drive-client/nxdrive/tests/test_integration_permission_hierarchy.py
+++ nuxeo-drive-client/nxdrive/tests/test_integration_permission_hierarchy.py
@@ -8,69 +8,80 @@ from nxdrive.client import RemoteDocumentClient
 class TestIntegrationPermissionHierarchy(IntegrationTestCase):
 
     def test_sync_delete_root(self):
+        try:
+            # Get remote and local clients
+            admin_remote_client = self.root_remote_client
+            user_remote_client = RemoteDocumentClient(
+                self.nuxeo_url, self.user_1, u'nxdrive-test-device-1',
+                self.version, password=self.password_1,
+                upload_tmp_dir=self.upload_tmp_dir)
+            local_client = LocalClient(self.local_nxdrive_folder_1)
 
-        # Bind server
-        ctl = self.controller_1
-        ctl.bind_server(self.local_nxdrive_folder_1, self.nuxeo_url,
-                        self.user_1, self.password_1)
-        syn = ctl.synchronizer
+            # Activate permission hierarchy profile as Administrator
+            admin_remote_client.activate_profile('permission')
 
-        # Get remote and local clients
-        admin_remote_client = self.root_remote_client
-        user_remote_client = RemoteDocumentClient(
-            self.nuxeo_url, self.user_1, u'nxdrive-test-device-1',
-            self.version, password=self.password_1,
-            upload_tmp_dir=self.upload_tmp_dir)
-        local_client = LocalClient(self.local_nxdrive_folder_1)
+            # Bind server
+            ctl = self.controller_1
+            ctl.bind_server(self.local_nxdrive_folder_1, self.nuxeo_url,
+                            self.user_1, self.password_1)
+            syn = ctl.synchronizer
 
-        # Activate permission hierarchy profile as Administrator
-        admin_remote_client.activate_profile('permission')
+            # Create user workspace parent as Administrator if it doesn't exist
+            user_workspaces_path = '/default-domain/UserWorkspaces/'
+            if not admin_remote_client.exists(user_workspaces_path):
+                admin_remote_client.make_folder('/default-domain',
+                                                'UserWorkspaces',
+                                                doc_type='UserWorkspacesRoot')
 
-        # Create test user workspace as Administrator
-        user_workspaces_path = '/default-domain/UserWorkspaces/'
-        user_workspace_title = 'nuxeoDriveTestUser_user_1'
-        admin_remote_client.make_folder(user_workspaces_path,
-                                       user_workspace_title,
-                                       doc_type='Workspace')
-        user_workspace_path = user_workspaces_path + user_workspace_title
-        # Grant ReadWrite permission to test user on its workspace
-        op_input = "doc:" + user_workspace_path
-        admin_remote_client.execute("Document.SetACE",
-            op_input=op_input,
-            user="nuxeoDriveTestUser_user_1",
-            permission="ReadWrite",
-            grant="true")
+            # Create test user workspace as Administrator
+            user_workspace_title = 'nuxeoDriveTestUser_user_1'
+            admin_remote_client.make_folder(user_workspaces_path,
+                                           user_workspace_title,
+                                           doc_type='Workspace')
+            user_workspace_path = user_workspaces_path + user_workspace_title
+            # Grant ReadWrite permission to test user on its workspace
+            op_input = "doc:" + user_workspace_path
+            admin_remote_client.execute("Document.SetACE",
+                op_input=op_input,
+                user="nuxeoDriveTestUser_user_1",
+                permission="ReadWrite",
+                grant="true")
 
-        # Create test folder in user workspace as test user
-        user_remote_client.make_folder(user_workspace_path, 'test_folder')
-        test_folder_path = user_workspace_path + '/test_folder'
-        # Create a document in the test folder
-        user_remote_client.make_file(test_folder_path, 'test_file.txt',
-                                    "Some content.")
+            # Create test folder in user workspace as test user
+            user_remote_client.make_folder(user_workspace_path, 'test_folder')
+            test_folder_path = user_workspace_path + '/test_folder'
+            # Create a document in the test folder
+            user_remote_client.make_file(test_folder_path, 'test_file.txt',
+                                        "Some content.")
 
-        # Register test folder as a sync root
-        user_remote_client.register_as_root(test_folder_path)
+            # Register test folder as a sync root
+            user_remote_client.register_as_root(test_folder_path)
 
-        # Synchronize
-        self._synchronize(syn)
+            # Synchronize
+            self._synchronize(syn)
 
-        # Check locally synchronized content
-        self.assertTrue(local_client.exists('/My Docs/test_folder'))
-        self.assertTrue(local_client.exists(
+            # Check locally synchronized content
+            self.assertTrue(local_client.exists('/My Docs/test_folder'))
+            self.assertTrue(local_client.exists(
                                         '/My Docs/test_folder/test_file.txt'))
 
-        # Delete test folder
-        user_remote_client.delete(test_folder_path)
+            # Delete test folder
+            user_remote_client.delete(test_folder_path)
 
-        # Synchronize
-        self._synchronize(syn)
+            # Synchronize
+            self._synchronize(syn)
 
-        # Check locally synchronized content
-        self.assertFalse(local_client.exists('/My Docs/test_folder'))
-        self.assertEquals(len(local_client.get_children_info('/My Docs')), 0)
-
-        # Cleanup
-        admin_remote_client.delete(user_workspace_path, use_trash=False)
+            # Check locally synchronized content
+            self.assertFalse(local_client.exists('/My Docs/test_folder'))
+            self.assertEquals(len(local_client.get_children_info('/My Docs')),
+                              0)
+        finally:
+            # Cleanup user workspace
+            if admin_remote_client.exists(user_workspace_path):
+                admin_remote_client.delete(user_workspace_path,
+                                           use_trash=False)
+            # Deactivate permission hierarchy profile
+            admin_remote_client.deactivate_profile('permission')
 
     def _synchronize(self, synchronizer):
         time.sleep(self.AUDIT_CHANGE_FINDER_TIME_RESOLUTION)
