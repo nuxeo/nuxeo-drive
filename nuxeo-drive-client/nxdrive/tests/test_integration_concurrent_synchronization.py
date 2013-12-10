f9273a9c3f9c6dbaa9ca93b9b30603b5a3e918aa
Antoine Taillefer <ataillefer@nuxeo.com>
2013-12-10 16:26:24 +0100

    NXP-13364: Don't consider metadata update (other than title) on a remote folder as a remote modification

--- nuxeo-drive-client/nxdrive/tests/test_integration_concurrent_synchronization.py
+++ nuxeo-drive-client/nxdrive/tests/test_integration_concurrent_synchronization.py
@@ -1,4 +1,5 @@
 import os
+import time
 from threading import Thread
 
 from nxdrive.tests.common import IntegrationTestCase
@@ -71,3 +72,69 @@ class TestIntegrationConcurrentSynchronization(IntegrationTestCase):
             c.name for c in local_client.get_children_info(u'/')]
         local_children_names.sort()
         self.assertEquals(local_children_names, children_names)
+
+    def test_delete_local_folder_update_remote_folder_property(self):
+
+        # Get local and remote clients
+        local = LocalClient(self.local_nxdrive_folder_1)
+        remote = self.remote_document_client_1
+
+        # Bind server and test workspace for nuxeoDriveTestUser_user_1
+        ctl = self.controller_1
+        ctl.bind_server(self.local_nxdrive_folder_1, self.nuxeo_url,
+                        self.user_1, self.password_1)
+        ctl.bind_root(self.local_nxdrive_folder_1, self.workspace)
+
+        # Launch first synchronization
+        time.sleep(self.AUDIT_CHANGE_FINDER_TIME_RESOLUTION)
+        self.wait()
+        sync = ctl.synchronizer
+        sync.loop(delay=0.1, max_loops=1)
+
+        # Test workspace should be created locally
+        self.assertTrue(local.exists('/Nuxeo Drive Test Workspace'))
+
+        # Create a local folder in the test workspace and a file inside
+        # this folder, then synchronize
+        local.make_folder('/Nuxeo Drive Test Workspace', 'Test folder')
+        local.make_file('/Nuxeo Drive Test Workspace/Test folder',
+                        'test.odt', 'Some content.')
+
+        sync.loop(delay=0.1, max_loops=1)
+
+        # Test folder should be created remotely in the test workspace
+        self.assertTrue(remote.exists('/Test folder'))
+        self.assertTrue(remote.exists('/Test folder/test.odt'))
+
+        # Delete Test folder locally and remotely update one of its property
+        # concurrently, then synchronize
+        local.delete('/Nuxeo Drive Test Workspace/Test folder')
+        self.assertFalse(local.exists(
+                                    '/Nuxeo Drive Test Workspace/Test folder'))
+        test_folder_ref = remote._check_ref('/Test folder')
+        # Wait for 1 second to make sure the folder's last modification time
+        # is different from the pair state's last remote update time
+        time.sleep(1.0)
+        remote.update(test_folder_ref,
+                      properties={'dc:description': 'Some description.'})
+        test_folder = remote.fetch(test_folder_ref)
+        self.assertEqual(test_folder['properties']['dc:description'],
+                         'Some description.')
+
+        time.sleep(self.AUDIT_CHANGE_FINDER_TIME_RESOLUTION)
+        self.wait()
+        sync.loop(delay=0.1, max_loops=1)
+
+        # Test folder should be deleted remotely in the test workspace.
+        # Even though fetching the remote changes will send a
+        # 'documentModified' event for Test folder as a result of its
+        # dc:description property update, since the folder will not have been
+        # renamed nor moved since last synchronization, its pair state will
+        # not be marked as 'modified', see Model.update_remote().
+        # Thus the pair state will be ('deleted', 'synchronized'), resolved as
+        # 'locally_deleted'.
+        self.assertFalse(remote.exists('/Test folder'))
+
+        # Check Test folder has not been re-created locally
+        self.assertFalse(local.exists(
+                                    '/Nuxeo Drive Test Workspace/Test folder'))
