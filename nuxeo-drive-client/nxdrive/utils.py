ec44872c46d549255075ebe5603e39fa05015f3e
Antoine Taillefer <ataillefer@nuxeo.com>
2014-01-16 11:31:04 +0100

    Add deprecated decorative function to utils and mark BaseAutomationClient.execute_with_blob as deprecated

--- nuxeo-drive-client/nxdrive/utils.py
+++ nuxeo-drive-client/nxdrive/utils.py
@@ -110,3 +110,17 @@ def _lazysecret(secret, blocksize=32, padding='}'):
     if not len(secret) in (16, 24, 32):
         return secret + (blocksize - len(secret)) * padding
     return secret
+
+
+def deprecated(func):
+    """"This is a decorator which can be used to mark functions
+    as deprecated. It will result in a warning being emitted
+    when the function is used."""
+    def new_func(*args, **kwargs):
+        log.warning("Call to deprecated function {}.".format(func.__name__),
+                    category=DeprecationWarning)
+        return func(*args, **kwargs)
+    new_func.__name__ = func.__name__
+    new_func.__doc__ = func.__doc__
+    new_func.__dict__.update(func.__dict__)
+    return new_func
