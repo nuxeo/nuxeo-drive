83471303fd9b1af95824074ef921315b3bcb46a2
Antoine Taillefer <ataillefer@nuxeo.com>
2014-01-24 13:51:54 +0100

    NXP-12981: Sign application bundle with 'Nuxeo Drive' identity and verify code signature in dmg creation script

--- tools/osx/create-dmg.sh
+++ tools/osx/create-dmg.sh
@@ -8,8 +8,16 @@ BACKGROUND_FILE="$SCRIPT_LOCATION/dmgbackground.png"
 GENERATED_DS_STORE="$SCRIPT_LOCATION/generated_DS_Store"
 
 PACKAGE_PATH="$SCRIPT_LOCATION/../../dist/Nuxeo Drive.app"
+SIGNING_IDENTITY="Nuxeo Drive"
 DMG_PATH="$SCRIPT_LOCATION/../../dist/Nuxeo Drive.dmg"
 DMG_SIZE=$(($(du -sm "$PACKAGE_PATH" | cut -d$'\t' -f1,1)+20))m
+
+# Sign application bundle
+codesign -s "$SIGNING_IDENTITY" "$PACKAGE_PATH" -v
+# Verify code signature
+codesign -vv "$PACKAGE_PATH"
+codesign -d -vvv "$PACKAGE_PATH"
+spctl --assess --type execute "$PACKAGE_PATH" --verbose
 
 rm -rf "$SRC_FOLDER_TEMP" "$DMG_TEMP"
 mkdir "$SRC_FOLDER_TEMP"
