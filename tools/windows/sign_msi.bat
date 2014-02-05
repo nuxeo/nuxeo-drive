e743cc1f950b86d96741c2330abbdac5186ff46b
Antoine Taillefer <ataillefer@nuxeo.com>
2014-02-05 15:23:11 +0100

    NXP-12981: Add PFX certificate password option when signing msi

--- tools/windows/sign_msi.bat
+++ tools/windows/sign_msi.bat
@@ -2,10 +2,11 @@
 
 set DIST_DIR=dist
 set CERTIFICATE_PATH="%USERPROFILE%\certificates\nuxeo.com.pfx"
+set PFX_PASSWORD=""
 set MSI_PROGRAM_NAME="Nuxeo Drive"
 set TIMESTAMP_URL=http://timestamp.verisign.com/scripts/timstamp.dll
 
-set SIGN_CMD=signtool sign /v /f %CERTIFICATE_PATH% /d %MSI_PROGRAM_NAME% /t %TIMESTAMP_URL%
+set SIGN_CMD=signtool sign /v /f %CERTIFICATE_PATH% /p %PFX_PASSWORD% /d %MSI_PROGRAM_NAME% /t %TIMESTAMP_URL%
 set VERIFY_CMD=signtool verify /v /pa
 
 FOR %%F IN (%DIST_DIR%\*.msi) DO (
