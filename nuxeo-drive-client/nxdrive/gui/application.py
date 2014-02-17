9db6e1910a89e4044286399f54c112b4276f6529
Antoine Taillefer <ataillefer@nuxeo.com>
2014-02-17 17:58:40 +0100

    NXP-13710: Fix comment

--- nuxeo-drive-client/nxdrive/gui/application.py
+++ nuxeo-drive-client/nxdrive/gui/application.py
@@ -501,7 +501,7 @@ class Application(QApplication):
         if self.sync_thread is None or not self.sync_thread.isAlive():
             delay = getattr(self.options, 'delay', 5.0)
             max_sync_step = getattr(self.options, 'max_sync_step', 10)
-            # Controller and its database session pool should be thread safe,
+            # Controller and its database session pool are thread safe,
             # hence reuse it directly
             self.controller.synchronizer.register_frontend(self)
             self.controller.synchronizer.delay = delay
