175654fe6e08cc36201885af3ddcf450a2897e5a
Antoine Taillefer <ataillefer@nuxeo.com>
2014-03-24 15:31:48 +0100

    NXP-14088: Test a simulated "IOError: No space left on device"

--- nuxeo-drive-client/nxdrive/commandline.py
+++ nuxeo-drive-client/nxdrive/commandline.py
@@ -455,6 +455,7 @@ class CliHandler(object):
             "nxdrive.tests.test_integration_encoding",
             "nxdrive.tests.test_integration_local_client",
             "nxdrive.tests.test_integration_local_move_and_rename",
+            "nxdrive.tests.test_integration_local_storage_space_issue",
             "nxdrive.tests.test_integration_permission_hierarchy",
             "nxdrive.tests.test_integration_reinit_database",
             "nxdrive.tests.test_integration_remote_changes",
