f6134a39fa3047145b15798d8dd0fd265ffd2fb2
Antoine Taillefer <ataillefer@nuxeo.com>
2014-04-10 15:24:17 +0200

    NXP-11660: Move mime type guesser to utils module and add relevant test
    Conflicts:
    
    	nuxeo-drive-client/nxdrive/client/base_automation_client.py

--- nuxeo-drive-client/nxdrive/commandline.py
+++ nuxeo-drive-client/nxdrive/commandline.py
@@ -476,6 +476,7 @@ class CliHandler(object):
             "nxdrive.tests.test_integration_versioning",
             "nxdrive.tests.test_integration_windows",
             "nxdrive.tests.test_synchronizer",
+            "nxdrive.tests.test_utils",
         ]
         return 0 if nose.run(argv=argv) else 1
 
