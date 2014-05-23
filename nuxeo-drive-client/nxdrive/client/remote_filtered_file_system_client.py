e263a6250ed4ae021bae67ae0a18d96aa89086a9
Antoine Taillefer <ataillefer@nuxeo.com>
2014-05-23 19:10:43 +0200

    NXP-14448: Apply pep8 to remote_filtered_file_system_client.py

--- nuxeo-drive-client/nxdrive/client/remote_filtered_file_system_client.py
+++ nuxeo-drive-client/nxdrive/client/remote_filtered_file_system_client.py
@@ -8,7 +8,8 @@ from nxdrive.model import Filter
 from nxdrive.logging_config import get_logger
 
 log = get_logger(__name__)
- 
+
+
 class RemoteFilteredFileSystemClient(RemoteFileSystemClient):
     '''
     classdocs
@@ -23,29 +24,36 @@ class RemoteFilteredFileSystemClient(RemoteFileSystemClient):
         '''
         Constructor
         '''
-        super(RemoteFilteredFileSystemClient,self).__init__(server_url, user_id, device_id,
-                                                             client_version, proxies, proxy_exceptions,
-                                                             password, token, repository, ignored_prefixes,
-                                                             ignored_suffixes, timeout, blob_timeout, cookie_jar,
-                                                             upload_tmp_dir, check_suspended)
+        super(RemoteFilteredFileSystemClient, self).__init__(
+            server_url, user_id, device_id,
+            client_version, proxies, proxy_exceptions,
+            password, token, repository, ignored_prefixes,
+            ignored_suffixes, timeout, blob_timeout, cookie_jar,
+            upload_tmp_dir, check_suspended)
         self.session = session
-        
+
     def get_children_info(self, fs_item_id):
-        result = super(RemoteFilteredFileSystemClient,self).get_children_info(fs_item_id)
+        result = super(RemoteFilteredFileSystemClient, self).get_children_info(
+                                                                    fs_item_id)
         # Need to filter the children result
         filtered = []
         for item in result:
             if not Filter.is_filter(self.session, None, item.path):
                 filtered.append(item)
             else:
-                log.debug("Filtering item %r", item )
+                log.debug("Filtering item %r", item)
         return filtered
-        
+
     def get_changes(self, server_binding):
-        result = super(RemoteFilteredFileSystemClient,self).get_changes(server_binding)
+        result = super(RemoteFilteredFileSystemClient, self).get_changes(
+                                                                server_binding)
         # Need to filter the result and add filterevents
-        # Force the scan of the server every time now, need to compare to last date filter
-        # Need to multiply the last_filter_date by 1000 as the last_sync_date come from nuxeo
-        if server_binding.last_filter_date and server_binding.last_sync_date < server_binding.last_filter_date * 1000:
+        # Force the scan of the server every time now, need to compare to last
+        # date filter
+        # Need to multiply the last_filter_date by 1000 as the last_sync_date
+        # come from nuxeo
+        if (server_binding.last_filter_date
+            and server_binding.last_sync_date < (
+                                    server_binding.last_filter_date * 1000)):
             result['hasTooManyChanges'] = True
         return result
