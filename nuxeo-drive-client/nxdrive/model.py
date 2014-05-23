56ec23487f15a73874a4e14a5c4b76097d6ea1a2
Antoine Taillefer <ataillefer@nuxeo.com>
2014-05-23 18:53:16 +0200

    NXP-14448: Apply pep8 to model.py

--- nuxeo-drive-client/nxdrive/model.py
+++ nuxeo-drive-client/nxdrive/model.py
@@ -526,35 +526,36 @@ class FileEvent(Base):
         if utc_time is None:
             utc_time = datetime.utcnow()
 
+
 class Filter(Base):
     __tablename__ = 'filters'
-    
+
     path = Column(String, primary_key=True)
     local_folder = Column(String, ForeignKey('server_bindings.local_folder'))
-    
+
     server_binding = relationship("ServerBinding")
-    
+
     def __init__(self, server_binding, path):
         if server_binding:
             self.local_folder = server_binding.local_folder
         else:
             self.local_folder = None
         self.path = Filter.clean_path(path)
-    
+
     def __repr__(self):
         return self.path
-    
+
     @staticmethod
-    def is_filter(session, server_binding, path, filters = None):
+    def is_filter(session, server_binding, path, filters=None):
         path = Filter.clean_path(path)
         if filters is None:
             filters = session.query(Filter).all()
         # Not the best way now need to move to count
-        if any([path.startswith(filter_obj.path) for filter_obj in filters]): 
+        if any([path.startswith(filter_obj.path) for filter_obj in filters]):
             return True
         else:
             return False
-        
+
     @staticmethod
     def add(session, server_binding, path):
         path = Filter.clean_path(path)
@@ -562,10 +563,11 @@ class Filter(Base):
             # Skip it as it is already filtered
             return
         # Remove any subfolders
-        filters = session.query(Filter).filter(Filter.path.like(path+'%')).all()
+        filters = session.query(Filter).filter(
+                                            Filter.path.like(path + '%')).all()
         [session.delete(filter_obj) for filter_obj in filters]
         # Add the filter now
-        filter_obj = Filter(server_binding,path)
+        filter_obj = Filter(server_binding, path)
         session.add(filter_obj)
         server_binding.last_filter_date = time()
         session.commit()
@@ -573,23 +575,24 @@ class Filter(Base):
     @staticmethod
     def getAll(session, server_binding):
         return session.query(Filter).all()
-    
+
     @staticmethod
     def clean_path(path):
         if not path.endswith("/"):
             path = path + "/"
         return path
-    
+
     @staticmethod
     def remove(session, server_binding, path):
         path = Filter.clean_path(path)
-        filters = session.query(Filter).filter(Filter.path.like(path+'%')).all()
+        filters = session.query(Filter).filter(
+                                            Filter.path.like(path + '%')).all()
         if len(filters) == 0:
             # Non existing filter
             return
         [session.delete(filter_obj) for filter_obj in filters]
         server_binding.last_filter_date = time()
-        session.commit() 
+        session.commit()
 
 
 def init_db(nxdrive_home, echo=False, echo_pool=False, scoped_sessions=True,
