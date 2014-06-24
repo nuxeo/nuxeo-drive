a092eb527a77a108beaf9cd44603dc67a425c79c
Antoine Taillefer <ataillefer@nuxeo.com>
2014-06-24 17:21:24 +0200

    NXDRIVE-39: Add simple test using Controller.trash_modified_file = lambda: True

--- nuxeo-drive-client/nxdrive/tests/test_integration_remote_deleted_parent.py
+++ nuxeo-drive-client/nxdrive/tests/test_integration_remote_deleted_parent.py
@@ -51,3 +51,41 @@ class TestIntegrationRemoteDeletion(IntegrationTestCase):
         # Error counter should be in place
         self._synchronize(syn)
         self.assertFalse(local.exists('/Test folder'))
+
+    def test_trash_modified_file(self):
+        """Test deleting a remote folder while a file is locally created in it.
+
+        See https://jira.nuxeo.com/browse/NXDRIVE-39
+        See TestIntegrationRemoteDeletion.test_synchronize_remote_deletion_local_modification
+        as the same use case is tested.
+        """
+        ctl = self.controller_1
+        # Override the behavior to force use of trash
+        ctl.trash_modified_file = lambda: True
+
+        # Bind the server and root workspace
+        ctl.bind_server(self.local_nxdrive_folder_1, self.nuxeo_url,
+                        self.user_1, self.password_1)
+        ctl.bind_root(self.local_nxdrive_folder_1, self.workspace)
+
+        # Get local and remote clients
+        local = LocalClient(os.path.join(self.local_nxdrive_folder_1,
+                                         self.workspace_title))
+        remote = self.remote_document_client_1
+
+        syn = ctl.synchronizer
+
+        # Create a remote folder then synchronize
+        remote.make_folder('/', 'Test folder')
+        self._synchronize(syn)
+        self.assertTrue(remote.exists('/Test folder'))
+        self.assertTrue(local.exists('/Test folder'))
+
+        # Delete remote folder and create files in the local one then
+        # synchronize
+        remote.delete('/Test folder')
+        local.make_file('/Test folder', 'joe.txt', 'My name is Joe.')
+        self._synchronize(syn)
+
+        self.assertFalse(remote.exists('/Test folder'))
+        self.assertFalse(local.exists('/Test folder'))
