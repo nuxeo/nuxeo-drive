637d6efa704a3cc366e8740b26ec8fd88fd71137
Antoine Taillefer <ataillefer@nuxeo.com>
2014-06-25 15:03:09 +0200

    NXDRIVE-40: Allow to not remove esky freeze output directory after zipping it

--- nx_esky/__init__.py
+++ nx_esky/__init__.py
@@ -5,17 +5,25 @@ creating a zip file of the esky freeze.
 """
 
 import sys
+import os
 from esky.bdist_esky import bdist_esky as e_bdist_esky
+from esky.util import get_platform
+from esky.util import create_zipfile
+from esky.util import really_rmtree
 
 
 class bdist_esky(e_bdist_esky):
 
     e_bdist_esky.user_options.append(
         ('create-zipfile=', None, "create zip file from esky freeze"))
+    e_bdist_esky.user_options.append(
+        ('rm-freeze-dir-after-zipping=', None, "remove esky freeze directory"
+         " after zipping it"))
 
     def initialize_options(self):
         e_bdist_esky.initialize_options(self)
         self.create_zipfile = False
+        self.rm_freeze_dir_after_zipping = True
 
     def _run(self):
         self._run_initialise_dirs()
@@ -27,6 +35,21 @@ class bdist_esky(e_bdist_esky):
         # Only create zip file from esky freeze if option is passed
         if self.create_zipfile:
             self._run_create_zipfile()
+
+    def _run_create_zipfile(self):
+        """Zip up the final distribution."""
+        print "zipping up the esky"
+        fullname = self.distribution.get_fullname()
+        platform = get_platform()
+        zfname = os.path.join(self.dist_dir,
+                              "%s.%s.zip" % (fullname, platform, ))
+        if hasattr(self.freezer_module, "zipit"):
+            self.freezer_module.zipit(self, self.bootstrap_dir, zfname)
+        else:
+            create_zipfile(self.bootstrap_dir, zfname, compress=True)
+        # Only remove bootstrap dir if option is passed
+        if self.rm_freeze_dir_after_zipping:
+            really_rmtree(self.bootstrap_dir)
 
 # Monkey-patch distutils to override bdist_esky command included in
 # esky.bdist_esky.
