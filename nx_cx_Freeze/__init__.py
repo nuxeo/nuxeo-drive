30952c86dcc4cb2ed2192d00e8677b416364aabe
Remi Cattiau <rcattiau@loopingz.com>
2014-08-26 23:20:03 +0100

    NXDRIVE-74: Add installer launch checkbox

--- nx_cx_Freeze/__init__.py
+++ nx_cx_Freeze/__init__.py
@@ -16,7 +16,7 @@ from cx_Freeze.dist import build as cx_build
 from cx_Freeze.dist import install as cx_install
 from cx_Freeze.dist import setup as cx_setup
 from cx_Freeze.dist import _AddCommandClass
-
+from cx_Freeze.windist import bdist_msi as cx_bdist_msi
 
 class build(cx_build):
 
@@ -33,6 +33,32 @@ class build(cx_build):
             subCommands.append(self.exe_command)
         return subCommands
 
+
+class bdist_msi(cx_bdist_msi):
+    def get_executable(self):
+        return "ndrivew.exe"
+        
+    def add_exit_dialog(self):
+        import msilib
+        dialog = distutils.command.bdist_msi.PyDialog(self.db, "ExitDialog",
+                self.x, self.y, self.width, self.height, self.modal,
+                self.title, "Finish", "Finish", "Finish")
+        dialog.title("Completing the [ProductName]")
+        dialog.back("< Back", "Finish", active = False)
+        dialog.cancel("Cancel", "Back", active = False)
+        dialog.text("Description", 15, 235, 320, 20, 0x30003,
+                "Click the Finish button to exit the installer.")
+        button = dialog.next("Finish", "Cancel", name = "Finish")
+        button.event("EndDialog", "Return")
+        msilib.add_data(self.db, "Property",
+                 # See "DefaultUIFont Property"
+                 [("StartClient", "1")])
+        c = dialog.control("LaunchAfterInstall", "CheckBox", 15, 200, 320, 20, 0x3,
+				"StartClient", "Launch [ProductName]", None, None)
+        c.condition("Hide",'Progress1<>"Install"')
+        # 18 is for execute a .exe from install
+        msilib.add_data(self.db, "CustomAction", [("LaunchNuxeoDrive", 18, "launcher.exe", self.get_executable())])
+        button.event("DoAction","LaunchNuxeoDrive",'StartClient=1 and Progress1="Install"')
 
 class install(cx_install):
 
@@ -59,4 +85,5 @@ def setup(**attrs):
     commandClasses = attrs.setdefault("cmdclass", {})
     _AddCommandClass(commandClasses, "build", build)
     _AddCommandClass(commandClasses, "install", install)
+    _AddCommandClass(commandClasses, "bdist_msi", bdist_msi)
     cx_setup(**attrs)
