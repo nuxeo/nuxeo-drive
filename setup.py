d84c106b457bc1a0ba5df70566bce8b8a5810519
Remi Cattiau <rcattiau@loopingz.com>
2014-08-26 23:19:01 +0100

    NXDRIVE-74: Add launcher

--- setup.py
+++ setup.py
@@ -131,7 +131,7 @@ class NuxeoDriveAttributes(object):
         return os.path.join(self.rubric_2nd_dir(), 'scripts', 'ndrive')
 
     def get_scripts(self):
-        return [es_Executable(self.get_script())]
+        return [es_Executable(self.get_script()), 'launcher.py']
 
     def get_win_script(self):
         return os.path.join(self.rubric_2nd_dir(), 'scripts', 'ndrivew.pyw')
@@ -422,7 +422,6 @@ class NuxeoDriveSetup(object):
                     )
                 )
             )
-
         setup(
             name=name,
             version=version,
