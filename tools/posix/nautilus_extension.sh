0d8d022674a648cc3408aaff789095ec94bf8b15
Antoine Taillefer <ataillefer@nuxeo.com>
2014-10-17 15:49:00 +0200

    NXDRIVE-51: Add contextual_menu package for metadata edit related code

--- tools/posix/nautilus_extension.sh
+++ tools/posix/nautilus_extension.sh
@@ -5,7 +5,7 @@ cp nuxeo-drive-client/nxdrive/overlay/nautilus/file_info_updater.py ~/.local/sha
 cp nuxeo-drive-client/nxdrive/data/icons/overlay/nautilus/* ~/.icons/hicolor/48x48/emblems/.
 
 # Contextual menu
-cp nuxeo-drive-client/nxdrive/overlay/nautilus/metadata_edit.py ~/.local/share/nautilus-python/extensions/.
+cp nuxeo-drive-client/nxdrive/contextual_menu/nautilus/metadata_view.py ~/.local/share/nautilus-python/extensions
 
 nautilus&
 
