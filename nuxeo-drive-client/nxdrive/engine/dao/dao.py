6595bc8851460b169a4dd6e1462f28f8aa042b2d
Remi Cattiau <rcattiau@gmail.com>
2014-12-22 09:56:18 -0800

    NXDRIVE-159: Refactor packages

---
+++ nuxeo-drive-client/nxdrive/engine/dao/dao.py
@@ -0,0 +1,24 @@
+'''
+Created on 17 oct. 2014
+
+@author: Remi Cattiau
+'''
+from PyQt4.QtCore import QObject
+
+
+class DAO(QObject):
+    '''
+    classdocs
+    '''
+    self._state_state = QtSignal 
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+
+    def get_state(self):
+        session.expunge(obj)
+
+    def save_state(self, object):
+        _save_state.emit(object)
