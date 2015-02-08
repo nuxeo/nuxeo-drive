c961e16e356b15c99098a4fb8e358c111218c259
Remi Cattiau <rcattiau@gmail.com>
2015-02-08 16:13:19 -0500

    NXDRIVE-45: Clean unit test

--- nuxeo-drive-client/nxdrive/tests/test_watchers.py
+++ nuxeo-drive-client/nxdrive/tests/test_watchers.py
@@ -2,46 +2,35 @@
 @author: Remi Cattiau
 '''
 from nxdrive.tests.common_unit_test import UnitTestCase
-from time import sleep
-from PyQt4 import QtCore
-
-class Slots(QtCore.QObject):
-    @QtCore.pyqtSlot()
-    def engine_started(self):
-        print "Engine started"
 
 
-class TestLocalWatcher(UnitTestCase):
+class TestWatchers(UnitTestCase):
 
     def test_local_scan(self):
-        test = Slots()
         files, folders = self.make_local_tree()
         self.queue_manager_1.pause()
-        self.engine_1._start.connect(test.engine_started)
         self.engine_1.start()
         self._interact(1)
         metrics = self.queue_manager_1.get_metrics()
         self.assertEquals(metrics["total_queue"], folders + files)
         self.assertEquals(metrics["local_folder_queue"], folders)
         self.assertEquals(metrics["local_file_queue"], files)
+        self.assertEquals(metrics["remote_file_queue"], 0)
+        self.assertEquals(metrics["remote_folder_queue"], 0)
 
     def test_reconcile_scan(self):
-        test = Slots()
         files, folders = self.make_local_tree()
         self.make_server_tree()
         self.queue_manager_1.pause()
-        self.engine_1._start.connect(test.engine_started)
         self.engine_1.start()
         self._interact(10)
         metrics = self.queue_manager_1.get_metrics()
-        print "%r" % metrics
-        queue = self.get_full_queue(self.queue_manager_1.get_local_file_queue())
-        print "QUEUE: %r" % queue
         # Remote as one more file
         self.assertEquals(metrics["total_queue"], folders + files + 1)
         self.assertEquals(metrics["local_folder_queue"], folders)
         self.assertEquals(metrics["local_file_queue"], files)
         self.assertEquals(metrics["remote_file_queue"], 1)
+        self.assertEquals(metrics["remote_folder_queue"], 0)
         # Verify it has been reconcile
         queue = self.get_full_queue(self.queue_manager_1.get_local_file_queue())
         for item in queue:
@@ -51,12 +40,10 @@ class TestLocalWatcher(UnitTestCase):
             self.assertEqual(item.pair_state, "synchronized")
 
     def test_remote_scan(self):
-        test = Slots()
         files, folders = self.make_server_tree()
         # Add the workspace folder
         folders = folders + 1
         self.queue_manager_1.pause()
-        self.engine_1._start.connect(test.engine_started)
         self.engine_1.start()
         self._interact(10)
         # Metrics should be the same as the local scan
@@ -64,3 +51,5 @@ class TestLocalWatcher(UnitTestCase):
         self.assertEquals(metrics["total_queue"], folders + files)
         self.assertEquals(metrics["remote_folder_queue"], folders)
         self.assertEquals(metrics["remote_file_queue"], files)
+        self.assertEquals(metrics["local_file_queue"], 0)
+        self.assertEquals(metrics["local_folder_queue"], 0)
