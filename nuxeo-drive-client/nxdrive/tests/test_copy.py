4e891d70a32978cd812fa0460f0f201f2f40cfed
Antoine Taillefer <ataillefer@nuxeo.com>
2015-02-12 11:07:54 +0100

    NXDRIVE-170: Fix TestCopy to actually test detection of 'documentCreatedByCopy' event in audit log

--- nuxeo-drive-client/nxdrive/tests/test_copy.py
+++ nuxeo-drive-client/nxdrive/tests/test_copy.py
@@ -5,24 +5,24 @@ from nxdrive.tests.common import IntegrationTestCase
 class TestCopy(IntegrationTestCase):
 
     def test_synchronize_remote_copy(self):
-        # Get local and remote clients
         local = self.local_client_1
         remote = self.remote_document_client_1
-
-        self.setUpDrive_1()
 
         # Create a file and a folder in the remote root workspace
         remote.make_file('/', 'test.odt', 'Some content.')
         remote.make_folder('/', 'Test folder')
 
-        # Copy the file to the folder remotely
-        remote.copy('/test.odt', '/Test folder')
-
-        # Launch ndrive, expecting 4 synchronized items
-        self.ndrive()
+        # Launch ndrive and check synchronization
+        self.setUpDrive_1(firstSync=True)
         self.assertTrue(local.exists('/'))
         self.assertTrue(local.exists('/Test folder'))
         self.assertTrue(local.exists('/test.odt'))
+
+        # Copy the file to the folder remotely
+        remote.copy('/test.odt', '/Test folder')
+
+        # Launch ndrive and check synchronization
+        self.ndrive()
         self.assertTrue(local.exists('/Test folder/test.odt'))
         self.assertEquals(local.get_content('/Test folder/test.odt'),
                           'Some content.')
