0cfb500534c59bc11d475c82ac26bcf86fc73366
Antoine Taillefer <ataillefer@nuxeo.com>
2015-02-27 16:34:24 +0100

    NXP-16339: Keep compatibility with old header name: X-NXDocumentProperties

--- nuxeo-drive-client/nxdrive/client/base_automation_client.py
+++ nuxeo-drive-client/nxdrive/client/base_automation_client.py
@@ -289,6 +289,8 @@ class BaseAutomationClient(BaseClient):
             "Content-Type": "application/json+nxrequest",
             "Accept": "application/json+nxentity, */*",
             "X-NXproperties": "*",
+            # Keep compatibility with old header name
+            "X-NXDocumentProperties": "*",
         }
         if void_op:
             headers.update({"X-NXVoidOperation": "true"})
