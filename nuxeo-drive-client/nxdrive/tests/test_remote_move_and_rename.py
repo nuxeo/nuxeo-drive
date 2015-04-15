0e1ae50ea46b8f3ce9b7f66d9f3c2e80153ce339
Antoine Taillefer <ataillefer@nuxeo.com>
2015-04-15 15:41:26 +0200

    NXDRIVE-170: Skip some more tests waiting to fix them

--- nuxeo-drive-client/nxdrive/tests/test_remote_move_and_rename.py
+++ nuxeo-drive-client/nxdrive/tests/test_remote_move_and_rename.py
@@ -198,7 +198,7 @@ class TestRemoteMoveAndRename(UnitTestCase):
         self.assertEquals(file_1_state.local_name, u'Original File 1.txt')
 
     def test_remote_move_and_rename_file(self):
-        if sys.platform == 'darwin':
+        if sys.platform == 'darwin' or sys.platform == 'win32':
             raise SkipTest("WIP in https://jira.nuxeo.com/browse/NXDRIVE-170")
         remote_client = self.remote_client_1
         local_client = self.local_client_1
