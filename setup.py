0459c23cc0944e33ef5276517540efccb72ff7a6
Antoine Taillefer <ataillefer@nuxeo.com>
2015-08-06 19:31:44 +0200

    NXDRIVE-360: Only normalize version if Python version needs/allows it

--- setup.py
+++ setup.py
@@ -11,7 +11,9 @@ import nx_esky
 from esky.bdist_esky import Executable as es_Executable
 import pkg_resources
 
-packaging = pkg_resources.packaging
+packaging = None
+if hasattr(pkg_resources, 'packaging'):
+    packaging = pkg_resources.packaging
 
 OUTPUT_DIR = 'dist'
 SERVER_MIN_VERSION = '5.6'
@@ -336,12 +338,13 @@ class NuxeoDriveSetup(object):
             # a, b, c and d are all 16 bits integers
             version = version.replace('-dev', ".%s" % (
                 timestamp[4:8]))
-            # Normalize version according to distutils policy
-            normalized_version = str(packaging.version.Version(version))
-            if normalized_version != version:
-                print "Normalizing version from %s to %s to be compliant with distutils" % (
-                                                                                        version, normalized_version)
-                version = normalized_version
+            # Normalize version according to distutils policy, only if Python version allows it
+            if packaging is not None:
+                normalized_version = str(packaging.version.Version(version))
+                if normalized_version != version:
+                    print "Normalizing version from %s to %s to be compliant with distutils" % (
+                        version, normalized_version)
+                    version = normalized_version
             update_version(init_file, version)
             print "Updated version to " + version
 
