a4b302025984e27c4accb9a6bc5aee3a2b8d3c6d
Antoine Taillefer <ataillefer@nuxeo.com>
2015-08-07 15:09:54 +0200

    NXDRIVE-409: Use proxy settings for Drive startup page check

--- nuxeo-drive-client/nxdrive/manager.py
+++ nuxeo-drive-client/nxdrive/manager.py
@@ -764,6 +764,9 @@ class Manager(QtCore.QObject):
                                                             proxy_settings)
         self.proxyUpdated.emit(proxy_settings)
 
+    def get_proxies(self):
+        return self.proxies
+
     def get_engine(self, local_folder):
         if self._engines is None:
             self.load()
