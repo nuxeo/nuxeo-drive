6c6089d54f51e816f9aa37b55a108e335e4c4939
Antoine Taillefer <ataillefer@nuxeo.com>
2015-11-20 20:30:51 +0100

    NXDRIVE-471: Temporarily skip test_move_and_copy_paste_folder_original_location under OS X

--- nuxeo-drive-client/nxdrive/tests/test_multiple_files.py
+++ nuxeo-drive-client/nxdrive/tests/test_multiple_files.py
@@ -8,7 +8,9 @@ Adapted to Drive
 from common_unit_test import UnitTestCase
 from nxdrive.tests.common_unit_test import log
 from nxdrive.tests.common_unit_test import FILE_CONTENT
+from nose.plugins.skip import SkipTest
 
+import sys
 import os
 import shutil
 
@@ -48,6 +50,8 @@ class MultipleFilesTestCase(UnitTestCase):
         log.debug('*** exit MultipleFilesTestCase.setUp()')
 
     def test_move_and_copy_paste_folder_original_location(self):
+        if sys.platform == 'darwin':
+            raise SkipTest("NXDRIVE-471: Temporarily skipped, need to investigate")
         """
         Move folder 'Nuxeo Drive Test Workspace/a1' under 'Nuxeo Drive Test Workspace/a2'.
         Then copy 'Nuxeo Drive Test Workspace/a2/a1' back under 'Nuxeo Drive Test Workspace', so files are both in
