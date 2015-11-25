9002c930a57dc1cd6303becf5b9cdbe0ecc39f76
Antoine Taillefer <ataillefer@nuxeo.com>
2015-11-25 14:39:51 +0100

    NXDRIVE-480: Unskip test_local_move_file, should be fixed by NXDRIVE-483

--- nuxeo-drive-client/nxdrive/tests/test_local_move_and_rename.py
+++ nuxeo-drive-client/nxdrive/tests/test_local_move_and_rename.py
@@ -190,8 +190,6 @@ class TestLocalMoveAndRename(UnitTestCase):
             u'Original Folder 1')
 
     def test_local_move_file(self):
-        if sys.platform == 'win32':
-            raise SkipTest("NXDRIVE-480: Temporarily skipped, need to investigate")
         local_client = self.local_client_1
         remote_client = self.remote_document_client_1
 
