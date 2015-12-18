a21b12edf185b7831d08f1f6632f41426246b9dc
Remi Cattiau <remi@cattiau.com>
2015-12-18 08:38:15 -0800

    NXDRIVE-282: Remove useless SYSTEM attribute on folder icon

--- nuxeo-drive-client/nxdrive/client/local_client.py
+++ nuxeo-drive-client/nxdrive/client/local_client.py
@@ -281,13 +281,11 @@ class LocalClient(BaseClient):
                 create_file.close()
                 win32api.SetFileAttributes(created_ini_file_path, win32con.FILE_ATTRIBUTE_SYSTEM)
                 win32api.SetFileAttributes(created_ini_file_path, win32con.FILE_ATTRIBUTE_HIDDEN)
-                win32api.SetFileAttributes(attrib_command_path, win32con.FILE_ATTRIBUTE_SYSTEM)
             except Exception as e:
                 log.error("Exception when setting folder icon : %r", e)
         else:
             win32api.SetFileAttributes(created_ini_file_path, win32con.FILE_ATTRIBUTE_SYSTEM)
             win32api.SetFileAttributes(created_ini_file_path, win32con.FILE_ATTRIBUTE_HIDDEN)
-            win32api.SetFileAttributes(attrib_command_path, win32con.FILE_ATTRIBUTE_SYSTEM)
 
     def _read_data(self, file_path):
         '''The data file contains the mac icons'''
