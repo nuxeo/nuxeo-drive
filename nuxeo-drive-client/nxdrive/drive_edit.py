ee2105df559d6452595ce45b79d389ad1edfd662
Antoine Taillefer <ataillefer@nuxeo.com>
2016-01-11 09:29:07 +0100

    NXDRIVE-256: Log raw DriveEdit URL when handling it

--- nuxeo-drive-client/nxdrive/drive_edit.py
+++ nuxeo-drive-client/nxdrive/drive_edit.py
@@ -84,7 +84,7 @@ class DriveEdit(Worker):
             url = self._url
         if url is None:
             return
-        log.debug("DriveEdit load: '%s'", url)
+        log.debug("DriveEdit load: '%r'", url)
         try:
             info = parse_protocol_url(str(url))
         except UnicodeEncodeError:
