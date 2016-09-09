1b43198fe6f14e1674bc942f1b0de4ae26856659
Remi Cattiau <remi@cattiau.com>
2016-09-09 09:11:04 -0400

    NXDRIVE-568: Fix typos

--- nuxeo-drive-client/nxdrive/client/remote_document_client.py
+++ nuxeo-drive-client/nxdrive/client/remote_document_client.py
@@ -246,7 +246,7 @@ class RemoteDocumentClient(BaseAutomationClient):
         return ref
 
     def _doc_to_info(self, doc, fetch_parent_uid=True, parent_uid=None):
-        return self.doc_to_info(self, doc, fetch_parent_uid, parent_uid)
+        return self.doc_to_info(doc, fetch_parent_uid, parent_uid)
 
     def doc_to_info(self, doc, fetch_parent_uid=True, parent_uid=None):
         """Convert Automation document description to NuxeoDocumentInfo"""
