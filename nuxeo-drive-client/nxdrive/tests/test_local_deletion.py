32ff0d4a74cb083b426b7b81f476b5d6bad2dd1c
RavinderSharp <lravinder@ssdi.sharp.co.in>
2016-10-29 10:02:48 +0530

    NXDRIVE-634: Update file move condition

--- nuxeo-drive-client/nxdrive/tests/test_local_deletion.py
+++ nuxeo-drive-client/nxdrive/tests/test_local_deletion.py
@@ -55,6 +55,30 @@ class TestLocalDeletion(UnitTestCase):
         self.assertFalse(self.local_client_1.exists('/File_To_Delete.txt'))
         self.assertEqual(self.local_client_1.get_content('/File_To_Delete2.txt'), 'New content')
 
+    def test_move_untrash_file_on_parent(self):
+        file_path = '/ToDelete/File_To_Delete.txt'
+        self.local_client_1.make_folder('/', 'ToDelete')
+        self.local_client_1.make_file('/ToDelete', 'File_To_Delete.txt', 'This is a content')
+        self.wait_sync()
+        self.assertTrue(self.remote_document_client_1.exists(file_path))
+        old_info = self.remote_document_client_1.get_info(file_path, use_trash=True)
+        abs_path = self.local_client_1._abspath(file_path)
+        # Pretend we had trash the file
+        shutil.move(abs_path, os.path.join(self.local_test_folder_1, 'File_To_Delete.txt'))        
+        self.wait_sync(wait_for_async=True)
+        self.local_client_1.delete('/ToDelete')
+        self.wait_sync(wait_for_async=True)
+        self.assertFalse(self.remote_document_client_1.exists(file_path))
+        self.assertFalse(self.local_client_1.exists(file_path))
+        # See if it untrash or recreate
+        shutil.move(os.path.join(self.local_test_folder_1, 'File_To_Delete.txt'), self.local_client_1._abspath('/'))
+        self.wait_sync(wait_for_async=True)
+        new_info = self.remote_document_client_1.get_info(old_info.uid, use_trash=True)        
+        self.assertFalse('ToDelete' in new_info.path)
+        self.assertEqual(new_info.state, 'project')
+        self.assertTrue(self.local_client_1.exists('/File_To_Delete.txt'))
+        pass
+    
     def test_untrash_file_on_delete_parent(self):
         file_path = '/ToDelete/File_To_Delete.txt'
         self.local_client_1.make_folder('/', 'ToDelete')
