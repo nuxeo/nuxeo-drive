0cfd51a58b7d4888c76b8836b7cc4408449e3605
Antoine Taillefer <ataillefer@nuxeo.com>
2017-03-18 09:53:17 +0100

    NXDRIVE-771: Set RandomBug on TestConflicts#test_real_conflict for Linux

--- nuxeo-drive-client/tests/test_conflicts.py
+++ nuxeo-drive-client/tests/test_conflicts.py
@@ -3,6 +3,7 @@ import shutil
 
 from tests.common import OS_STAT_MTIME_RESOLUTION
 from tests.common_unit_test import UnitTestCase
+from tests.common_unit_test import RandomBug
 from nxdrive.osi import AbstractOSIntegration
 from unittest import skipIf
 
@@ -53,6 +54,7 @@ class TestConflicts(UnitTestCase):
         self.assertEqual(remote.get_content(remote_children[0].uid), 'Remote update 2')
         self.assertEqual(self.engine_1.get_dao().get_normal_state_from_remote(self.file_id).pair_state, "conflicted")
 
+    @RandomBug('NXDRIVE-771', target='linux')
     def test_real_conflict(self):
         local = self.local_client_1
         remote = self.remote_file_system_client_2
