8c949002cb3a864706bf59e49b58fc77ac01c413
Mickaël Schoentgen <BoboTiG@users.noreply.github.com>
2017-03-31 17:03:39 +0200

    NXDRIVE-571: Use SSH agent to be able to push to GitHub

--- tools/jenkins/beta.groovy
+++ tools/jenkins/beta.groovy
@@ -18,20 +18,17 @@ properties([
 node('IT') {
     withEnv(["WORKSPACE=${pwd()}"]) {
         env.DRY_RUN = params.DRY_RUN
+        def credential_id = '4691426b-aa51-428b-901d-4e851ee37b01'
 
         stage('Checkout') {
             deleteDir()
-            checkout scm
-            checkout([
-                $class: 'GitSCM', branches: [[name: '*/master']],
-                extensions: [
-                    [$class: 'CleanCheckout'],
-                    [$class: 'LocalBranch', localBranch: 'master']]
-            ])
+            git credentialsId: credential_id, url: 'https://github.com/nuxeo/nuxeo-drive.git'
         }
 
         stage('Create') {
-            sh 'tools/release.sh --create'
+            sshagent([credential_id]) { {
+                sh 'tools/release.sh --create'
+            }
         }
 
         stage('Trigger') {
@@ -46,7 +43,9 @@ node('IT') {
             dir('dist') {
                 deleteDir()
             }
-            sh 'tools/release.sh --publish'
+            sshagent([credential_id]) { {
+                sh 'tools/release.sh --publish'
+            }
         }
     }
 }
