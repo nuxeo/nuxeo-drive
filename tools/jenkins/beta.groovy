4f78e7528746adb2692847018bce9a9d7376b5a0
Mickaël Schoentgen <mschoentgen@nuxeo.com>
2017-04-03 11:35:27 +0200

    NXDRIVE-571: Bypass packages step for testing purpose

--- tools/jenkins/beta.groovy
+++ tools/jenkins/beta.groovy
@@ -37,9 +37,9 @@ timestamps {
                 // Trigger the Drive packages job to build executables and have artifacts
                 release = sh script: 'git tag -l "release-*" --sort=-taggerdate | head -n1', returnStdout: true
                 release = release.trim()
-                build job: '/Drive/Drive-packages', parameters: [
+                /*build job: '/Drive/Drive-packages', parameters: [
                     [$class: 'StringParameterValue', name: 'BRANCH_NAME', value: 'refs/tags/' + release],
-                    [$class: 'BooleanParameterValue', name: 'CLEAN_WORKSPACE', value: true]]
+                    [$class: 'BooleanParameterValue', name: 'CLEAN_WORKSPACE', value: true]]*/
             }
 
             stage('Publish') {
