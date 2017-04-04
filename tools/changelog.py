8064638a885bf2e4389658673939ccf7ba22a8da
Mickaël Schoentgen <mschoentgen@nuxeo.com>
2017-04-04 17:28:30 +0200

    NXDRIVE-360: Refactor tools/changelog.py to accept all arguments that `git log` will

--- tools/changelog.py
+++ tools/changelog.py
@@ -1,48 +1,24 @@
 # coding: utf-8
 """
 Git changelog generator.
-
-Usage:
-  changelog.py [<start_commit>] [<end_commit>] [--format=FORMAT] [--issue-types=TYPE]
-  changelog.py (--help|--version)
-  changelog.py --examples
-
-Options:
-  --help              Show this screen.
-  --version           Show version.
-  --examples          Show usage examples.
-  --format=FORMAT     Output format [default: txt].
-  --issue-types=TYPE  Issues types [default: NXDRIVE,NXP].
-
-Arguments:
-
-    start_commit: ''   - use default value (HEAD)
-                  SHA1 - commit ID that is the scan upper bound
-
-    end_commit: ''   - use default value (latest release tag)
-                SHA1 - commit ID that is the scan lower bound
-
-    FORMAT: md  - Markdown
-            rst - reStructuredText
-            txt - plain text
 """
 from __future__ import print_function, unicode_literals
 
+from argparse import ArgumentParser
 from operator import itemgetter
 from re import findall
 from subprocess import check_output
 from sys import stderr
 
-from docopt import docopt
 from requests import HTTPError, get
 
-__version__ = '1.0.3'
+__version__ = '1.1.0'
 
 
 def backtick(cmd):
     """ Get command output as stripped string. """
 
-    output = check_output(cmd.split())
+    output = check_output(cmd)
     return output.decode('utf-8').strip()
 
 
@@ -120,42 +96,40 @@ def debug(*args, **kwargs):
 
 
 def examples():
-    """
-For every command, you can specify --format and --issue-types arguments.
-And, of course, you can use git notations like "HEAD^", "HEAD~6", etc..
+    """ Print several examples. """
 
-Example 1: changelog.py
+    print("""
+Example {}: changelog.py
 
     Print changelog of commits from HEAD to the latest release tag.
     If there is no release tag, it will use the full commit history.
 
-Example 2: changelog.py COMMIT_ID
 
-    Print changelog of commits from COMMIT_ID to the latest release tag.
-    If there is no release tag, it will use the full commit history starting
-    from COMMIT_ID.
-
-Example 3: changelog.py '' COMMIT_ID
+Example {}: changelog.py -- HEAD...COMMIT_ID
 
     Print changelog of commits from HEAD to COMMIT_ID.
 
-Example 4: changelog.py release-2.2.227 release-2.3.323
+
+Example {}: changelog.py -- HEAD...HEAD~6
+
+    Print changelog of latest 6 commits.
+
+
+Example {}: changelog.py -- release-2.2.227...release-2.2.323
 
     Print changelog of commits between the 2 releases.
-    """
-
-    print(examples.__doc__.strip())
 
 
-def get_commits_between(start, end):
-    """ Retrieve commits between two states. """
+Example {}: changelog.py -- --since=2017-03-16 --until=2017-03-28
 
-    commit_range = start
-    if end:
-        commit_range = '{}...{}'.format(start, end)
-    debug('>>> Retrieving commits {}'.format(commit_range))
-    all_commits = backtick('git log --pretty=oneline ' + commit_range)
-    return sorted(all_commits.splitlines(), reverse=True)
+    Print changelog of commits between the dates on the master branch.
+
+
+Example {}: changelog.py -- --since=2017-03-16 --until=2017-03-28 --author="Mickaël Schoentgen"
+
+    Print changelog of commits between the dates on the master branch for a given author.
+    Useful for sprint reports.
+""".format(*range(1, 42)).strip())  # noqa
 
 
 def get_latest_tag():
@@ -163,29 +137,32 @@ def get_latest_tag():
 
     debug('>>> Retrieving latest created tag')
     cmd = 'git rev-list --tags --remove-empty --branches=master --max-count=10'
-    for sha1 in backtick(cmd).splitlines():
-        tag = backtick('git describe --abbrev=0 --tags ' + sha1)
+    for sha1 in backtick(cmd.split()).splitlines():
+        cmd = 'git describe --abbrev=0 --tags ' + sha1
+        tag = backtick(cmd.split())
         if tag.startswith('release-'):
             return tag
     return ''
 
 
-def get_issues(start='HEAD', end=None, types=('NXDRIVE', 'NXP')):
+def get_issues(args):
     """ Get issues from commits message. """
 
-    if not start:
-        start = 'HEAD'
-    if not end:
+    if not args.GIT_OPTIONS:
         # No commit ID, so we just need to find commits after the latest tag
-        end = get_latest_tag()
+        args.GIT_OPTIONS = ['HEAD...' + get_latest_tag()]
 
-    all_commits = get_commits_between(start, end)
+    debug('>>> Retrieving commits {}'.format(
+        ' '.join(arg.decode('utf-8') for arg in args.GIT_OPTIONS)))
+    cmd = ['git', 'log', '--pretty=oneline'] + args.GIT_OPTIONS
+    all_commits = backtick(cmd)
+
     debug('>>> Retrieving issues')
     commits = []
     reverts = []
-    for commit in all_commits:
+    for commit in sorted(all_commits.splitlines(), reverse=True):
         parts = commit.split()
-        for issue_type in types:
+        for issue_type in args.types:
             if parts[1] == 'Revert':
                 issue = parts[2].lstrip('"').rstrip(':')
                 reverts.append(issue)
@@ -263,28 +240,26 @@ def get_version():
 def main():
     """ Main logic. """
 
-    debug('>>> Changelog.py v{}'.format(__version__))
+    parser = ArgumentParser()
+    parser.add_argument('--version', action='version', version=__version__)
+    parser.add_argument('--examples', action='store_true',
+                        help='show usage examples')
+    parser.add_argument('--format', default='txt',
+                        choices=('md', 'rst', 'txt'),
+                        help='report output format (default: %(default)s)')
+    parser.add_argument('--types', nargs='*', default=('NXDRIVE', 'NXP'),
+                        help='issues types (default: NXDRIVE NXP)')
+    parser.add_argument('GIT_OPTIONS', nargs='*',
+                        help='git options forwarded to `git log`')
+    args = parser.parse_args()
 
-    args = docopt(__doc__, version=__version__)
-
-    if args['--examples']:
+    if args.examples:
         examples()
         return
 
-    start = args['<start_commit>']
-    end = args['<end_commit>']
-    issue_types = set(args['--issue-types'].split(','))
-    formatter = args['--format']
-
-    if formatter not in ('md', 'rst', 'txt'):
-        formatter = 'txt'
-        debug('>>> Bad formatter value, using "{}"'.format(formatter))
-
     # Get commits and print out formatted interesting informations
-    issues = []
-    for issue in get_issues(start=start, end=end, types=issue_types):
-        issues.append(issue)
-    changelog(issues, formatter=formatter)
+    debug('>>> Changelog.py v{}'.format(__version__))
+    changelog([issue for issue in get_issues(args)], formatter=args.format)
 
 
 if __name__ == '__main__':
