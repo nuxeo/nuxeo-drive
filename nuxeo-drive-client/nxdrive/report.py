57cfc68e1ce6b2700c256ff90c9187f13734f00a
Mickaël Schoentgen <BoboTiG@users.noreply.github.com>
2017-04-12 08:03:02 +0200

    NXDRIVE-820: Fix MemoryError on report creation and use deflate compression

--- nuxeo-drive-client/nxdrive/report.py
+++ nuxeo-drive-client/nxdrive/report.py
@@ -3,23 +3,19 @@ Created on 6 mai 2015
 
 @author: Remi Cattiau
 '''
+import codecs
 import os
 from datetime import datetime
-from nxdrive.logging_config import get_logger, get_handler, MAX_LOG_DISPLAYED
-from zipfile import ZipFile
+from zipfile import ZipFile, ZIP_DEFLATED
+
+from nxdrive.logging_config import MAX_LOG_DISPLAYED, get_handler, get_logger
 
 log = get_logger(__name__)
 
 
 class Report(object):
-    '''
-    classdocs
-    '''
 
     def __init__(self, manager, report_path=None):
-        '''
-        Constructor
-        '''
         self._manager = manager
         if report_path is None:
             self._report_name = 'report_' + datetime.now().strftime('%y%m%d_%H%M%S')
@@ -32,18 +28,17 @@ class Report(object):
         self._zipfile = os.path.join(folder, self._report_name + '.zip')
 
     def copy_logs(self, myzip):
-        try:
-            folder = os.path.join(self._manager.get_configuration_folder(), 'logs')
-            for filename in os.listdir(folder):
-                path = os.path.join(folder, filename)
-                if not os.path.isfile(path):
-                    continue
-                myzip.write(path, os.path.join('logs/', filename))
-        except:
-            # Do not prevent report to work on copy errors
-            pass
+        folder = os.path.join(self._manager.get_configuration_folder(), 'logs')
+        if not os.path.isdir(folder):
+            return
 
-    def copy_db(self, myzip, dao):
+        for filename in os.listdir(folder):
+            path = os.path.join(folder, filename)
+            if os.path.isfile(path):
+                myzip.write(path, os.path.join('logs', filename))
+
+    @staticmethod
+    def copy_db(myzip, dao):
         # Lock to avoid inconsistence
         dao._lock.acquire()
         try:
@@ -56,29 +51,31 @@ class Report(object):
 
     @staticmethod
     def _export_logs():
-        logs = u''
         logger = get_logger(None)
         handler = get_handler(logger, "memory")
         log_buffer = handler.get_buffer(MAX_LOG_DISPLAYED)
         for record in log_buffer:
             try:
-                log_ = handler.format(record).decode('utf-8', errors='replace')
+                yield handler.format(record).decode('utf-8', errors='replace')
             except UnicodeEncodeError:
-                log_ = handler.format(record).encode('utf-8', errors='replace')\
-                                             .decode('utf-8')
-            logs += log_ + u'\n'
-        return logs
+                yield handler.format(record).encode('utf-8', errors='replace')\
+                                            .decode('utf-8')
 
     def generate(self):
         log.debug("Create report '%s'", self._report_name)
         log.debug("Manager metrics: '%s'", self._manager.get_metrics())
-        with ZipFile(self._zipfile, 'w') as myzip:
+        with ZipFile(self._zipfile, mode='w', compression=ZIP_DEFLATED) as zip:
             dao = self._manager.get_dao()
-            self.copy_db(myzip, dao)
+            self.copy_db(zip, dao)
             for engine in self._manager.get_engines().values():
                 log.debug("Engine metrics: '%s'", engine.get_metrics())
-                self.copy_db(myzip, engine.get_dao())
+                self.copy_db(zip, engine.get_dao())
                 # Might want threads too here
-            self.copy_logs(myzip)
-            content = self._export_logs().encode('utf-8', errors='ignore')
-            myzip.writestr('debug.log', content.strip())
+            self.copy_logs(zip)
+
+            # Memory efficient debug.log creation
+            with codecs.open('debug.log', mode='wb', encoding='utf-8',
+                             errors='replace') as output:
+                for line in self._export_logs():
+                    output.write(line + '\n')
+            zip.write('debug.log')
