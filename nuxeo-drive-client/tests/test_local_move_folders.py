2ebef5e59d185e7884b5fdb2f21246ddd45182e1
Mickaël Schoentgen <mschoentgen@nuxeo.com>
2017-04-13 12:27:55 +0200

    NXDRIVE-808: Generate valid PNG files

--- nuxeo-drive-client/tests/test_local_move_folders.py
+++ nuxeo-drive-client/tests/test_local_move_folders.py
@@ -29,17 +29,17 @@ class TestLocalMoveFolders(UnitTestCase):
         # Add image files to a1
         abs_folder_path_1 = self.local_client_1.abspath(self.folder_path_1)
         for file_num in range(1, self.NUMBER_OF_LOCAL_IMAGE_FILES + 1):
-            file_name = self.FILE_NAME_PATTERN % (file_num, 'jpg')
+            file_name = self.FILE_NAME_PATTERN % (file_num, 'png')
             file_path = os.path.join(abs_folder_path_1, file_name)
-            self.generate_random_jpg(file_path, random.randint(1000, 3000))
+            self.generate_random_png(file_path, random.randint(1, 42))
         log.debug('Local test files created in a1')
 
         # Add image files to a2
         abs_folder_path_2 = self.local_client_1.abspath(self.folder_path_2)
         for file_num in range(1, self.NUMBER_OF_LOCAL_IMAGE_FILES + 1):
-            file_name = self.FILE_NAME_PATTERN % (file_num, 'jpg')
+            file_name = self.FILE_NAME_PATTERN % (file_num, 'png')
             file_path = os.path.join(abs_folder_path_2, file_name)
-            self.generate_random_jpg(file_path, random.randint(1000, 3000))
+            self.generate_random_png(file_path, random.randint(1, 42))
         log.debug('Local test files created in a2')
 
         self.engine_1.start()
@@ -51,7 +51,7 @@ class TestLocalMoveFolders(UnitTestCase):
         self.assertEqual(len(children_1), self.NUMBER_OF_LOCAL_IMAGE_FILES,
                          'Number of local files (%d) in a1 is different from original (%d)' %
                          (len(children_1), self.NUMBER_OF_LOCAL_IMAGE_FILES))
-        self.assertEqual(set(children_1), set(['file%03d.jpg' % file_num
+        self.assertEqual(set(children_1), set(['file%03d.png' % file_num
                                                for file_num in range(1, self.NUMBER_OF_LOCAL_IMAGE_FILES + 1)]))
 
         # Check local files in a2
@@ -60,7 +60,7 @@ class TestLocalMoveFolders(UnitTestCase):
         self.assertEqual(len(children_2), self.NUMBER_OF_LOCAL_IMAGE_FILES,
                          'Number of local files (%d) in a2 is different from original (%d)' %
                          (len(children_2), self.NUMBER_OF_LOCAL_IMAGE_FILES))
-        self.assertEqual(set(children_2), set(['file%03d.jpg' % file_num
+        self.assertEqual(set(children_2), set(['file%03d.png' % file_num
                                                for file_num in range(1, self.NUMBER_OF_LOCAL_IMAGE_FILES + 1)]))
 
         # Check remote files in a1
@@ -73,7 +73,7 @@ class TestLocalMoveFolders(UnitTestCase):
         self.assertEqual(len(remote_children_1), self.NUMBER_OF_LOCAL_IMAGE_FILES,
                          'Number of remote files (%d) in a1 is different from original (%d)' %
                          (len(remote_children_1), self.NUMBER_OF_LOCAL_IMAGE_FILES))
-        self.assertEqual(set(remote_children_1), set(['file%03d.jpg' % file_num
+        self.assertEqual(set(remote_children_1), set(['file%03d.png' % file_num
                                                       for file_num in range(1, self.NUMBER_OF_LOCAL_IMAGE_FILES + 1)]))
 
         # Check remote files in a2
@@ -86,7 +86,7 @@ class TestLocalMoveFolders(UnitTestCase):
         self.assertEqual(len(remote_children_2), self.NUMBER_OF_LOCAL_IMAGE_FILES,
                          'Number of remote files (%d) in a2 is different from original (%d)' %
                          (len(remote_children_2), self.NUMBER_OF_LOCAL_IMAGE_FILES))
-        self.assertEqual(set(remote_children_2), set(['file%03d.jpg' % file_num
+        self.assertEqual(set(remote_children_2), set(['file%03d.png' % file_num
                                                       for file_num in range(1, self.NUMBER_OF_LOCAL_IMAGE_FILES + 1)]))
 
     def test_local_move_folder_with_files(self):
@@ -105,7 +105,7 @@ class TestLocalMoveFolders(UnitTestCase):
         self.assertEqual(len(children_2), self.NUMBER_OF_LOCAL_IMAGE_FILES,
                          'Number of local files (%d) in a2 is different from original (%d)' %
                          (len(children_2), self.NUMBER_OF_LOCAL_IMAGE_FILES))
-        self.assertEqual(set(children_2), set(['file%03d.jpg' % file_num
+        self.assertEqual(set(children_2), set(['file%03d.png' % file_num
                                                for file_num in range(1, self.NUMBER_OF_LOCAL_IMAGE_FILES + 1)]))
 
         # Check local files in a2/a1
@@ -114,7 +114,7 @@ class TestLocalMoveFolders(UnitTestCase):
         self.assertEqual(len(children_1), self.NUMBER_OF_LOCAL_IMAGE_FILES,
                          'Number of local files (%d) in a1 is different from original (%d)' %
                          (len(children_1), self.NUMBER_OF_LOCAL_IMAGE_FILES))
-        self.assertEqual(set(children_1), set(['file%03d.jpg' % file_num
+        self.assertEqual(set(children_1), set(['file%03d.png' % file_num
                                                for file_num in range(1, self.NUMBER_OF_LOCAL_IMAGE_FILES + 1)]))
 
         # Check that a1 doesn't exist anymore remotely
@@ -131,7 +131,7 @@ class TestLocalMoveFolders(UnitTestCase):
         self.assertEqual(len(remote_children_2), self.NUMBER_OF_LOCAL_IMAGE_FILES,
                          'Number of remote files (%d) in a2 is different from original (%d)' %
                          (len(remote_children_2), self.NUMBER_OF_LOCAL_IMAGE_FILES))
-        self.assertEqual(set(remote_children_2), set(['file%03d.jpg' % file_num
+        self.assertEqual(set(remote_children_2), set(['file%03d.png' % file_num
                                                       for file_num in range(1, self.NUMBER_OF_LOCAL_IMAGE_FILES + 1)]))
         # Check remote files in a2/a1
         a1_remote_id = self.local_client_1.get_remote_id('/a2/a1')
@@ -143,7 +143,7 @@ class TestLocalMoveFolders(UnitTestCase):
         self.assertEqual(len(remote_children_1), self.NUMBER_OF_LOCAL_IMAGE_FILES,
                          'Number of remote files (%d) in a1 is different from original (%d)' %
                          (len(remote_children_1), self.NUMBER_OF_LOCAL_IMAGE_FILES))
-        self.assertEqual(set(remote_children_1), set(['file%03d.jpg' % file_num
+        self.assertEqual(set(remote_children_1), set(['file%03d.png' % file_num
                                                       for file_num in range(1, self.NUMBER_OF_LOCAL_IMAGE_FILES + 1)]))
 
     def test_local_move_folder_both_sides_while_stopped(self):
