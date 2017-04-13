1ab9c85ed4fc887e09f16e9ab59852e67c0b6075
Antoine Taillefer <ataillefer@nuxeo.com>
2017-04-13 11:55:09 +0200

    NXDRIVE-808: Add some more RandomBugs

--- nuxeo-drive-client/tests/test_watchers.py
+++ nuxeo-drive-client/tests/test_watchers.py
@@ -34,6 +34,7 @@ class TestWatchers(UnitTestCase):
         # With root
         self.assertEqual(len(res), folders + files + 1)
 
+    @RandomBug('NXDRIVE-808', target='windows', repeat=2)
     def test_reconcile_scan(self):
         files, folders = self.make_local_tree()
         self.make_server_tree()
@@ -73,6 +74,7 @@ class TestWatchers(UnitTestCase):
         self.assertEqual(len(res), folders + files + 1)
 
     @RandomBug('NXDRIVE-806', target='linux', mode='BYPASS')
+    @RandomBug('NXDRIVE-806', target='windows', repeat=2)
     def test_local_watchdog_creation(self):
         # Test the creation after first local scan
         self.queue_manager_1.suspend()
@@ -258,6 +260,7 @@ class TestWatchers(UnitTestCase):
         self.assertEqual(remote.get_info(u'/Accentue\u0301.odt').name,
                          u'Accentu\xe9 avec un \xea et un \xe9.odt')
 
+    @RandomBug('NXDRIVE-808', target='mac', repeat=2)
     def test_watchdog_encoding(self):
         local = self.local_client_1
         remote = self.remote_document_client_1
@@ -316,6 +319,7 @@ class TestWatchers(UnitTestCase):
         self.assertFalse(
             remote.exists(u'/Sub folder/e\u0302tre ou ne pas \xeatre.odt'))
 
+    @RandomBug('NXDRIVE-808', target='windows', repeat=2)
     def test_watcher_remote_id_setter(self):
         local = self.local_client_1
         # As some user can rewrite same file for no reason
