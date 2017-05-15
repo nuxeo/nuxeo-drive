eac4f137d48323c9e60b243d87814d725ac8d57c
Mickaël Schoentgen <mschoentgen@nuxeo.com>
2017-05-15 10:00:13 +0200

    NXDRIVE-645: Update generate_random_png() for all tests

--- nuxeo-drive-client/tests/test_shared_folders.py
+++ nuxeo-drive-client/tests/test_shared_folders.py
@@ -182,21 +182,21 @@ class TestSharedFolders(UnitTestCase):
     def test_conflict_resolution_with_renaming(self):
         """ NXDRIVE-645: shared Folders conflict resolution with renaming. """
 
-        local_1 = self.local_client_1
-        remote_2 = self.remote_document_client_2
+        local = self.local_client_1
+        remote = self.remote_document_client_2
         self.engine_1.start()
         self.wait_sync(wait_for_async=True)
 
         # Create initial folder and file
-        folder = remote_2.make_folder('/', 'Final')
-        remote_2.make_file('/Final', 'Aerial04.jpg', b'42')
+        folder = remote.make_folder('/', 'Final')
+        remote.make_file('/Final', 'Aerial04.png', self.generate_random_png())
 
         # First checks, everything should be online for every one
         self.wait_sync(wait_for_async=True)
-        self.assertTrue(remote_2.exists('/Final'))
-        self.assertTrue(remote_2.exists('/Final/Aerial04.jpg'))
-        self.assertTrue(local_1.exists('/Final'))
-        self.assertTrue(local_1.exists('/Final/Aerial04.jpg'))
+        self.assertTrue(remote.exists('/Final'))
+        self.assertTrue(remote.exists('/Final/Aerial04.png'))
+        self.assertTrue(local.exists('/Final'))
+        self.assertTrue(local.exists('/Final/Aerial04.png'))
         folder_pair_state = self.engine_1.get_dao().get_state_from_local(
             '/' + self.workspace_title + '/Final')
         self.assertIsNotNone(folder_pair_state)
@@ -205,21 +205,22 @@ class TestSharedFolders(UnitTestCase):
         self.engine_1.stop()
 
         # Make changes
-        folder_conflicted = local_1.make_folder('/', 'Finished')
-        local_1.make_file(folder_conflicted, 'Aerial04.jpg', b'42.42')
-        remote_2.update(folder, properties={'dc:title': 'Finished'})
+        folder_conflicted = local.make_folder('/', 'Finished')
+        local.make_file(folder_conflicted, 'Aerial04.png',
+                        self.generate_random_png())
+        remote.update(folder, properties={'dc:title': 'Finished'})
 
         # Restart clients and wait
         self.engine_1.start()
         self.wait_sync(wait_for_async=True)
 
-        # Check remote 2
-        self.assertTrue(remote_2.exists('/Finished'))
-        self.assertTrue(remote_2.exists('/Finished/Aerial04.jpg'))
+        # Check remote
+        self.assertTrue(remote.exists('/Finished'))
+        self.assertTrue(remote.exists('/Finished/Aerial04.png'))
 
-        # Check client 1
-        self.assertTrue(local_1.exists('/Finished'))
-        self.assertTrue(local_1.exists('/Finished/Aerial04.jpg'))
+        # Check client
+        self.assertTrue(local.exists('/Finished'))
+        self.assertTrue(local.exists('/Finished/Aerial04.png'))
 
         # Check folder status
         folder_pair_state = self.engine_1.get_dao().get_state_from_id(
