c414e5b671393ab64ef45667d302b8bf2c21150f
Mickaël Schoentgen <mschoentgen@nuxeo.com>
2017-06-13 16:25:39 +0200

    NXDRIVE-680: Fix unwanted local upload on network connection lost

--- nuxeo-drive-client/nxdrive/engine/watcher/local_watcher.py
+++ nuxeo-drive-client/nxdrive/engine/watcher/local_watcher.py
@@ -237,7 +237,9 @@ class LocalWatcher(EngineWorker):
     def _scan(self):
         log.debug("Full scan started")
         start_ms = current_milli_time()
-        self._suspend_queue()
+        to_pause = not self._engine.get_queue_manager().is_paused()
+        if to_pause:
+            self._suspend_queue()
         self._delete_files = dict()
         self._protected_files = dict()
 
@@ -247,7 +249,8 @@ class LocalWatcher(EngineWorker):
         self._metrics['last_local_scan_time'] = current_milli_time() - start_ms
         log.debug("Full scan finished in %dms", self._metrics['last_local_scan_time'])
         self._local_scan_finished = True
-        self._engine.get_queue_manager().resume()
+        if to_pause:
+            self._engine.get_queue_manager().resume()
         self.localScanFinished.emit()
 
     def _scan_handle_deleted_files(self):
--- nuxeo-drive-client/tests/test_synchronization_suspend.py
+++ nuxeo-drive-client/tests/test_synchronization_suspend.py
@@ -32,6 +32,46 @@ class TestSynchronizationSuspend(UnitTestCase):
         self.assertEqual(len(remote.get_children_info(self.workspace_1)), 4)
         self.assertTrue(self.engine_1.get_queue_manager().is_paused())
 
+    def test_synchronization_local_watcher_paused_when_offline(self):
+        """ NXDRIVE-680: fix unwanted local upload when offline. """
+
+        local = self.local_client_1
+        remote = self.remote_document_client_1
+        engine = self.engine_1
+
+        # Create one file locally and wait for sync
+        engine.start()
+        self.wait_sync(wait_for_async=True)
+        local.make_file('/', 'file1.txt', content=b'42')
+        self.wait_sync()
+
+        # Checks
+        self.assertTrue(remote.exists('/file1.txt'))
+        self.assertTrue(local.exists('/file1.txt'))
+
+        # Simulate offline mode (no more network for instance)
+        engine.get_queue_manager().suspend()
+
+        # Create a bunch of files locally
+        local.make_folder('/', 'files')
+        for num in range(20):
+            local.make_file('/files',
+                            'file-' + str(num) + '.txt',
+                            content=b'Content of file-' + str(num))
+        self.wait_sync(fail_if_timeout=False)
+
+        # Checks
+        self.assertEqual(len(remote.get_children_info(self.workspace_1)), 1)
+        self.assertTrue(engine.get_queue_manager().is_paused())
+
+        # Restore network connection
+        engine.get_queue_manager().resume()
+
+        # Wait for sync and check synced files
+        self.wait_sync(wait_for_async=True)
+        self.assertEqual(len(remote.get_children_info(self.workspace_1)), 2)
+        self.assertFalse(engine.get_queue_manager().is_paused())
+
     @RandomBug('NXDRIVE-812', target='linux', mode='BYPASS')
     def test_synchronization_end_with_children_ignore_parent(self):
         """ NXDRIVE-655: children of ignored folder are not ignored. """
