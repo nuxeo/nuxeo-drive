3663c54b4cfa14210c808369720991a3022cd73c
Mickaël Schoentgen <BoboTiG@users.noreply.github.com>
2017-07-04 12:27:44 +0200

    Update darwin.py

--- nuxeo-drive-client/nxdrive/osi/darwin/darwin.py
+++ nuxeo-drive-client/nxdrive/osi/darwin/darwin.py
@@ -143,7 +143,7 @@ class DarwinIntegration(AbstractOSIntegration):
             if not os.access(folder, os.W_OK):
                 import stat
                 os.chmod(folder, stat.S_IXUSR | stat.S_IRGRP | stat.S_IXGRP
-                         |stat.S_IRUSR | stat.S_IWGRP | stat.S_IWUSR)
+                         | stat.S_IRUSR | stat.S_IWGRP | stat.S_IWUSR)
             import xattr
             attr = 'drive-test'
             xattr.setxattr(folder, attr, attr)
