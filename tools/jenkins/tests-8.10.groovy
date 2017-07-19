d945d8b15dc330fd6198f5a86db8f48977da5b8d
Mickaël Schoentgen <mschoentgen@nuxeo.com>
2017-07-19 12:50:04 +0200

    NXDRIVE-917: Do not test against 8.10 on GNU/Linux

--- tools/jenkins/tests-8.10.groovy
+++ tools/jenkins/tests-8.10.groovy
@@ -60,7 +60,8 @@ if (env.BRANCH_NAME.startsWith('wip-')) {
 }
 
 // Jenkins slaves we will build on
-slaves = ['OSXSLAVE-DRIVE', 'SLAVE810', 'WINSLAVE']
+//slaves = ['OSXSLAVE-DRIVE', 'SLAVE810', 'WINSLAVE']
+slaves = ['OSXSLAVE-DRIVE', 'WINSLAVE']  // removed GNU/Linux slave as it cannot be updated with required dependencies
 labels = [
     'OSXSLAVE-DRIVE': 'macOS',
     'SLAVE810': 'GNU/Linux',
