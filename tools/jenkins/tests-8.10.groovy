e593c9bb37e7323bece26f6d1f4a0fb6f3aa0869
Mickaël Schoentgen <mschoentgen@nuxeo.com>
2017-10-06 11:21:13 +0200

    Align 8.10 tests with devel

--- tools/jenkins/tests-8.10.groovy
+++ tools/jenkins/tests-8.10.groovy
@@ -3,9 +3,9 @@
 
 // Default values for required envars
 python_drive_version = '2.7.14'  // XXX: PYTHON_DRIVE_VERSION
-pyqt_version = '4.12'
-cxfreeze_version = '4.3.3'
-sip_version = '4.19'
+pyqt_version = '4.12.1'  // XXX: PYQT_VERSION
+sip_version = '4.19.3'  // XXX: SIP_VERSION
+cxfreeze_version = '4.3.3'  // XXX: CXFREEZE_VERSION
 
 // Pipeline properties
 properties([
