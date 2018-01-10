26fc885713670214ff25f0f01e444f5c68138a1a
Mickaël Schoentgen <mschoentgen@nuxeo.com>
2018-01-10 16:22:26 +0100

    Fix unicode

--- nuxeo-drive-client/nxdrive/utils.py
+++ nuxeo-drive-client/nxdrive/utils.py
@@ -82,7 +82,10 @@ def is_generated_tmp_file(name):
     delay, do_not_delay, no_delay_effect = True, False, None
 
     if isinstance(name, bytes):
-        name = name.decode(locale.getpreferredencoding() or 'utf-8')
+        try:
+            name = unicode(name, encoding='utf-8')
+        except TypeError:
+            name = name.decode(locale.getpreferredencoding() or 'utf-8')
 
     # Default ignored suffixes already handle .bak, .tmp, etc..
     if name.endswith(Options.ignored_suffixes):
