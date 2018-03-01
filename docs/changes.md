ee4bbba72b0a4e0919af5170e5d130ca806d1c18
Mickaël Schoentgen <mschoentgen@nuxeo.com>
2018-03-01 09:02:15 +0100

    Jenkins: Possibility to launch the release job on a given beta version

--- docs/changes.md
+++ docs/changes.md
@@ -21,7 +21,7 @@ Release date: `2018-??-??`
 #### Minor changes
 - Framework: Use ecm:isVersion instead of ecm:isCheckedInVersion
 - Jenkins: Possibility to launch the beta job on a given branch
-- Jenkins: Possibility to launch the release job on a given branch
+- Jenkins: Possibility to launch the release job on a given beta version
 - Packaging: Updated `pypiwin32` from 219 to 223
 
 
--- tools/deploy.sh
+++ tools/deploy.sh
@@ -7,13 +7,16 @@
 #
 
 release() {
-    # Takes the beta version to release as 1st argument.
+    # Take the beta version to release as 1st argument.
     local latest_release
     local drive_version
     local release_url
     local path
     local dmg
     local exe
+    local ext_mac
+    local ext_win
+    local ext_win_only
 
     drive_version="$1"
     latest_release="release-${drive_version}"
@@ -23,9 +26,20 @@ release() {
         exit 1
     fi
 
+    if version_gt "${drive_version}" "3.0.99"; then
+        # Starting with 3.1.0, installers changed in various ways (new extension, new name).
+        ext_win_only="exe"
+        ext_win=".${ext_win_only}"
+        ext_mac=".dmg"
+    else
+        ext_win_only="msi"
+        ext_win="-win32.${ext_win_only}"
+        ext_mac="-osx.dmg"
+    fi
+
     path="/var/www/community.nuxeo.com/static"
-    dmg="${path}/drive/nuxeo-drive-${drive_version}-osx.dmg"
-    exe="${path}/drive/nuxeo-drive-${drive_version}-win32.exe"
+    dmg="${path}/drive/nuxeo-drive-${drive_version}${ext_mac}"
+    exe="${path}/drive/nuxeo-drive-${drive_version}${ext_win}"
 
     echo ">>> [${latest_release}] Deploying to the production website"
     ssh -T nuxeo@lethe.nuxeo.com <<EOF
@@ -34,7 +48,7 @@ cp -vf ${path}/drive-tests/*${drive_version}* ${path}/drive
 
 # Create symbolic links of the latest packages
 ln -sfrv ${dmg} ${path}/drive/latest/nuxeo-drive.dmg
-ln -sfrv ${exe} ${path}/drive/latest/nuxeo-drive.exe
+ln -sfrv ${exe} ${path}/drive/latest/nuxeo-drive.${ext_win_only}
 
 # Create symbolic links of the latest packages for all supported versions of Nuxeo
 for folder in \$(find ${path}/drive/latest -maxdepth 1 -type d); do
@@ -56,6 +70,11 @@ EOF
     release_url="$(grep '"url"' prerelease.json | head -1 | cut -d'"' -f4)"
     echo "Pre-release URL: ${release_url}"
     curl -X PATCH -i -n -d '{ "prerelease": false }' "${release_url}"
+}
+
+function version_gt() {
+    # Compare 2 versions and return a boolean stating if the 1st one is greater than the 2nd.
+    test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1"
 }
 
 release "$@"
