459deabf7d61831bc33c064bc75c4df9a925d12e
Mickaël Schoentgen <mschoentgen@nuxeo.com>
2018-04-05 16:34:55 +0200

    ff

--- nuxeo-drive-client/nxdrive/client/local_client.py
+++ nuxeo-drive-client/nxdrive/client/local_client.py
@@ -21,7 +21,7 @@ from .common import (BaseClient, DuplicationDisabledError,
                      FILE_BUFFER_SIZE, NotFound,
                      UNACCESSIBLE_HASH, safe_filename)
 from ..options import Options
-from ..utils import (guess_digest_algorithm, normalized_path,
+from ..utils import (guess_digest_algorithm, force_decode, normalized_path,
                      safe_long_path)
 
 # from typing import List, Optional, Text, Tuple, Union
@@ -502,7 +502,7 @@ FolderType=Generic
         # type: (Text, Text) -> bool
         """ Note: added parent_ref to be able to filter on size if needed. """
 
-        file_name = file_name.lower()
+        file_name = force_decode(file_name.lower())
 
         if (file_name.endswith(Options.ignored_suffixes)
                 or file_name.startswith(Options.ignored_prefixes)):
@@ -790,6 +790,8 @@ FolderType=Generic
                 ctime = datetime.strptime(ctime, '%Y-%m-%d %H:%M:%S')
 
             if sys.platform == 'darwin':
+                if isinstance(filename, unicode):
+                    filename = filename.encode('utf8')
                 os.system('SetFile -d "{}" "{}"'.format(
                     ctime.strftime('%m/%d/%Y %H:%M:%S'), filename))
             elif sys.platform == 'win32':
