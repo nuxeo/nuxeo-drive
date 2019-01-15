2804695d05945ab2589db8a672ed2e3bbe893004
Léa Klein <34026167+LKleinNux@users.noreply.github.com>
2019-01-15 18:11:00 +0100

    NXDRIVE-1496: Bump version to 4.1.0

--- nxdrive/__init__.py
+++ nxdrive/__init__.py
@@ -26,7 +26,7 @@ To declare a beta, use this schema:
 """
 
 __author__ = "Nuxeo"
-__version__ = "4.0.4"
+__version__ = "4.1.0"
 __copyright__ = """
     Copyright Nuxeo (https://www.nuxeo.com) and others.
 
