6c6e9788bf8ef06dbf1b2962e2b2684fbfb3ebc8
Mickaël Schoentgen <mschoentgen@nuxeo.com>
2019-01-18 14:22:33 +0100

    NXDRIVE-1475: Dot not send Mock'ed exceptions from tests
    Prevent Sentry events pollution.

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,7 +4,7 @@ import os
 import shutil
 import sys
 import tempfile
-from contextlib import contextmanager
+from contextlib import contextmanager, suppress
 from pathlib import Path
 from typing import Any, Dict, List, Tuple, Union
 
@@ -23,6 +23,23 @@ from nxdrive.options import Options
 from nxdrive.utils import force_encode, safe_filename
 
 
+def before_send(event: Any, hint: Any) -> Any:
+    """
+    Alter an event before sending to the Sentry server.
+    The event will not be sent if None is returned.
+    """
+
+    # Do not send Mock'ed exceptions to not pollute Sentry events
+    with suppress(KeyError):
+        for thread in event["threads"]:
+            for frame in thread["stacktrace"]["frames"]:
+                for value in frame["vars"].values():
+                    if "mock" in value.lower():
+                        return None
+
+    return event
+
+
 def setup_sentry() -> None:
     """ Setup Sentry. """
     if os.getenv("SKIP_SENTRY", "0") == "1":
@@ -39,7 +56,11 @@ def setup_sentry() -> None:
     from nxdrive import __version__ as version
 
     sentry_sdk.init(
-        dsn=sentry_dsn, environment="testing", release=version, attach_stacktrace=True
+        dsn=sentry_dsn,
+        environment="testing",
+        release=version,
+        attach_stacktrace=True,
+        before_send=before_send,
     )
 
 
