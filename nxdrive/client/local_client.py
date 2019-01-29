85a5ac21dd759775a45637282782fe268351a948
Mickaël Schoentgen <mschoentgen@nuxeo.com>
2019-01-29 12:32:06 +0100

    NXDRIVE-1530: Handle PermissionError when trashing documents
    Also lower logging in LocalClient.delete() on error.

--- nxdrive/client/local_client.py
+++ nxdrive/client/local_client.py
@@ -569,7 +569,7 @@ FolderType=Generic
         try:
             send2trash(str(os_path))
         except OSError as exc:
-            log.exception(f"Cannot trash {os_path!r}")
+            log.warning(f"Cannot trash {os_path!r}")
             with suppress(Exception):
                 # WindowsError(None, None, path, retcode)
                 _, _, _, retcode = exc.args
