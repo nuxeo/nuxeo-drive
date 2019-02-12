9cba9f69e4088174712569dff65058490276e74c
Léa Klein <lklein@nuxeo.com>
2019-02-12 09:53:54 +0100

    NXDRIVE-1538: Remove Win7 bug workaround

--- nxdrive/objects.py
+++ nxdrive/objects.py
@@ -66,13 +66,9 @@ class RemoteFileInfo:
         folderish = fs_item.get("folder", False)
 
         timestamp = fs_item.get("lastModificationDate")
-        last_update = (
-            datetime.fromtimestamp(max(86400, timestamp // 1000)) if timestamp else None
-        )
+        last_update = datetime.fromtimestamp(timestamp // 1000) if timestamp else None
         timestamp = fs_item.get("creationDate")
-        creation = (
-            datetime.fromtimestamp(max(86400, timestamp // 1000)) if timestamp else None
-        )
+        creation = datetime.fromtimestamp(timestamp // 1000) if timestamp else None
 
         if folderish:
             digest = None
