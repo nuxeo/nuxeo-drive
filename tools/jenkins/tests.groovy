e4a7c860ddf9001c48e25e4678365c3d44c9e3f3
Léa Klein <lklein@nuxeo.com>
2019-05-20 14:14:25 +0200

    NXBT-2866: Use StringParameterValue for ChoiceParameterDefinition

--- tools/jenkins/tests.groovy
+++ tools/jenkins/tests.groovy
@@ -215,11 +215,11 @@ for (def x in slaves.keySet()) {
             build job: job_name, parameters: [
                 [$class: 'StringParameterValue', name: 'SPECIFIC_TEST', value: params.SPECIFIC_TEST],
                 [$class: 'StringParameterValue', name: 'PYTEST_ADDOPTS', value: params.PYTEST_ADDOPTS],
-                [$class: 'ChoiceParameterValue', name: 'RANDOM_BUG_MODE', value: params.RANDOM_BUG_MODE],
+                [$class: 'StringParameterValue', name: 'RANDOM_BUG_MODE', value: params.RANDOM_BUG_MODE],
                 [$class: 'StringParameterValue', name: 'ENGINE', value: params.ENGINE],
                 [$class: 'BooleanParameterValue', name: 'CLEAN_WORKSPACE', value: params.CLEAN_WORKSPACE],
                 [$class: 'StringParameterValue', name: 'BRANCH_NAME', value: env.BRANCH_NAME],
-                [$class: 'ChoiceParameterValue', name: 'OPERATING_SYSTEM', value: label]
+                [$class: 'StringParameterValue', name: 'OPERATING_SYSTEM', value: label]
             ]
 
             if (job.result == "SUCESS") {
