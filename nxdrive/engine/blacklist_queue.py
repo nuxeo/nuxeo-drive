501c6bb53c6921eb19db4cfea6a5325768a0a901
Mickaël Schoentgen <mschoentgen@nuxeo.com>
2019-05-31 18:24:21 +0200

    NXDRIVE-1615: Fix BlackListQueue tests by casting time.monotonic() to an integer

--- nxdrive/engine/blacklist_queue.py
+++ nxdrive/engine/blacklist_queue.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-import time
+from time import monotonic
 from logging import getLogger
 from threading import Lock
 from typing import Dict, Generator
@@ -9,19 +9,31 @@ log = getLogger(__name__)
 
 
 class BlacklistItem:
-    def __init__(self, item_id: str, item: str, next_try: int = 30) -> None:
-        self.uid = item_id
-        self._item = item
+    def __init__(self, uid: str, name: str, next_try: int = 30) -> None:
+        self.uid = uid
+        self.name = name
         self._interval = next_try
 
-        self._next_try = self._interval + int(time.monotonic())
+        self._next_try = self._interval + int(monotonic())
         self.count = 1
+
+    def __repr__(self) -> str:
+        return f"<{type(self).__name__} uid={self.uid!r}, name={self.name!r}, count={self.count}>"
+
+    def __str__(self) -> str:
+        return repr(self)
 
     def check(self, cur_time: int) -> bool:
         return cur_time > self._next_try
 
-    def get(self):
-        return self._item
+    def increase(self, next_try: int = None) -> None:
+        # Only used in tests, but it is more practical to keep there.
+        self.count += 1
+        cur_time = int(monotonic())
+        if next_try is not None:
+            self._next_try = next_try + cur_time
+        else:
+            self._next_try = self.count * self._interval + cur_time
 
 
 class BlacklistQueue:
@@ -31,16 +43,28 @@ class BlacklistQueue:
         self._queue: Dict[str, BlacklistItem] = dict()
         self._lock = Lock()
 
-    def push(self, id_obj: str, obj: str) -> None:
-        log.debug(f"Blacklisting {obj!r}")
-        item = BlacklistItem(item_id=id_obj, item=obj, next_try=self._delay)
+    def __repr__(self) -> str:
+        return f"<{type(self).__name__} queue_size={len(self._queue)}>"
+
+    def __str__(self) -> str:
+        return repr(self)
+
+    def push(self, uid: str, name: str) -> None:
         with self._lock:
+            item = BlacklistItem(uid, name, next_try=self._delay)
+            log.debug(f"Blacklisting {item!r}")
+            self._queue[item.uid] = item
+
+    def repush(self, item: BlacklistItem, increase_wait: bool = True) -> None:
+        # Only used in tests, but it is more practical to keep there.
+        with self._lock:
+            item.increase(next_try=None if increase_wait else self._delay)
             self._queue[item.uid] = item
 
     def get(self) -> Generator[BlacklistItem, None, None]:
-        cur_time = int(time.monotonic())
         with self._lock:
+            cur_time = int(monotonic())
             for item in self._queue.copy().values():
                 if item.check(cur_time):
-                    del self._queue[item.uid]
-                    yield item
+                    log.debug(f"Whitelisting {item!r}")
+                    yield self._queue.pop(item.uid)
--- tests/unit/test_blacklist_queue.py
+++ tests/unit/test_blacklist_queue.py
@@ -1,32 +1,11 @@
 # coding: utf-8
-from time import monotonic, sleep
+from time import sleep
 
 from nxdrive.engine.blacklist_queue import BlacklistItem, BlacklistQueue
 
 
-def increase(item: BlacklistItem, next_try: int = None) -> None:
-    item.count += 1
-    cur_time = int(monotonic())
-    if next_try is not None:
-        item._next_try = next_try + cur_time
-    else:
-        item._next_try = item.count * item._interval + cur_time
-
-
-def repush(
-    queue: BlacklistQueue, item: BlacklistItem, increase_wait: bool = True
-) -> None:
-    if increase_wait:
-        increase(item)
-    else:
-        increase(item, next_try=queue._delay)
-
-    with queue._lock:
-        queue._queue[item.uid] = item
-
-
 def test_delay():
-    sleep_time = 4
+    sleep_time = 3
 
     # Push two items with a delay of 1s
     queue = BlacklistQueue(delay=1)
@@ -39,31 +18,33 @@ def test_delay():
 
     # Verfiy we get the two items now
     item = next(queue.get())
-    assert item.get() == "Item1"
+    assert isinstance(item, BlacklistItem)
+    assert item.name == "Item1"
     assert item.uid == "1"
     item = next(queue.get())
-    assert item.get() == "Item2"
+    assert item.name == "Item2"
     assert item.uid == "2"
     assert item.count == 1
 
     # Repush item without increasing delay
-    repush(queue, item, increase_wait=False)
+    queue.repush(item, increase_wait=False)
     assert not list(queue.get())
     sleep(sleep_time)
 
     # We should get the repushed item after 1s wait
     item = next(queue.get())
-    assert item.get() == "Item2"
+    assert item.name == "Item2"
     assert item.uid == "2"
     assert item.count == 2
 
     # Repush item with increase
-    repush(queue, item, increase_wait=True)
+    queue.repush(item, increase_wait=True)
     sleep(sleep_time)
     assert not list(queue.get())
+
     sleep(sleep_time)
     item = next(queue.get())
-    assert item.get() == "Item2"
+    assert item.name == "Item2"
     assert item.uid == "2"
     assert item.count == 3
     assert not list(queue.get())
