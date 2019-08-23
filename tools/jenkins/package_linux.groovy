6c092a87071d5fcc335da43b0fa80f5a9802b1f8
Mickaël Schoentgen <mschoentgen@nuxeo.com>
2019-08-23 10:44:51 +0200

    NXDRIVE-1755: [GNU/Linux] Check AppImage requirements
    - Fixed the .desktop file name and location.
    - Added the appdata.xml file.
    - Added AppImage conformity checks.
    - Added manual steps to build the AppImage in comments of the jenkinsfile.
    - Added a nice preview picture in the README (as required by the AppImage too).

--- tools/jenkins/package_linux.groovy
+++ tools/jenkins/package_linux.groovy
@@ -6,9 +6,27 @@
 Connect to the container:
     docker run -it --rm centos:7.2.151 /bin/bash
 
+Manual steps:
+    # Setup
+    yum install -y git-core bzip2-devel libffi-devel openssl-devel readline-devel sqlite-devel xz-devel zlib-devel mesa-libGL desktop-file-utils file findutils gcc libappstream-glib make which wget zip
+    git clone --branch master --depth=1 https://github.com/nuxeo/nuxeo-drive.git
+    cd nuxeo-drive
+    export WORKSPACE="$(pwd)"
+    ./tools/linux/deploy_jenkins_slave.sh --install-release
+
+    # Build
+    ./tools/linux/deploy_jenkins_slave.sh --build
+
+    # Check
+    wget https://github.com/AppImage/pkg2appimage/raw/master/excludelist
+    wget https://github.com/AppImage/pkg2appimage/raw/master/appdir-lint.sh
+    bash appdir-lint.sh dist/AppRun
+    appstream-util validate-relax dist/AppRun/usr/share/metainfo/*.appdata.xml
+
 Retrieve a file:
     # docker cp CONTAINER:/nuxeo-drive/dist/nuxeo-drive-linux-4.1.4.zip .
     container_id="$(docker ps | grep centos:7.2.1511 | head -1 | cut -d" " -f1)"
+    docker cp ${container_id}:/nuxeo-drive/dist/nuxeo-drive-4.1.4-x86_64.AppImage .
     docker cp ${container_id}:/nuxeo-drive/dist/nuxeo-drive-linux-4.1.4.zip .
 */
 
@@ -57,6 +75,9 @@ pipeline {
 
                 // General
                 sh 'yum install -y file findutils gcc make wget zip'
+
+                // Needed by AppImage conformity tools
+                sh 'yum install -y desktop-file-utils libappstream-glib which'
 
                 // Nuxeo Drive requirements
                 dir('nuxeo-drive') {
--- tools/linux/deploy_jenkins_slave.sh
+++ tools/linux/deploy_jenkins_slave.sh
@@ -9,9 +9,9 @@ export OSI="linux"
 
 remove_blacklisted_files() {
     # Remove blacklisted files known as having bad side effects
-    local appdir="$1"
+    local app_dir="$1"
 
-    echo ">>> [${appdir}] Removing blacklisted files"
+    echo ">>> [${app_dir}] Removing blacklisted files"
 
     [ -f excludelist ] && rm -fv excludelist
     wget "https://raw.githubusercontent.com/AppImage/pkg2appimage/master/excludelist"
@@ -19,7 +19,7 @@ remove_blacklisted_files() {
     while IFS= read -r line; do
         file="$(echo "${line}" | cut -d' ' -f1)"
         if [ ! "${file}" = "" ] && [ ! "${file}" = "#" ]; then
-            [ -f "${appdir}/${file}" ] && rm -fv "${appdir}/${file}"
+            [ -f "${app_dir}/${file}" ] && rm -fv "${app_dir}/${file}"
         fi
     done < excludelist
 
@@ -29,27 +29,46 @@ remove_blacklisted_files() {
 create_package() {
     # Create the final AppImage
     local app_name="nuxeo-drive"
+    local app_id="org.nuxeo.drive"
     local app_version="$(python tools/changelog.py --drive-version)"
-    local appdir="dist/AppRun"
+    local app_dir="dist/AppRun"
 
     echo ">>> [AppImage] Adjusting file names to fit in the AppImage"
     # Taken from https://gitlab.com/scottywz/ezpyi/blob/master/ezpyi
-    [ -d "${appdir}" ] && rm -rfv "${appdir}"
-    mv -v dist/ndrive "${appdir}"
-    mv -v "${appdir}/ndrive" "${appdir}/AppRun"
+    [ -d "${app_dir}" ] && rm -rfv "${app_dir}"
+    mv -v "dist/ndrive" "${app_dir}"
+    mv -v "${app_dir}/ndrive" "${app_dir}/AppRun"
+
+    echo ">>> [AppImage] Copying icons"
+    cp -v "tools/linux/app_icon.svg" "${app_dir}/.DirIcon.svg"
+    cp -v "tools/linux/app_icon.svg" "${app_dir}/.icon.svg"
 
     echo ">>> [AppImage] Copying metadata files"
-    cp -v tools/linux/app.desktop "${appdir}/.desktop"
-    cp -v tools/linux/app_icon.svg "${appdir}/.DirIcon.svg"
-    cp -v tools/linux/app_icon.svg "${appdir}/.icon.svg"
+    mkdir -pv "${app_dir}/usr/share/metainfo"
+    cp -v "tools/linux/${app_id}.appdata.xml" "${app_dir}/usr/share/metainfo"
+    mkdir -pv "${app_dir}/usr/share/applications"
+    cp -v "tools/linux/${app_id}.desktop" "${app_dir}/usr/share/applications"
+    ln -srv "${app_dir}/usr/share/applications/${app_id}.desktop" "${app_dir}/${app_id}.desktop"
 
     echo ">>> [AppImage] Downloading the AppImage tool"
-    wget https://github.com/AppImage/AppImageKit/releases/download/12/appimagetool-x86_64.AppImage
-    chmod a+x appimagetool-x86_64.AppImage
+    wget "https://github.com/AppImage/AppImageKit/releases/download/12/appimagetool-x86_64.AppImage"
+    chmod -v a+x "appimagetool-x86_64.AppImage"
     ./appimagetool-x86_64.AppImage --appimage-extract
 
     echo ">>> [AppImage] Creating the AppImage file"
-    ./squashfs-root/AppRun --no-appstream "${appdir}" "dist/${app_name}-${app_version}-x86_64.AppImage"
+    # --no-appstream because appstreamcli is not easily installable on CentOS
+    ./squashfs-root/AppRun --no-appstream "${app_dir}" "dist/${app_name}-${app_version}-x86_64.AppImage"
+
+    echo ">>> [AppImage] Downloading AppImage conformity tools"
+    wget "https://github.com/AppImage/pkg2appimage/raw/master/excludelist"
+    wget "https://github.com/AppImage/pkg2appimage/raw/master/appdir-lint.sh"
+
+    echo ">>> [AppImage] Checking the AppImage conformity"
+    bash appdir-lint.sh "${app_dir}"
+    appstream-util validate-relax "${app_dir}/usr/share/metainfo/${app_id}.appdata.xml"
+    echo "!!! Further checks needed, not usable on CentOS, keep it as example and regularly do it manually"
+    echo "appstreamcli validate '${app_dir}/usr/share/metainfo/${app_id}.appdata.xml'"
+    echo "appstreamcli validate-tree '${app_dir}'"
 }
 
 main "$@"
