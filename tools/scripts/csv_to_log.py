89c75b5b9f23c29bc3c62fd7789d83744294d846
Mickaël Schoentgen <mschoentgen@nuxeo.com>
2020-01-22 11:25:36 +0100

    NXDRIVE-2013: Create a script to convert NCO CSV log files to real log files

---
+++ tools/scripts/csv_to_log.py
@@ -0,0 +1,45 @@
+# coding: utf-8
+"""
+Convert a CSV "log" file to a real log file.
+Such files are ones attached to NCO tickets.
+"""
+import csv
+import sys
+from pathlib import Path
+from typing import List
+
+
+def convert(file: Path) -> Path:
+    """Convert a given CVS *file* to a simple log file."""
+    output = file.with_suffix(".log")
+    with file.open() as csvfile, output.open("w") as fout:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            # date,host,message
+            line = "{date} {message}\n".format(**row)
+            fout.write(line)
+    return output
+
+
+def main(files: List[str]) -> int:
+    """Handle given *files*."""
+    if not files:
+        print(__doc__)
+        print("Usage: python", Path(sys.argv[0]).name, "FILE [FILE...]")
+        return 1
+
+    errcode = 0
+    for file in files:
+        try:
+            output = convert(Path(file))
+            print(">>>", file, "->", output)
+        except Exception as exc:
+            # An error should not stop the entire process
+            print(" ! ", file, str(exc))
+            errcode = 2
+
+    return errcode
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))
