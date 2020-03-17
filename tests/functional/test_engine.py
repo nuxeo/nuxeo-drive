a6f068bde78f7b8e517559c315998be18db5d48b
Mickaël Schoentgen <mschoentgen@nuxeo.com>
2020-03-17 18:08:24 +0100

    NXDRIVE-1866: The conflict resolver should not fail when the Engine is not started

--- tests/functional/test_engine.py
+++ tests/functional/test_engine.py
@@ -1,10 +1,56 @@
 # coding: utf-8
 from datetime import datetime
 from pathlib import Path
+from unittest.mock import patch
 
 from nxdrive.client.local import FileInfo
 from nxdrive.constants import DelAction
+from nxdrive.exceptions import ThreadInterrupt
 from nxdrive.objects import RemoteFileInfo
+
+from .. import ensure_no_exception
+
+
+def test_conflict_resolver(manager_factory, tmp):
+    manager, engine = manager_factory()
+    dao = engine.dao
+
+    def bad(*args, **kwargs):
+        raise ThreadInterrupt("Mock'ed exception")
+
+    def doc_pair(name: str) -> int:
+        """Create a valid doc pair in the database and return the row ID."""
+        # Craft the local file
+        path = tmp()
+        file = path / name
+        file.parent.mkdir()
+        file.write_text("azerty")
+
+        finfo = FileInfo(path, file, False, datetime.now())
+        rowid = dao.insert_local_state(finfo)
+
+        # Edit pair states to mimic a synced document
+        doc_pair = dao.get_state_from_id(rowid)
+        doc_pair.local_state = "modified"
+        doc_pair.remote_state = "modified"
+        dao.update_local_state(doc_pair, finfo)
+        dao.update_remote_name(rowid, name)
+
+        return rowid
+
+    with ensure_no_exception(), manager:
+        # Test a conflict with an inexistent doc_pair
+        engine.conflict_resolver(42)
+
+        # Test a conflict
+        rowid = doc_pair("conflict")
+        engine.conflict_resolver(rowid, emit=False)
+
+        # Test a conflict when the Engine is stopped and the digest computation fails accordingly.
+        # It should not fail.
+        engine.stop()
+        with patch.object(engine.local, "is_equal_digests", new=bad):
+            engine.conflict_resolver(rowid, emit=False)
 
 
 def test_delete_doc(manager_factory, tmp):
