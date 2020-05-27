4b1e0dd9b89ee7040f240949a80e839d9159aa4f
Mickaël Schoentgen <mschoentgen@nuxeo.com>
2020-05-27 18:13:46 +0200

    NXDRIVE-2117: Use CircleCI for functional tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -73,6 +73,24 @@ def before_send(event: Any, hint: Any) -> Any:
     return event
 
 
+def disable_sentry_ssl_check():
+    # disable sni warnings
+    import urllib3
+
+    urllib3.disable_warnings()
+
+    def _get_pool_options(self, ca_certs):
+        return {
+            "num_pools": 2,
+            "cert_reqs": "CERT_NONE",
+        }
+
+    # disable ssl check
+    from sentry_sdk.transport import HttpTransport
+
+    HttpTransport._get_pool_options = _get_pool_options
+
+
 def setup_sentry() -> None:
     """ Setup Sentry. """
 
@@ -107,6 +125,7 @@ def setup_sentry() -> None:
     )
 
 
+disable_sentry_ssl_check()
 setup_sentry()
 
 
