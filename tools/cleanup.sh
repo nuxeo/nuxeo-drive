a1143c9e9d834fbd111f4941b9b8e80828240b6b
Mickaël Schoentgen <mschoentgen@nuxeo.com>
2020-07-31 17:33:39 +0200

    NXDRIVE-2259: Move the alpha cleaner job to GitHub Actions
    NXDRIVE-1709: Add a manual clean-up job for obsolete non-officially plublished versions

--- tools/cleanup.sh
+++ tools/cleanup.sh
@@ -2,49 +2,59 @@
 #
 # Delete old alpha releases.
 #
-# Warning: do not execute this script manually but from Jenkins.
-#
+
+purge() {
+    # $1: the version to remove
+    local version
+    local path
+
+    version="$1"
+    path="/var/www/community.nuxeo.com/static/drive-updates"
+
+    echo " - ${version}"
+    python3 tools/versions.py --delete "${version}"
+    ssh -o StrictHostKeyChecking=no -T nuxeo@lethe.nuxeo.com "rm -vf ${path}/alpha/*${version}.* ${path}/alpha/*${version}-*" || true
+    git tag --delete "alpha-${version}" || true
+    git push --delete origin "wip-alpha-${version}" || true  # branch
+    git push --delete origin "alpha-${version}" || true  # tag
+}
 
 main() {
-    local older_than
+    # $1 is an optional version to delete
     local path
     local release
-    local version
 
-    older_than=21
     path="/var/www/community.nuxeo.com/static/drive-updates"
 
     echo ">>> Installing requirements"
     python3 -m pip install --user pyyaml==5.3.1
 
     echo ">>> Retrieving versions.yml"
-    rsync -vz nuxeo@lethe.nuxeo.com:${path}/versions.yml .
+    rsync -e "ssh -o StrictHostKeyChecking=no" -vz nuxeo@lethe.nuxeo.com:"${path}/versions.yml" .
 
     echo ">>> Checking versions.yml integrity"
     python3 tools/versions.py --check || exit 1
 
-    echo ">>> Removing alpha versions older than ${older_than} days"
-    while IFS= read release; do
-        version="$(echo ${release} | sed s'/alpha-//')"
-        echo " - ${version}"
-        python3 tools/versions.py --delete "${version}"
-    done < <(git tag -l "alpha-*" --sort=-taggerdate | tail -n +${older_than})
+    if [ -n "$1" ]; then
+        if [ $(echo "$1" | tr -d -c '.' | wc -c) -gt 2 ]; then
+            echo ">>> Removing alpha version $1"
+            purge "$1"
+        else
+            echo " !! Invalid version number: $1 (not alpha)"
+            exit 0
+        fi
+    else
+        echo ">>> Removing alpha versions older than 21 days"
+        while IFS= read release; do
+            purge "$(echo ${release} | sed s'/alpha-//')"
+        done < <(git tag -l "alpha-*" --sort=-taggerdate | tail -n +21)
+    fi
 
     echo ">>> Checking versions.yml integrity"
     python3 tools/versions.py --check || exit 1
 
     echo ">>> Uploading versions.yml"
-    rsync -vz versions.yml nuxeo@lethe.nuxeo.com:${path}/
-
-    echo ">>> Removing binaries, tags and branches:"
-    while IFS= read release; do
-        version="$(echo ${release} | sed s'/alpha-//')"
-        echo " - ${version}"
-        ssh -T nuxeo@lethe.nuxeo.com "rm -vf ${path}/alpha/*${version}.* ${path}/alpha/*${version}-*" || true
-        git tag --delete "${release}" || true
-        git push --delete origin "wip-${release}" || true  # branch
-        git push --delete origin "${release}" || true  # tag
-    done < <(git tag -l "alpha-*" --sort=-taggerdate | tail -n +${older_than})
+    rsync -e "ssh -o StrictHostKeyChecking=no" -vz versions.yml nuxeo@lethe.nuxeo.com:"${path}/"
 }
 
-main
+main "$@"
