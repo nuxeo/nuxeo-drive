f841d99ace6f9c56c552187d462a62ab31dee610
Mickaël Schoentgen <mschoentgen@nuxeo.com>
2020-08-04 11:49:40 +0200

    NXDRIVE-2259: Adapt the Dockerfile for Travis-CI

--- tools/linux/entrypoint.sh
+++ tools/linux/entrypoint.sh
@@ -4,21 +4,13 @@ set -e
 if [ $# -gt 0 ]; then
     exec "$@"
 else
-    # On Travis-CI, the current path will be the same as $WORKSPACE. If it is the case,
-    # we already are into a cloned repository, so skipping the "git clone" step.
-    if [ -z "${WORKSPACE}" ]; then
-        export WORKSPACE="/opt"
-        echo "[build n°${BUILD_VERSION}] git clone --depth=1 --branch ${BRANCH_NAME:-master} ${GIT_URL} sources"
-        git clone --depth=1 --branch "${BRANCH_NAME:-master}" "${GIT_URL}" sources && cd sources
-    fi
+    export WORKSPACE="/opt"
+    export WORKSPACE_DRIVE="/opt/sources"
 
+    cd "${WORKSPACE_DRIVE}"
     ./tools/linux/deploy_jenkins_slave.sh --install-release
     ./tools/linux/deploy_jenkins_slave.sh --build
 
-    # Running outside Travis-CI, likely a local test
-    if [ "$(pwd)" = "/opt/sources" ]; then
-        echo "[build n°${BUILD_VERSION}] Copying interesting files into the volume"
-        cp -v dist/*.AppImage /opt/dist
-        cp -v dist/*.zip /opt/dist
-    fi
+    echo "[build n°${BUILD_VERSION}] Copying interesting files into the volume"
+    cp -v dist/*.AppImage /opt/dist
 fi
