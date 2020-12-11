6686c135a67a7c5e697c85c149396f83b1b369ad
Mickaël Schoentgen <mschoentgen@nuxeo.com>
2020-12-11 16:49:08 +0100

    NXDRIVE-2319: [Windows] Fix double long path prefix
    Also some improvements:
    - Remove useless `str()` cast for `os.path.abspath()`.
    - Use `os.fsdecode()` instead of `str.decode()`.

--- nxdrive/client/local/base.py
+++ nxdrive/client/local/base.py
@@ -549,9 +549,9 @@ class LocalClientMixin:
 
     def get_path(self, target: Path) -> Path:
         """ Relative path to the local client from an absolute OS path. """
-        # NXDRIVE-2319: using os.path.abspath() instead of .resolve and .absolute().
+        # NXDRIVE-2319: using os.path.abspath() instead of Path.resolve and Path.absolute().
         try:
-            return Path(os.path.abspath(str(target))).relative_to(self.base_folder)
+            return Path(os.path.abspath(target)).relative_to(self.base_folder)
         except ValueError:
             # From the Path.relative_to() doc: if the operation is not possible
             # (because this is not a subpath of the other path), raise ValueError.
