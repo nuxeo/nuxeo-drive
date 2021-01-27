445ac2c745737bbab9dcae64a050c26cb7f998dd
Romain Grasland <rgrasland@nuxeo.com>
2021-01-27 12:00:04 +0100

    NXDRIVE-2489: [Direct Transfer] Fix folder and sub-folders upload

--- nxdrive/client/uploader/direct_transfer.py
+++ nxdrive/client/uploader/direct_transfer.py
@@ -71,7 +71,7 @@ class DirectTransferUploader(BaseUploader):
                 {"title": doc_pair.local_name},
             )
             self.dao.update_remote_parent_path_dt(
-                str(file_path), item["path"], item["uid"]
+                file_path.as_posix(), item["path"], item["uid"]
             )
         else:
             # Only replace the document if the user wants to
