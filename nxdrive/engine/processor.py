33b1f0d49053fc73cc2f642efb720bbf2e25f4b5
Romain Grasland <rgrasland@nuxeo.com>
2021-04-29 16:45:36 +0200

    NXDRIVE-2624: Handle HTTP 408 errors (Request Timeout)
    Co-authored-by: Mickaël Schoentgen <mschoentgen@nuxeo.com>

--- nxdrive/engine/processor.py
+++ nxdrive/engine/processor.py
@@ -392,7 +392,7 @@ class Processor(EngineWorker):
                     with suppress(FileNotFoundError):
                         shutil.rmtree(tmp_folder)
                     self._postpone_pair(doc_pair, "Requested Range Not Satisfiable")
-                elif exc.status == 500:
+                elif exc.status in (408, 500):
                     self.increase_error(doc_pair, "SERVER_ERROR", exception=exc)
                 elif exc.status in (502, 503, 504):
                     log.warning("Server is unavailable", exc_info=True)
