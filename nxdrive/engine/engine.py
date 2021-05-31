nuxeo/nuxeo-drive

commit fd4ec12f250cdecbcc2238a8c1de7c957d9e1615

NXDRIVE-2641: Disable local folder checks and creation when the sync is disabled

nxdrive/engine/engine.py
@@ -185,14 +185,16 @@ class Engine(QObject):
         self.csv_dir = self._set_csv_dir_or_cleanup()
 
         if not binder:
+            self._setup_local_folder()
             if not self.server_url:
                 raise EngineInitError(self)
             self._check_https()
             self.remote = self.init_remote()
 
         self._create_queue_manager()
-        self._create_remote_watcher()
-        self._create_local_watcher()
+        if Feature.synchronization:
+            self._create_remote_watcher()
+            self._create_local_watcher()
 
         # Connect for sync start
         self.newQueueItem.connect(self._check_sync_start)
@@ -257,6 +259,10 @@ class Engine(QObject):
         self.queue_manager.newItem.connect(self.newQueueItem)
         self.queue_manager.newErrorGiveUp.connect(self.newError)
 
+        if not Feature.synchronization:
+            # Launch queue processors when the Engine started
+            self.started.connect(self.queue_manager.init_processors)
+
     def _create_local_watcher(self) -> None:
         self._local_watcher = LocalWatcher(self, self.dao)
         self.create_thread(self._local_watcher, "LocalWatcher")
@@ -301,8 +307,9 @@ class Engine(QObject):
         started = not self._stopped
         if started:
             self.stop()
-        self.dao.reinit_states()
-        self._check_root()
+        if Feature.synchronization:
+            self.dao.reinit_states()
+            self._check_root()
         self.download_dir = self._set_download_dir()
         if started:
             self.start()
@@ -1068,8 +1075,9 @@ class Engine(QObject):
 
     def _thread_finished(self) -> None:
         for thread in self._threads:
-            if thread in (self._local_watcher.thread, self._remote_watcher.thread):
-                continue
+            with suppress(AttributeError):
+                if thread in (self._local_watcher.thread, self._remote_watcher.thread):
+                    continue
             if thread.isFinished():
                 thread.quit()
                 self._threads.remove(thread)
@@ -1079,9 +1087,6 @@ class Engine(QObject):
 
     def start(self) -> None:
         log.info(f"Engine {self.uid} is starting")
-
-        if not self.check_fs_marker():
-            raise MissingXattrSupport(self.local_folder)
 
         # Checking root in case of failed migration
         self._check_root()
@@ -1258,6 +1263,25 @@ class Engine(QObject):
         }
         return self.remote_cls(*args, **kwargs)
 
+    def _setup_local_folder(self) -> None:
+        if not Feature.synchronization:
+            return
+
+        new_folder = not self.local_folder.is_dir()
+        if new_folder:
+            self.local_folder.mkdir(parents=True)
+        try:
+            self._check_fs(self.local_folder)
+        except InvalidDriveException:
+            if new_folder:
+                with suppress(OSError):
+                    self.local.unset_readonly(self.local_folder)
+                    self.local_folder.rmdir()
+            raise
+
+        if not self.check_fs_marker():
+            raise MissingXattrSupport(self.local_folder)
+
     def bind(self, binder: Binder, /) -> None:
         check_credentials = not binder.no_check
         check_fs = not (Options.nofscheck or binder.no_fscheck)
@@ -1270,17 +1294,7 @@ class Engine(QObject):
 
         # Check first if the folder is on a supported FS
         if check_fs:
-            new_folder = not self.local_folder.is_dir()
-            if new_folder:
-                self.local_folder.mkdir(parents=True)
-            try:
-                self._check_fs(self.local_folder)
-            except InvalidDriveException as exc:
-                if new_folder:
-                    with suppress(OSError):
-                        self.local.unset_readonly(self.local_folder)
-                        self.local_folder.rmdir()
-                raise exc
+            self._setup_local_folder()
 
         # Persist the user preference about the SSL behavior.
         # It can be tweaked via ca-bundle or ssl-no-verify options. But also
@@ -1349,6 +1363,9 @@ class Engine(QObject):
             self.send_metric("server", "protocol", "http->https")
 
     def _check_root(self) -> None:
+        if not Feature.synchronization:
+            return
+
         root = self.dao.get_state_from_local(ROOT)
         if root is None:
             if self.local_folder.is_dir():

nxdrive/engine/dao/sqlite.py
@@ -1359,7 +1359,7 @@ class EngineDAO(ConfigurationDAO):
 
             # This will be needed later
             sql = "SELECT max(ROWID) FROM States"
-            current_max_row_id = c.execute(sql).fetchone()[0]
+            current_max_row_id = c.execute(sql).fetchone()[0] or 0
 
             # Insert data in one shot
             query = (

nxdrive/gui/application.py
@@ -661,7 +661,7 @@ class Application(QApplication):
         msg = self.question(
             Translator.get("DRIVE_ROOT_DELETED_HEADER"),
             Translator.get(
-                "DRIVE_ROOT_DELETED", values=["engine.local_folder", APP_NAME]
+                "DRIVE_ROOT_DELETED", values=[engine.local_folder, APP_NAME]
             ),
             icon=qt.Critical,
         )

nxdrive/manager.py
@@ -879,6 +879,8 @@ class Manager(QObject):
             engine.local.unset_readonly(engine.local_folder)
             try:
                 shutil.rmtree(engine.local_folder)
+            except FileNotFoundError:
+                pass
             except OSError:
                 log.warning("Cannot purge local files", exc_info=True)
 

tests/functional/test_behavior.py
@@ -3,6 +3,7 @@ import os
 import pytest
 
 from nxdrive.exceptions import FolderAlreadyUsed
+from nxdrive.options import Options
 
 from .. import ensure_no_exception
 from ..markers import not_windows
@@ -42,7 +43,9 @@ def test_crash_no_engine_database(manager_factory):
         assert manager.engines
 
 
-def test_crash_engine_no_local_folder(manager_factory):
+@pytest.mark.parametrize("sync_enabled", [True, False])
+@Options.mock()
+def test_crash_engine_no_local_folder(manager_factory, sync_enabled):
     """
         Drive should not crash when the engine local folder is removed.  Traceback:
 
@@ -59,10 +62,13 @@ def test_crash_engine_no_local_folder(manager_factory):
     """
     import shutil
 
+    Options.feature_synchronization = sync_enabled
+
     manager, engine = manager_factory()
 
     engine.local.unset_readonly(engine.local_folder)
-    shutil.rmtree(engine.local_folder)
+    if sync_enabled:
+        shutil.rmtree(engine.local_folder)
     assert not engine.local_folder.is_dir()
 
     with manager:

tests/functional/test_context_menu.py
@@ -1,5 +1,7 @@
 import pytest
 
+from nxdrive.options import Options
+
 
 def test_ctx_menu_entry_inexistent_file(manager_factory):
     manager, engine = manager_factory()
@@ -8,7 +10,10 @@ def test_ctx_menu_entry_inexistent_file(manager_factory):
             manager.get_metadata_infos("/an inexistent folder/a file.bin")
 
 
+@Options.mock()
 def test_ctx_menu_entries(manager_factory):
+    Options.feature_synchronization = True
+
     manager, engine = manager_factory()
     with manager:
         local = engine.local

tests/functional/test_server_options.py
@@ -149,6 +149,8 @@ def test_features(feature, feat_name, default, manager_factory, tmp_path):
 @Options.mock()
 def test_delay_remote_watcher(app, manager_factory):
     """Check that the delay, when set from the server config, is taken into account in the RemoteWatcher."""
+    Options.feature_synchronization = True
+
     manager, engine = manager_factory()
     updater = ServerOptionsUpdater(manager)
 
@@ -202,10 +204,12 @@ def test_delay_remote_watcher(app, manager_factory):
 @Options.mock()
 def test_synchronization_enabled(manager_factory):
     """Check that synchronization_enabled also disables feature_synchronization."""
+    Options.feature_synchronization = False
+
     manager, engine = manager_factory()
     updater = ServerOptionsUpdater(manager)
 
-    def disabled():
+    def enabled():
         return {"synchronization_enabled": True}
 
     # Default values are False
@@ -213,8 +217,8 @@ def test_synchronization_enabled(manager_factory):
     assert Options.feature_synchronization is False
     assert Feature.synchronization is False
 
-    # Mimic the IT team disabling the option
-    with patch.object(engine.remote, "get_server_configuration", new=disabled):
+    # Mimic the IT team enabling the option
+    with patch.object(engine.remote, "get_server_configuration", new=enabled):
         updater._poll()
         assert Options.synchronization_enabled is True
         assert Options.feature_synchronization is True

tests/old_functional/common.py
@@ -441,11 +441,14 @@ class TwoUsersTest(TestCase):
 
         self.app.aboutToQuit.connect(manager.stop)
         engine.syncCompleted.connect(self.app.sync_completed)
-        engine._remote_watcher.remoteScanFinished.connect(
-            self.app.remote_scan_completed
-        )
-        engine._remote_watcher.changesFound.connect(self.app.remote_changes_found)
-        engine._remote_watcher.noChangesFound.connect(self.app.no_remote_changes_found)
+        if hasattr(engine, "_remote_watcher"):  # Sync is disabled
+            engine._remote_watcher.remoteScanFinished.connect(
+                self.app.remote_scan_completed
+            )
+            engine._remote_watcher.changesFound.connect(self.app.remote_changes_found)
+            engine._remote_watcher.noChangesFound.connect(
+                self.app.no_remote_changes_found
+            )
 
         engine_uid = engine.uid
         self._wait_sync[engine_uid] = True

commit 8d6a82ac2c1294788a1253cde4af174fec2a4a00

NXDRIVE-2640: Fix UNC path handling
Regression introduced with fd4ec12f250cdecbcc2238a8c1de7c957d9e1615.

nxdrive/engine/engine.py
@@ -184,7 +184,7 @@ class Engine(QObject):
         self.csv_dir = self._set_csv_dir_or_cleanup()
 
         if not binder:
-            self._setup_local_folder()
+            self._setup_local_folder(not Options.nofscheck)
             if not self.server_url:
                 raise EngineInitError(self)
             self._check_https()
@@ -1262,8 +1262,8 @@ class Engine(QObject):
         }
         return self.remote_cls(*args, **kwargs)
 
-    def _setup_local_folder(self) -> None:
-        if not Feature.synchronization:
+    def _setup_local_folder(self, check_fs: bool) -> None:
+        if not Feature.synchronization or not check_fs:
             return
 
         new_folder = not self.local_folder.is_dir()
@@ -1290,7 +1290,7 @@ class Engine(QObject):
 
         # Check first if the folder is on a supported FS
         if check_fs:
-            self._setup_local_folder()
+            self._setup_local_folder(check_fs)
 
         # Persist the user preference about the SSL behavior.
         # It can be tweaked via ca-bundle or ssl-no-verify options. But also