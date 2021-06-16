62b3ca029570c2e1229f61455f3108c3731dad7f
Mickaël Schoentgen <mschoentgen@nuxeo.com>
2021-06-16 14:53:10 +0200

    [Windows] Fix check-upgrade.bat

--- tools/windows/scripts/check-upgrade.bat
+++ tools/windows/scripts/check-upgrade.bat
@@ -3,7 +3,7 @@ rem Test the auto-upgrade process.
 
 call envvars.bat
 
-%python% tools\scripts\check_update_process.py
+powershell -ExecutionPolicy Bypass .\tools\windows\deploy_ci_agent.ps1 -check_upgrade
 
 echo.
 pause
