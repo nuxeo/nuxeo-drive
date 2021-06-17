f4c56a13d63fcf962a4a5c707d9ddd1423e30a22
Mickaël Schoentgen <mschoentgen@nuxeo.com>
2021-06-17 11:50:12 +0200

    NXDRIVE-2505: Use the original packages folder on the server

--- tools/env.sh
+++ tools/env.sh
@@ -1,4 +1,4 @@
 # shellcheck shell=bash
 # Where to store binaries on the packages server
-export REMOTE_PATH_PROD="/var/www/community.nuxeo.com/static/drive-updates-gh"
-export REMOTE_PATH_STAGING="/var/www/community.nuxeo.com/static/drive-staging-gh"
+export REMOTE_PATH_PROD="/var/www/community.nuxeo.com/static/drive-updates"
+export REMOTE_PATH_STAGING="/var/www/community.nuxeo.com/static/drive-staging"
