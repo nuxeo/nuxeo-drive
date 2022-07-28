2156292837b9536c94176e44b9bbd34fb5bba163
mr-shekhar <85621953+mr-shekhar@users.noreply.github.com>
2022-07-28 15:53:21 +0530

    adding verbose option

--- tools/osx/deploy_ci_agent.sh
+++ tools/osx/deploy_ci_agent.sh
@@ -10,6 +10,7 @@ export OSI="osx"
 
 # Global variables
 CODESIGN="codesign                              \
+    --dvvv
     --options runtime                           \
     --timestamp                                 \
     --entitlements tools/osx/entitlements.plist \
