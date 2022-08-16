f8b20d2ba0be976f070eac5406b18b0cd76e1cfb
Sweta Yadav <Sweta.Yadav@hyland.com>
2022-08-16 11:17:08 +0530

    NXDRIVE-2768: Upgrade mac latest version

--- nxdrive/metrics/utils.py
+++ nxdrive/metrics/utils.py
@@ -1,4 +1,5 @@
 import platform
+import os
 from functools import lru_cache
 from typing import Tuple
 
@@ -17,6 +18,7 @@ def _get_current_os_details() -> Tuple[str, str, str]:
     """
     if MAC:
         name = "macOS"
+        os.environ["SYSTEM_VERSION_COMPAT"] = "0"
         ver_full = platform.mac_ver()[0]  # 10.15.3
     elif WINDOWS:
         name = "Windows"
