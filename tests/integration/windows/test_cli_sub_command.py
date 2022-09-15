848f5dd155a6840c77fdaa9abae4a4ba4dfd5c58
Sweta Yadav <Sweta.Yadav@hyland.com>
2022-09-15 16:44:04 +0530

    Integration test fix (test_unbind_server)

--- tests/integration/windows/test_cli_sub_command.py
+++ tests/integration/windows/test_cli_sub_command.py
@@ -70,17 +70,18 @@ def test_bind_server_missing_arguments(exe, args):
 
 
 @pytest.mark.parametrize(
-    "folder", ["%temp%\\Lea$", "%temp%\\this folder is good enough"]
+    "folder", ["%temp%\\Léa$", "%temp%\\this folder is good enough こん ツリ ^^"]
 )
 def test_unbind_server(nuxeo_url, exe, folder):
     """Will also test clean-folder."""
     expanded_folder = os.path.expandvars(folder)
+    print(f">>>> expanded_folder: {expanded_folder}, is folder: {os.path.isdir(expanded_folder)} ")
     local_folder = f'--local-folder="{folder}"'
-    nuxeo_test_pwd = f'--password="{env.NXDRIVE_TEST_PASSWORD}"'
-    args = f"{env.NXDRIVE_TEST_USERNAME} {nuxeo_url} {local_folder} {nuxeo_test_pwd}"
+    args = f"{env.NXDRIVE_TEST_USERNAME} {nuxeo_url} {local_folder}"
 
     try:
         assert bind(exe, args)
+        print(f">>> is folder: {os.path.isdir(expanded_folder)}, is file: {os.path.isdir(expanded_folder)}")
         assert os.path.isdir(expanded_folder)
         assert unbind(exe, local_folder)
     finally:
