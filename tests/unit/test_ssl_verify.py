772de134cd1608dede3227be2beff84710893f15
gitofanindya <Anindya.Roy@hyland.com>
2022-11-25 16:10:40 +0530

    NXDRIVE-1999: Direct Transfer let user select document type

---
+++ tests/unit/test_ssl_verify.py
@@ -0,0 +1,23 @@
+import pytest
+
+from nxdrive.options import Options
+from nxdrive.utils import get_verify
+
+
+@pytest.mark.parametrize(
+    "raw_value, expected_value",
+    [
+        (False, True),
+        (True, False),
+    ],
+)
+# @Options.mock()
+def test_get_verify(raw_value, expected_value):
+    old_ssl_no_verify = Options.ssl_no_verify
+    Options.ssl_no_verify = raw_value
+    verify = get_verify()
+    if raw_value or Options.ca_bundle:
+        assert verify is expected_value
+    else:
+        assert verify is not expected_value
+    Options.ssl_no_verify = old_ssl_no_verify
