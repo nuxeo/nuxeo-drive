9d4938e3734374cc502d06c42079a4f1fe466393
Sweta Yadav <106366788+swetayadav1@users.noreply.github.com>
2023-01-17 20:21:23 +0530

    Update FinderSync.swift

--- tools/osx/drive/NuxeoFinderSync/FinderSync.swift
+++ tools/osx/drive/NuxeoFinderSync/FinderSync.swift
@@ -24,7 +24,7 @@ class FinderSync: FIFinderSync {
     var title1 = "Access online"
     var title2 = "Copy share-link"
     var title3 = "Edit metadata"
-    var title4 = "Upload content"
+    var title4 = "Nuxeo Drive Upload Content"
 
     let fileStatus = FileStatus()
     let addr = "127.0.0.1"
