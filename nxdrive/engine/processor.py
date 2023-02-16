7049184b8b3aae49740d57218a4def5b3328d0dd
gitofanindya <Anindya.Roy@hyland.com>
2023-02-16 15:14:10 +0530

    duplicate

--- nxdrive/engine/processor.py
+++ nxdrive/engine/processor.py
@@ -1,4 +1,5 @@
 import errno
+import os
 import shutil
 import sqlite3
 import sys
@@ -1462,12 +1463,14 @@ class Processor(EngineWorker):
         local_parent_path = parent_pair.local_path
         self._unlock_readonly(local_parent_path)
         try:
-            if doc_pair.folderish:
-                if "Workspaces - " in name:
-                    partitioned_name = name.partition("Workspaces - ")
-                    name = partitioned_name[2]
+            if "Workspaces - " in name:
+                partitioned_name = name.partition("Workspaces - ")
+                new_name = partitioned_name[2]
+                folder_path = os.path.join(local_parent_path, new_name)
+                if not self.local.exists(folder_path):
+                    name = new_name
                 log.info(
-                    f"Creating local folder {name!r} "
+                    f"Creating local folder {name!r}s"
                     f"in {self.local.abspath(local_parent_path)!r}"
                 )
                 return self.local.make_folder(local_parent_path, name)
