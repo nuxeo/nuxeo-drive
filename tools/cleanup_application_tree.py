bac4cb02777ddf1395b1b5f08800ea6103ac1d2a
Sweta Yadav <Sweta.Yadav@hyland.com>
2023-02-17 09:20:50 +0530

    NXDRIVE-2822: Fix sync errors with S3 enabled on drive 5.3.0

--- tools/cleanup_application_tree.py
+++ tools/cleanup_application_tree.py
@@ -98,10 +98,13 @@ FILES: Tuple[str] = (
     "boto3/data/s[0-24-9a-z]*",  # Keep s3*
     "boto3/examples",
     # Botocore has a lot of useless files (only S3 and endpoints.json are interesting)
-    "botocore/data/[0-9a-df-rt-z]*",
+    "botocore/data/[0-9a-df-oq-rt-z]*",
     "botocore/data/e[a-mo-z]*",  # Keep en*
     "botocore/data/en[a-c-e-z]*",  # Keep only end*
-    "botocore/data/s[0-24-9a-z]*",  # Keep s3*
+    "botocore/data/s[0-24-9a-ce-z]*",  # Keep s3*
+    "botocore/data/sd[a-jl-z]*",
+    "botocore/data/p[b-z]*",
+    "botocore/data/pa[a-qs-z]*",
 )
 
 
