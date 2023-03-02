200a407ab3bfe6828b1730337b7463e8754af4bf
gitofanindya <Anindya.Roy@hyland.com>
2023-03-02 15:14:04 +0530

    NXDRIVE-2801: Do not prefix synchronized folder name with ancestors when there is no duplicated root NEW1

--- tests/functional/test_create_sync_folder.py
+++ tests/functional/test_create_sync_folder.py
@@ -10,12 +10,19 @@ def test_create_folder(manager_factory, tmp_path):
     test_processor = Processor(engine, "")
     dp = DocPair
     dp.local_path = tmp_path
+    dp.remote_ref = "defaultSyncRootFolderItemFactory"
+    dp.folderish = True
     create_rem = test_processor._create_remotely
+
     full_path = create_rem(dp, dp, "default - domain - Workspaces - Administrator")
     assert str(path.join(tmp_path, "Administrator")) == str(full_path)
-    full_path_duplicated = create_rem(
-        dp, dp, "default - domain1 - Workspaces - Administrator"
-    )
+
+    full_path = create_rem(dp, dp, "default - domain1 - Workspaces - Administrator")
     assert str(
         path.join(tmp_path, "default - domain1 - Workspaces - Administrator")
-    ) == str(full_path_duplicated)
+    ) == str(full_path)
+
+    dp.remote_ref = "defaultFileSystemItemFactory"
+
+    full_path = create_rem(dp, dp, "Administrator")
+    assert str(path.join(tmp_path, "Administrator_1")) == str(full_path)
