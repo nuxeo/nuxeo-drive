200bdd5a3d3ba213929e38f06afc72ae113e0df2
Sweta Yadav <sweta.yadav@del1-lmc-n80110.local>
2023-03-29 11:42:45 +0530

    NXDRIVE-2764: Fix handling of special and non-English characters in document names

--- nxdrive/utils.py
+++ nxdrive/utils.py
@@ -521,6 +521,13 @@ def safe_filename(name: str, /, *, replacement: str = "-") -> str:
     """Replace forbidden characters (at the OS and Nuxeo levels) for a given *name*.
     See benchmarks/test_safe_filename.py for the best implementation.
     """
+    if MAC:
+        return (
+            (name)
+            .replace('"', replacement)
+            .replace("<", replacement)
+            .replace(">", replacement)
+        )
     return (
         # Windows doesn't allow whitespace or dots at the end of filenames
         (name.rstrip(" .") if WINDOWS else name)
