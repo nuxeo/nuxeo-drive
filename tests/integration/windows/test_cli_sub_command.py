02766021765bc705aaacc03598635cfe1d2a868f
Sweta Yadav <sweta.yadav@hyland.com>
2023-07-03 14:13:04 +0530

    NXDRIVE-2815: Integration test fix

--- tests/integration/windows/test_cli_sub_command.py
+++ tests/integration/windows/test_cli_sub_command.py
@@ -86,7 +86,7 @@ def test_unbind_server(nuxeo_url, exe, folder):
         assert bind(exe, args)
         # print(f">>>> path type: {type(expanded_folder)}, path: {expanded_folder}")
         # path_str = "C:\\Users\\RUNNER~1\\AppData\\Local\\Temp"
-        print(f">>>>>> {os.listdir(expanded_folder)}")
+        print(f">>>>>> {os.listdir(os.getcwd())}")
         # print(f">>>> cwd: {os.getcwd()}")
         assert os.path.isdir(expanded_folder)
         assert unbind(exe, local_folder)
