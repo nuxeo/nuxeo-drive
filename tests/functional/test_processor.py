3b977fae9a3dbb1f1773a61ab1d54b410c82b2b9
gitofanindya <Anindya.Roy@hyland.com>
2023-07-20 10:20:17 +0530

    updated testcases 002

--- tests/functional/test_processor.py
+++ tests/functional/test_processor.py
@@ -1,12 +1,24 @@
 from collections import namedtuple
+from pathlib import Path
 from unittest.mock import patch
 
 from nxdrive.engine.processor import Processor
 
+Mocked_Session = namedtuple(
+    "session",
+    "status, uid",
+    defaults=("ok", "1"),
+)
+
+session = Mocked_Session()
+
+str_path = "unknownPath"
+path = Path(str_path)
+
 DocPair = namedtuple(
     "DocPair",
     "local_path, session",
-    defaults=("unknownPath", {"status": "OK"}),
+    defaults=(path, session),
 )
 
 
@@ -17,7 +29,7 @@ def test__synchronize_direct_transfer(manager_factory):
     doc_pair = DocPair()
 
     def mocked_get_session(*args, **kwargs):
-        return {"status": "OK", "": ""}
+        return session
 
     def mocked_upload(*args, **kwargs):
         return
@@ -25,12 +37,16 @@ def test__synchronize_direct_transfer(manager_factory):
     def mocked_direct_transfer_end(*args, **kwargs):
         return
 
+    def mocked_pause_session(*args, **kwargs):
+        return
+
     processor = Processor(engine, True)
-    sync_transfer = Processor._synchronize_direct_transfer
+    # sync_transfer = Processor._synchronize_direct_transfer
 
     with patch.object(dao, "get_session", new=mocked_get_session):
-        with patch.object(remote, "upload", new=mocked_upload):
-            with patch.object(
-                processor, "_direct_transfer_end", new=mocked_direct_transfer_end
-            ):
-                assert sync_transfer(doc_pair) is None
+        with patch.object(dao, "pause_session", new=mocked_pause_session):
+            with patch.object(remote, "upload", new=mocked_upload):
+                with patch.object(
+                    processor, "_direct_transfer_end", new=mocked_direct_transfer_end
+                ):
+                    assert processor._synchronize_direct_transfer(doc_pair) is None
