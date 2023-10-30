2c0dfe7c0b286bad36b3f7b79b508370d82f69d1
gitofanindya <Anindya.Roy@hyland.com>
2023-10-30 10:25:27 +0530

    0003

--- tests/functional/test_api.py
+++ tests/functional/test_api.py
@@ -1,24 +1,33 @@
+from collections import namedtuple
 from unittest.mock import patch
 
 from nxdrive.gui.api import QMLDriveApi
-from nxdrive.gui.application import Application
-from tests.conftest import nuxeo_url
 
 
-def test_web_authentication(manager_factory):
-    manager, engine = manager_factory()
+def test_web_authentication(manager_factory, nuxeo_url):
+    manager = manager_factory(with_engine=False)
+    manager.application = ""
 
-    def func(val):
+    def func(*args):
         return True
 
-    app = Application(manager)
+    def mocked_open_authentication_dialog():
+        return
+
+    Mocked_App = namedtuple(
+        "app",
+        "manager, open_authentication_dialog",
+        defaults=(manager, mocked_open_authentication_dialog),
+    )
+    app = Mocked_App()
+
     drive_api = QMLDriveApi(app)
 
     with manager:
         with patch.object(manager, "check_local_folder_available", new=func):
-            url = nuxeo_url()
+            url = nuxeo_url + "/login.jsp?requestedUrl=ui%2F"
             returned_val = drive_api.web_authentication(
-                url + "/login.jsp?requestedUrl=ui%2F",
+                url,
                 "/dummy-path",
                 True,
             )
