4c26efb8c7d3ee39e845bd7594ce3930051ebc2a
Anindya Roy <Anindya.Roy@hyland.com>
2023-11-23 10:23:36 +0530

    NXDRIVE-2860: Code Coverage - added two testcases in test_api.py

--- tests/functional/test_api.py
+++ tests/functional/test_api.py
@@ -52,3 +52,55 @@ def test_get_features_list(manager_factory):
     with manager:
         returned_val = drive_api.get_features_list()
         assert returned_val
+
+
+def test_generate_report(manager_factory):
+    manager = manager_factory(with_engine=False)
+    manager.application = ""
+
+    def mocked_open_authentication_dialog():
+        return
+
+    def func(*args):
+        return "Report"
+
+    Mocked_App = namedtuple(
+        "app",
+        "manager, open_authentication_dialog",
+        defaults=(manager, mocked_open_authentication_dialog),
+    )
+    app = Mocked_App()
+    drive_api = QMLDriveApi(app)
+
+    with manager:
+        with patch.object(manager, "generate_report", new=func):
+            returned_val = drive_api.generate_report()
+            assert returned_val
+
+
+def test_get_disk_space_info_to_width(manager_factory):
+    manager, engine = manager_factory()
+    manager.application = ""
+
+    def mocked_open_authentication_dialog():
+        return
+
+    def func(*args):
+        return 100, 200
+
+    Mocked_App = namedtuple(
+        "app",
+        "manager, open_authentication_dialog",
+        defaults=(manager, mocked_open_authentication_dialog),
+    )
+    app = Mocked_App()
+    drive_api = QMLDriveApi(app)
+
+    with manager:
+        from nxdrive import utils
+
+        with patch.object(utils, "disk_space", new=func):
+            returned_val = drive_api.get_disk_space_info_to_width(
+                "001", "dummy_path", 100
+            )
+            assert returned_val
