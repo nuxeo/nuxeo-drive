c799b728a2bd9d163cd3ff49614b7b3021f058cb
swetayadav1 <sweta.yadav@hyland.com>
2024-01-03 17:26:17 +0530

    Added Testcase for Linux LocalClient

---
+++ tests/unit/test_local_client_linux.py
@@ -0,0 +1,30 @@
+import subprocess
+from unittest.mock import Mock
+
+import pytest
+
+from nxdrive.client.local.linux import LocalClient
+
+
+@pytest.fixture
+def localclient(tmp_path):
+    localclient = LocalClient
+    localclient.shared_icons = tmp_path
+    return localclient
+
+
+def test_has_folder_icon(localclient, tmp_path, monkeypatch):
+    file = localclient.shared_icons / "emblem-nuxeo.svg"
+    file.write_bytes(b"baz\n")
+    localclient.abspath = Mock(return_value=tmp_path)
+    subprocess.check_output = Mock(return_value="metadata::emblems:")
+    assert not localclient.has_folder_icon(localclient, tmp_path)
+
+    # Test the exception handling
+    def mock_check_output(*args, **kwargs):
+        raise Exception
+
+    monkeypatch.setattr(subprocess, "check_output", mock_check_output)
+
+    with pytest.raises(Exception):
+        assert localclient.has_folder_icon(localclient, tmp_path)
