8aaaf7afedc6f129e2a04292ba2cbd7e5179eede
swetayadav1 <sweta.yadav@hyland.com>
2024-01-23 16:20:02 +0530

    Replaced distutil Losseversion with Packaging.version.parse

--- nxdrive/updater/utils.py
+++ nxdrive/updater/utils.py
@@ -2,8 +2,8 @@ import re
 from logging import getLogger
 from typing import Any, Dict, Optional, Tuple
 
-from distlib.version import LegacyVersion
 from nuxeo.utils import version_le, version_lt
+from packaging.version import parse
 
 from ..feature import Feature
 from ..options import Options
@@ -116,7 +116,7 @@ def get_latest_version(versions: Versions, channel: str, /) -> str:
         log.debug(f"No version found in {channel} channel.")
         return ""
 
-    highest = str(max(map(LegacyVersion, versions_list)))
+    highest = str(max(map(parse, versions_list)))
     return highest  # ᕦ(ò_óˇ)ᕤ
 
 
