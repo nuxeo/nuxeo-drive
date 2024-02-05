eeb192bb563840da454e42e33d6edc57b00ba61e
swetayadav1 <sweta.yadav@hyland.com>
2024-02-05 18:54:25 +0530

    Added testcase to check_os_version

---
+++ tests/unit/test_fatal_error.py
@@ -0,0 +1,18 @@
+import platform
+from unittest.mock import Mock
+
+from nxdrive import fatal_error
+
+
+def test_check_os_version(monkeypatch):
+    """Check the OS version compatiblity for Nuxeo Drive"""
+    assert fatal_error.check_os_version()
+
+    # Test for lower version of MacOS. It will pop-up a Fatal error screen
+    def mac_ver_test():
+        return ["10.2.1"]
+
+    monkeypatch.setattr(platform, "mac_ver", mac_ver_test)
+
+    fatal_error.fatal_error_mac = Mock()
+    assert not fatal_error.check_os_version()
