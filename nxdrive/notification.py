c9b57adddf52724c74762e0d979ac33b8071fd61
Sweta Yadav <Sweta.Yadav@hyland.com>
2024-07-05 09:29:00 +0530

    Added DisplayPendigTask class in notification.py

--- nxdrive/notification.py
+++ nxdrive/notification.py
@@ -14,7 +14,6 @@ from .utils import short_name
 if TYPE_CHECKING:
     from .engine.engine import Engine  # noqa
     from .manager import Manager  # noqa
-    from .notifications.tasks_notification import DisplayPendingTask  # noqa
 
 __all__ = ("DefaultNotificationService", "Notification")
 
@@ -517,6 +516,39 @@ class InvalidCredentialNotification(Notification):
             ),
             action="web_update_token",
             action_args=(engine_uid,),
+        )
+
+
+class DisplayPendingTask(Notification):
+    """Display a notification for pending tasks"""
+
+    def __init__(
+        self,
+        engine_uid: str,
+        remote_ref: str,
+        remote_path: str,
+        notification_title: str,
+        /,
+    ) -> None:
+        values = [remote_path]
+        super().__init__(
+            uid=notification_title,
+            title=(" ".join(notification_title.split("_"))).title(),
+            description=Translator.get(notification_title, values=values),
+            level=Notification.LEVEL_INFO,
+            flags=(
+                Notification.FLAG_PERSISTENT
+                | Notification.FLAG_BUBBLE
+                | Notification.FLAG_ACTIONABLE
+                | Notification.FLAG_DISCARD_ON_TRIGGER
+                | Notification.FLAG_REMOVE_ON_DISCARD
+            ),
+            action="display_pending_task",
+            action_args=(
+                engine_uid,
+                remote_ref,
+                remote_path,
+            ),
         )
 
 
