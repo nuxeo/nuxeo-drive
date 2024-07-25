b4e539c989fa5835482f36143f0ede4b4e1f508e
Anindya Roy <Anindya.Roy@hyland.com>
2024-07-25 15:32:51 +0530

    NXDRIVE-2889: Display system notification for document review -rollback deleted windows sign

--- tools/windows/deploy_ci_agent.ps1
+++ tools/windows/deploy_ci_agent.ps1
@@ -536,29 +536,25 @@ function sign($file) {
 	#	}
 	#}
 	Write-Output ">>> $Env:SM_CODE_SIGNING_CERT_SHA1_HASH"
+	Write-Output ">>> Signing $file"
+	& $Env:SIGNTOOL_PATH\signtool.exe sign `
+		/sha1 "$ENV:SM_CODE_SIGNING_CERT_SHA1_HASH" `
+		/n "$Env:SIGNING_ID_NEW" `
+		/d "$Env:APP_NAME" `
+		/td SHA256 /fd sha256 `
+		/tr http://timestamp.digicert.com/sha256/timestamp `
+		/v `
+		"$file"
 
+	if ($lastExitCode -ne 0) {
+		ExitWithCode $lastExitCode
+	}
 
-	#Write-Output ">>> Signing $file"
-	#& $Env:SIGNTOOL_PATH\signtool.exe sign `
-	#	/sha1 "$ENV:SM_CODE_SIGNING_CERT_SHA1_HASH" `
-	#	/n "$Env:SIGNING_ID_NEW" `
-	#	/d "$Env:APP_NAME" `
-	#	/td SHA256 /fd sha256 `
-	#	/tr http://timestamp.digicert.com/sha256/timestamp `
-	#	/v `
-	#	"$file"
-
-	#if ($lastExitCode -ne 0) {
-	#	ExitWithCode $lastExitCode
-	#}
-
-	#Write-Output ">>> Verifying $file"
-	#& $Env:SIGNTOOL_PATH\signtool.exe verify /pa /v "$file"
-	#if ($lastExitCode -ne 0) {
-	#	ExitWithCode $lastExitCode
-	#}
-
-
+	Write-Output ">>> Verifying $file"
+	& $Env:SIGNTOOL_PATH\signtool.exe verify /pa /v "$file"
+	if ($lastExitCode -ne 0) {
+		ExitWithCode $lastExitCode
+	}
 }
 function build_installer_and_sign {
 	# Build the installer
