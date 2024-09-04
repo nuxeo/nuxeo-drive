4878e47b3ef2de269df42e00b22060b3952ab0c2
Anindya Roy <104991338+gitofanindya@users.noreply.github.com>
2024-09-04 12:22:36 +0530

    NXDRIVE-2915: Translate Document type and container type labels on Direct Transfer popup (#5129)
    * NXDRIVE-2915: Translate Document type and container type labels on Direct Transfer popup

---
+++ nxdrive/gui/constants.py
@@ -0,0 +1,24 @@
+from ..translator import Translator
+
+
+def get_known_types_translations() -> dict:
+    KNOWN_FOLDER_TYPES = {
+        "OrderedFolder": Translator.get("ORDERED_FOLDER"),
+        "Folder": Translator.get("FOLDER"),
+    }
+    KNOWN_FILE_TYPES = {
+        "Audio": Translator.get("AUDIO"),
+        "File": Translator.get("FILE"),
+        "Picture": Translator.get("PICTURE"),
+        "Video": Translator.get("VIDEO"),
+    }
+    DEFAULT_TYPES = {
+        "Automatic": Translator.get("AUTOMATICS"),
+        "Create": Translator.get("CREATE"),
+    }
+
+    return {
+        "FOLDER_TYPES": KNOWN_FOLDER_TYPES,
+        "FILE_TYPES": KNOWN_FILE_TYPES,
+        "DEFAULT": DEFAULT_TYPES,
+    }
