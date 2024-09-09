ad962a38ed231ab7f165b813f89fff417e000012
Pooja Ramkrishna Daine <Pooja.RamkrishnaDaine@hyland.com>
2024-09-09 18:31:30 +0530

    NXDRIVE-2929: Upgrade Python from 3.9.5 to 3.12.3

--- nxdrive/utils.py
+++ nxdrive/utils.py
@@ -12,7 +12,7 @@ import stat
 import sys
 from configparser import DEFAULTSECT, ConfigParser
 from copy import deepcopy
-from datetime import datetime
+from datetime import datetime, timezone
 from functools import lru_cache
 from itertools import islice
 from logging import getLogger
@@ -1286,7 +1286,7 @@ def today_is_special() -> bool:
     """This beautiful day is special, isn't it? As all other days, right? :)"""
     return (
         os.getenv("I_LOVE_XMAS", "0") == "1"
-        or int(datetime.utcnow().strftime("%j")) >= 354
+        or int(datetime.now(tz=timezone.utc).strftime("%j")) >= 354
     )
 
 
@@ -1300,7 +1300,7 @@ def get_current_locale() -> str:
     else:
         import locale
 
-        encoding = locale.getdefaultlocale()[1] or ""
+        encoding = locale.getpreferredencoding(False) or ""
 
     # Guess the current locale name
     if WINDOWS:
@@ -1314,7 +1314,7 @@ def get_current_locale() -> str:
         l10n_code = NSLocale.currentLocale()
         l10n = NSLocale.localeIdentifier(l10n_code)
     else:
-        l10n = locale.getdefaultlocale()[0] or ""
+        l10n = locale.getpreferredencoding(False) or ""
 
     return ".".join([l10n, encoding])
 
