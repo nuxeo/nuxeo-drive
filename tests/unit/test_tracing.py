c71e2d9ca5674e73f47f031fe935dda5e4b85d8d
Anindya Roy <104991338+gitofanindya@users.noreply.github.com>
2025-03-11 10:45:06 +0530

    Update test_tracing.py

--- tests/unit/test_tracing.py
+++ tests/unit/test_tracing.py
@@ -17,22 +17,32 @@ import nxdrive.tracing
 #
 
 
+sc = scope.Scope()
+
 class CustomTransport(transport.Transport):
     def __init__(self):
         super().__init__()
         self._queue = None
+        print(f">>>> self._queue: {self._queue!r}")
 
     def capture_envelope(self):
+        print("Returning...")
         return
 
 
 @pytest.fixture(scope="function")
 def sentry_init_custom(monkeypatch):
     def inner(*a, **kw):
-        # scope = isolation_scope()
+        print(f">>>> a: {a!r}")
+        print(f">>>> kw: {kw!r}")
+        # sc = scope.Scope()
+        print(f">>>> sc: {sc!r}")
         client = Client(*a, **kw)
-        Scope.set_client(client)
-        monkeypatch.setattr(api.get_client(), "transport", CustomTransport())
+        # client.init()
+        print(f">>>> client: {client!r}")
+        sc.set_client(client)
+        print(f">>>> sc.get_client(): {sc.get_client()!r}")
+        monkeypatch.setattr(sc.get_client(), "transport", CustomTransport())
 
     yield inner
 
@@ -47,19 +57,22 @@ def test_flooding_prevention(sentry_init_custom):
     will not explode the Sentry quota.
     """
 
-    def whoopsy():
+    def whoopsy(sc):
         """Problematic function ..."""
+        print(f">>>>1 sc: {sc!r}")
         try:
             raise ValueError("Mock'ed error")
         except Exception:
-            capture_exception()
+            print("EXCP 1")
+            sc.capture_exception()
 
     def whoopsy2():
         """Problematic function ..."""
         try:
             raise ValueError("Mock'ed error")
         except Exception:
-            capture_exception()
+            print("EXCP 2")
+            sc.capture_exception()
 
     sentry_init_custom(before_send=nxdrive.tracing.before_send)
 
