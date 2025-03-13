01e496edab9091ecab44c9e877a78ae47a1d822c
Anindya Roy <104991338+gitofanindya@users.noreply.github.com>
2025-03-13 14:31:56 +0530

    Update base.py

--- nxdrive/dao/base.py
+++ nxdrive/dao/base.py
@@ -34,7 +34,7 @@ class AutoRetryCursor(Cursor):
 
                 new_param = tuple(
                     (
-                        sqlite3.register_adapter(param, self.adapt_datetime_iso)
+                        sqlite3.register_adapter(param, self.adapt_datetime_iso) # noqa
                         if isinstance(param, datetime)
                         else param
                     )
