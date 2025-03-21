6ddd3e5a09927ceacf6a7db74e2d2d05facae1ad
Anindya Roy <104991338+gitofanindya@users.noreply.github.com>
2025-03-21 08:58:15 +0530

    Update utils.py

--- tests/integration/windows/utils.py
+++ tests/integration/windows/utils.py
@@ -31,6 +31,8 @@ def fatal_error_dlg(app, with_details: bool = True) -> bool:
     log.info(f">>>> dlg: {dlg!r}")
     print(f">>>> dlg: {dlg!r}")
     print(f">>>> dlg.exists(): {dlg.exists()!r}")
+    print(f">>>> dlg.__dir__: {dlg.__dir__!r}")
+    print(f">>>> dlg.__dict__: {dlg.__dict__!r}")
     if dlg.exists():
         log.info(">>>> if dlg.exists")
         print(">>>> if dlg.exists")
