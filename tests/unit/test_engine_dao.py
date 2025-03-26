b6c64a0a76e6bf400ccbef99ea63d47b238a3e7a
Anindya Roy <104991338+gitofanindya@users.noreply.github.com>
2025-03-26 10:49:29 +0530

    Update test_engine_dao.py

--- tests/unit/test_engine_dao.py
+++ tests/unit/test_engine_dao.py
@@ -15,8 +15,8 @@ from ..markers import windows_only
 
 DocPair = namedtuple(
     "DocPair",
-    "error_count, local_state, pair_state, processor, id, last_error, error_count",
-    defaults=(0, "", "", 0, 1, "", 0),
+    "error_count, local_state, pair_state, processor, id, last_error",
+    defaults=(0, "", "", 0, 1, ""),
 )
 
 
