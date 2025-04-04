3c4a589c505a688a9ddb55b05bc79d4ebfb3cc42
Anindya Roy <104991338+gitofanindya@users.noreply.github.com>
2025-04-04 13:26:43 +0530

    Update utils.py

--- nxdrive/utils.py
+++ nxdrive/utils.py
@@ -1367,4 +1367,4 @@ def get_task_type(type_of_task: str) -> str:
 
 def adapt_datetime_iso(val: datetime, /) -> Any:
     datetime_object = datetime.fromtimestamp(int(val.strftime("%s")), tz=timezone.utc)
-    return datetime_object.strftime('%Y-%m-%d %H:%M:%S')
+    return datetime_object.strftime("%Y-%m-%d %H:%M:%S")
