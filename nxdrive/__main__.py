4f2320b39957d0ffc8dd04de08897b684c95ab18
Anindya Roy <104991338+gitofanindya@users.noreply.github.com>
2025-04-04 13:09:19 +0530

    Update __main__.py

--- nxdrive/__main__.py
+++ nxdrive/__main__.py
@@ -6,7 +6,9 @@ See https://github.com/pyinstaller/pyinstaller/issues/2560
 import locale
 import platform
 import signal
+import sqlite3
 import sys
+from datetime import datetime
 from types import FrameType
 
 from nxdrive.constants import APP_NAME
@@ -15,6 +17,7 @@ from nxdrive.fatal_error import (
     check_os_version,
     show_critical_error,
 )
+from nxdrive.utils import adapt_datetime_iso
 
 
 def signal_handler(signum: int, _: FrameType, /) -> None:
@@ -33,6 +36,8 @@ def main() -> int:
 
     # Catch CTRL+C
     signal.signal(signal.SIGINT, signal_handler)
+
+    sqlite3.register_adapter(datetime, adapt_datetime_iso)
 
     ret = 0
 
