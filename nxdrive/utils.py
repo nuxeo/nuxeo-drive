32da6477880c6fd8f2249924a25d4a9d5e761262
Anindya Roy <104991338+gitofanindya@users.noreply.github.com>
2025-04-09 13:46:43 +0530

    Update utils.py

--- nxdrive/utils.py
+++ nxdrive/utils.py
@@ -1366,5 +1366,6 @@ def get_task_type(type_of_task: str) -> str:
 
 
 def adapt_datetime_iso(val: datetime, /) -> Any:
+    # datetime adapter for sqlite3
     datetime_object = datetime.fromtimestamp(val.timestamp(), tz=timezone.utc)
     return datetime_object.strftime("%Y-%m-%d %H:%M:%S")
