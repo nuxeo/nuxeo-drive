7cfe0d86ef6602c6f6fa6ab1849f87ec1de27486
S-Ghosh-Dev2 <saptarshi.ghosh@hyland.com>
2025-06-04 14:40:50 +0530

    using a variable to specify the TLS version

--- nxdrive/utils.py
+++ nxdrive/utils.py
@@ -69,8 +69,6 @@ DEFAULTS_CERT_DETAILS = {
     "notBefore": "N/A",
 }
 
-MINIMUM_TLS_VERSION = "TLSv1_2"
-
 log = getLogger(__name__)
 
 
@@ -605,12 +603,13 @@ def retrieve_ssl_certificate(hostname: str, /, *, port: int = 443) -> str:
     """Retrieve the SSL certificate from a given hostname."""
 
     import ssl
+    MINIMUM_TLS_VERSION = ssl.TLSVersion.TLSv1_2
 
     with ssl.create_connection((hostname, port)) as conn:  # type: ignore
         # Declaring a minimum version to restrict the protocol
         # For more information check NXDRIVE-2920
         context = ssl.create_default_context()
-        context.minimum_version = ssl.TLSVersion.TLSv1_2
+        context.minimum_version = MINIMUM_TLS_VERSION
         with context.wrap_socket(conn, server_hostname=hostname) as sock:
             cert_data: bytes = sock.getpeercert(binary_form=True)  # type: ignore
             return ssl.DER_cert_to_PEM_cert(cert_data)
