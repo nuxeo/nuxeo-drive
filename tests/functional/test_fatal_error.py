f4bb1d1cb8ebe7c1c1ffa2b4d4bb2a91d3763024
S-Ghosh-Dev2 <saptarshi.ghosh@hyland.com>
2025-07-01 13:24:43 +0530

    1 test case added

--- tests/functional/test_fatal_error.py
+++ tests/functional/test_fatal_error.py
@@ -5,11 +5,22 @@ Functional test for nxdrive/fatal_error.py
 from pathlib import Path
 from unittest.mock import patch
 
-from nxdrive.fatal_error import check_executable_path_error_qt
+from nxdrive.fatal_error import check_executable_path_error_qt, fatal_error_qt
+
+from ..markers import not_linux
 
 
+@not_linux(reason="Qt does not work correctly on Linux")
 @patch("nxdrive.qt.imports.QMessageBox.exec_")
 def test_check_executable_path_error_qt(mock_exec):
     mock_exec.return_value = None
     output = check_executable_path_error_qt(Path())
+    assert output is None
+
+
+@not_linux(reason="Qt does not work correctly on Linux")
+@patch("nxdrive.qt.imports.QApplication.exec_")
+def test_fatal_error_qt(mock_exec):
+    mock_exec.return_value = None
+    output = fatal_error_qt("Dummy exception")
     assert output is None
