018307a9bd710d07b6f66638ad075d46df1f9ef7
S-Ghosh-Dev2 <saptarshi.ghosh@hyland.com>
2025-07-16 19:28:00 +0530

    application test case updated

--- tests/functional/test_application.py
+++ tests/functional/test_application.py
@@ -24,10 +24,37 @@ def test_exit_app(manager_factory):
         "nxdrive.gui.application.Application.init_nxdrive_listener"
     ) as mock_listener, patch(
         "nxdrive.gui.application.Application.show_metrics_acceptance"
-    ) as mock_show_metrics:
+    ) as mock_show_metrics, patch(
+        "nxdrive.engine.activity.FileAction.__repr__"
+    ) as mock_download_repr:
         mock_root_objects.return_value = [QObject()]
         mock_find_child.return_value = mock_qt
         mock_listener.return_value = None
         mock_show_metrics.return_value = None
+        mock_download_repr.return_value = "Nuxeo Drive"
         app = Application(manager)
         assert app.exit_app() is None
+
+
+@not_linux(reason="Qt does not work correctly on Linux")
+def test_shutdown(manager_factory):
+    manager, engine = manager_factory()
+    mock_qt = Mock_Qt()
+    with patch(
+        "PyQt5.QtQml.QQmlApplicationEngine.rootObjects"
+    ) as mock_root_objects, patch(
+        "PyQt5.QtCore.QObject.findChild"
+    ) as mock_find_child, patch(
+        "nxdrive.gui.application.Application.init_nxdrive_listener"
+    ) as mock_listener, patch(
+        "nxdrive.gui.application.Application.show_metrics_acceptance"
+    ) as mock_show_metrics, patch(
+        "nxdrive.engine.activity.FileAction.__repr__"
+    ) as mock_download_repr:
+        mock_root_objects.return_value = [QObject()]
+        mock_find_child.return_value = mock_qt
+        mock_listener.return_value = None
+        mock_show_metrics.return_value = None
+        mock_download_repr.return_value = "Nuxeo Drive"
+        app = Application(manager)
+        assert app._shutdown() is None
