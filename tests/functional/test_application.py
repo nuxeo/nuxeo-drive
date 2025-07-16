cc8a1a180fa270a1dbc439cc45a8e158f9020c1a
S-Ghosh-Dev2 <saptarshi.ghosh@hyland.com>
2025-07-16 17:42:44 +0530

    test_application.py re-added, added logs to application.py to debug

---
+++ tests/functional/test_application.py
@@ -0,0 +1,30 @@
+"""
+Functional tests for nxdrive/gui/application.py
+"""
+
+from unittest.mock import patch
+
+from PyQt5.QtCore import QObject
+
+from nxdrive.gui.application import Application
+from tests.functional.mocked_classes import Mock_Qt
+
+from ..markers import not_linux
+
+
+@not_linux(reason="Qt does not work correctly on Linux")
+def test_exit_app(manager_factory):
+    manager, engine = manager_factory()
+    mock_qt = Mock_Qt()
+    with patch(
+        "PyQt5.QtQml.QQmlApplicationEngine.rootObjects"
+    ) as mock_root_objects, patch(
+        "PyQt5.QtCore.QObject.findChild"
+    ) as mock_find_child, patch(
+        "nxdrive.gui.application.Application.init_nxdrive_listener"
+    ) as mock_listener:
+        mock_root_objects.return_value = [QObject()]
+        mock_find_child.return_value = mock_qt
+        mock_listener.return_value = None
+        app = Application(manager)
+        assert app.exit_app() is None
