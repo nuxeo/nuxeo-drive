37158fb8d55659ec6565fd74d2db8da8b00cdc25
S-Ghosh-Dev2 <saptarshi.ghosh@hyland.com>
2025-07-17 11:00:41 +0530

    application test case update

--- tests/functional/test_application.py
+++ tests/functional/test_application.py
@@ -106,3 +106,27 @@ def test_update_workflow(manager_factory):
         mock_download_repr.return_value = "Nuxeo Drive"
         app = Application(manager)
         assert app.update_workflow() is None
+
+
+@not_linux(reason="Qt does not work correctly on Linux")
+def test_update_feature_state(manager_factory):
+    manager, engine = manager_factory()
+    mock_qt = Mock_Qt()
+    with patch(
+        "PyQt5.QtQml.QQmlApplicationEngine.rootObjects"
+    ) as mock_root_objects, patch(
+        "PyQt5.QtCore.QObject.findChild"
+    ) as mock_find_child, patch(
+        "nxdrive.gui.application.Application.init_nxdrive_listener"
+    ) as mock_listener, patch(
+        "nxdrive.gui.application.Application.show_metrics_acceptance"
+    ) as mock_show_metrics, patch(
+        "nxdrive.engine.activity.FileAction.__repr__"
+    ) as mock_download_repr:
+        mock_root_objects.return_value = [QObject()]
+        mock_find_child.return_value = mock_qt
+        mock_listener.return_value = None
+        mock_show_metrics.return_value = None
+        mock_download_repr.return_value = "Nuxeo Drive"
+        app = Application(manager)
+        assert app._update_feature_state("tasks_management", True) is None
