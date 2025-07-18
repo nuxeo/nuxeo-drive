8c4578dad71f16ada467065038c4af5ac2408d7a
S-Ghosh-Dev2 <saptarshi.ghosh@hyland.com>
2025-07-18 08:56:33 +0530

    application test updated

--- tests/functional/test_application.py
+++ tests/functional/test_application.py
@@ -165,7 +165,7 @@ def test_msbox(manager_factory):
 
 
 @not_linux(reason="Qt does not work correctly on Linux")
-def test_display_info(manager_factory):
+def test_direct_edit_conflict(manager_factory):
     manager, engine = manager_factory()
     mock_qt = Mock_Qt()
     with patch(
@@ -179,42 +179,19 @@ def test_display_info(manager_factory):
     ) as mock_show_metrics, patch(
         "nxdrive.engine.activity.FileAction.__repr__"
     ) as mock_download_repr, patch(
-        "nxdrive.gui.application.Application._msgbox"
-    ) as mock_msg_box:
+        "PyQt5.QtWidgets.QDialog.exec_"
+    ) as mock_exec, patch(
+        "nxdrive.engine.workers.PollWorker._execute"
+    ) as mock_execute:
         mock_root_objects.return_value = [QObject()]
         mock_find_child.return_value = mock_qt
         mock_listener.return_value = None
         mock_show_metrics.return_value = None
         mock_download_repr.return_value = "Nuxeo Drive"
-        mock_msg_box.return_value = None
-        app = Application(manager)
-        assert app.display_info("Nuxeo Drive", "All OK", ["value1", "value2"]) is None
-
-
-@not_linux(reason="Qt does not work correctly on Linux")
-def test_display_warning(manager_factory):
-    manager, engine = manager_factory()
-    mock_qt = Mock_Qt()
-    with patch(
-        "PyQt5.QtQml.QQmlApplicationEngine.rootObjects"
-    ) as mock_root_objects, patch(
-        "PyQt5.QtCore.QObject.findChild"
-    ) as mock_find_child, patch(
-        "nxdrive.gui.application.Application.init_nxdrive_listener"
-    ) as mock_listener, patch(
-        "nxdrive.gui.application.Application.show_metrics_acceptance"
-    ) as mock_show_metrics, patch(
-        "nxdrive.engine.activity.FileAction.__repr__"
-    ) as mock_download_repr, patch(
-        "nxdrive.gui.application.Application._msgbox"
-    ) as mock_msg_box:
-        mock_root_objects.return_value = [QObject()]
-        mock_find_child.return_value = mock_qt
-        mock_listener.return_value = None
-        mock_show_metrics.return_value = None
-        mock_download_repr.return_value = "Nuxeo Drive"
-        mock_msg_box.return_value = None
+        mock_exec.return_value = None
+        mock_execute.return_value = None
         app = Application(manager)
         assert (
-            app.display_warning("Nuxeo Drive", "All OK", ["value1", "value2"]) is None
+            app._direct_edit_conflict("dummy_file_name", "dummr_ref_path", "md5")
+            is None
         )
