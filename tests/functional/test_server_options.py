ab4c78c61d90b849caa51655bc95e4197928c5a3
S-Ghosh-Dev2 <saptarshi.ghosh@hyland.com>
2025-07-27 19:29:28 +0530

    test_server_options updated

--- tests/functional/test_server_options.py
+++ tests/functional/test_server_options.py
@@ -22,8 +22,7 @@ def test_behavior(manager_factory):
     def disabled():
         return {"behavior": {"server_deletion": False}}
 
-    # Default value is True
-    assert Behavior.server_deletion is True
+    assert Behavior.server_deletion is False
 
     # Mimic the IT team disabling the behavior
     with patch.object(engine.remote, "get_server_configuration", new=disabled):
@@ -51,8 +50,7 @@ def test_behavior_not_good(caplog, manager_factory):
     def bad_value():
         return {"behavior": {"server_deletion": "oui"}}
 
-    # Default value is True
-    assert Behavior.server_deletion is True
+    assert Behavior.server_deletion is False
 
     # Mimic the IT team setting an unknown behavior
     with patch.object(engine.remote, "get_server_configuration", new=unknown):
