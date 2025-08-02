7c36d9064dbc908cce4e3bb894c02076a7971818
nuxeodrive <drive@hyland.com>
2025-08-02 00:53:21 +0000

    Bump version to 5.6.1.2

--- nxdrive/__init__.py
+++ nxdrive/__init__.py
@@ -27,7 +27,7 @@ To declare a beta, use this schema:
 """
 
 __author__ = "Nuxeo"
-__version__ = "5.6.1"
+__version__ = "5.6.1.2"
 __copyright__ = """
     Copyright © 2025 Hyland Software, Inc. and its affiliates. All rights reserved.
     All Hyland product names are registered or unregistered trademarks of Hyland Software, Inc. or its affiliates
