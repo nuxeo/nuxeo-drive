c17a0387add254d7d4c0e65ed234276fea3e37cd
Anindya Roy <104991338+gitofanindya@users.noreply.github.com>
2025-09-03 00:13:37 +0530

    updated test files

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -1363,12 +1363,12 @@ def test_unlock():
 
 
 def test_find_real_office_file():
-    file_path = self.location / "resources" / "files" / "~$stFile.doc"
+    file_path = "tests/resources/files/~$stFile.doc"
     file_name = nxdrive.utils.find_real_office_file(str(file_path))
     assert file_name == "testFile.doc"
 
 
 def test_unlock():
-    file_path = self.location / "resources" / "files" / ".~lock.testFile.odt#"
+    file_path = "tests/resources/files/.~lock.testFile.odt#"
     file_name = nxdrive.utils.find_real_office_file(str(file_path))
     assert file_name == "testFile.odt"
