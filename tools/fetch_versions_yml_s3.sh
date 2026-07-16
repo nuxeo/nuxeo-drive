cccadade628b68fedf9dd0be6b8272ebf35dc3d2
S-Ghosh-Dev2 <saptarshi.ghosh@hyland.com>
2026-07-16 11:38:04 +0530

    Update versions yaml fetch script

--- tools/fetch_versions_yml_s3.sh
+++ tools/fetch_versions_yml_s3.sh
@@ -4,8 +4,10 @@
 git_env=$1
 if [ "$git_env" = "development" ]; then
 	url="https://hyland-software-channel-dist.opsexp.alfresco.me/alfresco/drive-updates/versions.yml"
+	bucket="s3://test-hyland-software-channel-dist/alfresco/drive-updates/"
 elif [ "$git_env" = "production" ]; then
 	url="https://drive.software-channels.hyland.com/alfresco/drive-updates/versions.yml"
+	bucket="s3://drive.software-channels.hyland.com/alfresco/drive-updates/"
 else
 	echo "Missing/Incorrect environment provided!! Aborting..."
 	exit 1
@@ -16,7 +18,10 @@ response=$(curl -l $url)
 
 if [[ $response == *"<Code>AccessDenied</Code>"* ]]; then
 	echo "versions.yml not found"
-	exit 1
+	echo "Creating empty versions.yml file as it does not exist in S3."
+	touch versions.yml
+    aws s3 cp versions.yml $bucket
+	exit 0
 else
 	echo "versions.yml found !! Downloading...."
 	curl -o ./versions.yml $url
