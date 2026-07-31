669a2021982f8e29ed2e641afa928f81e440b0b0
Anindya Roy <Anindya.Roy@hyland.com>
2026-07-31 18:45:36 +0530

    fixed intigration for nuxeo 3

--- tests/alfresco/integration/windows/conftest.py
+++ tests/alfresco/integration/windows/conftest.py
@@ -6,5 +6,17 @@ Re-exports the ``exe`` fixture from the common integration conftest.
 from tests.common.integration.windows.conftest import (  # noqa: F401
     exe,
     final_exe,
-    pytest_addoption,
 )
+
+
+def pytest_addoption(parser):
+    """Register --executable only if not already registered."""
+    try:
+        parser.addoption(
+            "--executable",
+            action="store",
+            default="dist\\ndrive\\ndrive.exe",
+            help="Path to the executable to test.",
+        )
+    except ValueError:
+        pass
